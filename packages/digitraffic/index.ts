d71bab50f6bb5e6459fa715e51f7536e00a0fbce
Jasper Nykänen <nykanen@hey.com>
2022-02-28 09:35:29 +0200

    feat: export get live trains

--- packages/digitraffic/index.ts
+++ packages/digitraffic/index.ts
@@ -1,6 +1,7 @@
 export * from './src/get_stations'
 export * from './src/tweak_station_names'
 export * from './src/get_station_path'
+export * from './src/get_live_trains'
 
 export * from './types/station'
 export * from './types/timetable_row'
