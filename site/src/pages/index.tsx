e5bce2d81e12054b5e44d863a9e8da21af23f291
Jasper Nykänen <nykanen@hey.com>
2022-03-08 20:07:31 +0200

    feat: remove get stations from cahce
    The wrapper function is removed in favor of caching the requests directly in packages/digitraffic

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import type { GetStaticPropsContext, GetStaticPropsResult } from 'next'
-import type { LocalizedStation } from '~digitraffic'
+import { getStations, LocalizedStation } from '~digitraffic'
 
 import { getStationPath } from '~digitraffic'
 
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { getLocaleOrThrow } from '../utils/get_locale_or_throw'
-import { getStationsFromCache } from '../utils/get_station_from_cache'
 
 interface HomePageProps {
   stations: LocalizedStation[]
@@ -37,7 +36,7 @@ export default function HomePage({ stations }: HomePageProps) {
 export const getStaticProps = async (
   context: GetStaticPropsContext
 ): Promise<GetStaticPropsResult<HomePageProps>> => {
-  const stations = await getStationsFromCache<LocalizedStation[]>({
+  const stations = await getStations<LocalizedStation[]>({
     includeNonPassenger: false,
     locale: getLocaleOrThrow(context.locale)
   })
