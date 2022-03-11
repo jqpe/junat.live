cfc97ac5489cf50de9d28f8537aca24323b4f377
Jasper Nykänen <nykanen@hey.com>
2022-03-11 12:51:56 +0200

    feat: add sort trains util

---
+++ packages/digitraffic/src/sort_trains.ts
@@ -0,0 +1,34 @@
+import type { Train } from '../types/train'
+
+const findTimetableRow = (
+  train: Train,
+  stationShortCode: string,
+  type: 'DEPARTURE' | 'ARRIVAL'
+) => {
+  return train.timeTableRows.find(
+    tr => tr.stationShortCode === stationShortCode && tr.type === type
+  )
+}
+
+export const sortTrains = (
+  trains: Train[],
+  stationShortCode: string,
+  type: 'ARRIVAL' | 'DEPARTURE'
+) => {
+  return trains.sort((aTrain, bTrain) => {
+    const aRow = findTimetableRow(aTrain, stationShortCode, type)
+    const bRow = findTimetableRow(bTrain, stationShortCode, type)
+
+    if (!(aRow && bRow)) {
+      console.warn(
+        `Couldn't find a timetable row for train ${aTrain.trainNumber} and ${bTrain.trainNumber}.`
+      )
+      return 0
+    }
+
+    return (
+      +Date.parse(aRow.liveEstimateTime || aRow.scheduledTime) -
+      +Date.parse(bRow.liveEstimateTime || bRow.scheduledTime)
+    )
+  })
+}
