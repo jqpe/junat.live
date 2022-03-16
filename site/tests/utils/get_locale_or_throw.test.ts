a33fc9bdca8ce58d463a5ac59a1c6bca6efc5e78
Jasper Nykänen <nykanen@hey.com>
2022-03-16 06:59:01 +0200

    tests: add test for get locale or throw

---
+++ site/tests/utils/get_locale_or_throw.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+import { getLocaleOrThrow } from '../../src/utils/get_locale_or_throw'
+
+import config from '../../next.config'
+
+describe('get locale or throw', () => {
+  it("doesn't throw for the languages listed in config", () => {
+    expect(config.i18n?.locales).toBeDefined()
+
+    for (const locale of config.i18n?.locales!) {
+      expect(() => getLocaleOrThrow(locale)).not.toThrow()
+    }
+  })
+
+  it('throws for an unsupported language', () => {
+    expect(() => getLocaleOrThrow('not a locale')).toThrow()
+  })
+})
