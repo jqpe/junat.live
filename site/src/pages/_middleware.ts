3825b1e63a87dc1025ca4bb60c9ada51bbb0303b
Jasper Nykänen <nykanen@hey.com>
2022-03-22 18:19:01 +0200

    refactor: remove unused argument and console.log

--- site/src/pages/_middleware.ts
+++ site/src/pages/_middleware.ts
@@ -1,6 +1,6 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest, event: NextFetchEvent) {
+export function middleware(request: NextRequest) {
   const path = decodeURI(request.nextUrl.pathname)
     .split('/')
     .filter(path => Boolean(path))
@@ -8,8 +8,6 @@ export function middleware(request: NextRequest, event: NextFetchEvent) {
   const trainPaths = ['tåg', 'train', 'juna']
 
   if (trainPaths.includes(path[0])) {
-    console.log(decodeURI(request.nextUrl.href).replace(path[0], 'train'))
-
     return NextResponse.rewrite(
       decodeURI(request.nextUrl.href).replace(path[0], 'train')
     )
