cbb9b5b39613a989417c65945e8fe77434f62021
Jasper Nykänen <nykanen@hey.com>
2022-03-22 18:46:06 +0200

    refactor: add a jsdoc comment to format train time

--- site/src/utils/format_train_time.ts
+++ site/src/utils/format_train_time.ts
@@ -1,3 +1,9 @@
+/**
+ * Formats a date into format `hh:mm` for use in timetable rows.
+ * @param {string} dateString
+ * A valid date that can be parsed with `Date.parse()`
+ * @see https://mdn.io/date_parse
+ */
 export const formatTrainTime = (dateString: string) =>
   Intl.DateTimeFormat('fi', {
     hour: '2-digit',
