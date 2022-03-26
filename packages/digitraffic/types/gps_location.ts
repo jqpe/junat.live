2e2ddbaf7f88579a84d8c3e727dfc030d47bb24d
Jasper Nykänen <nykanen@hey.com>
2022-03-26 21:00:20 +0200

    feat(digitraffic): add gps location type

---
+++ packages/digitraffic/types/gps_location.ts
@@ -0,0 +1,10 @@
+export interface GpsLocation {
+  trainNumber: number
+  departureDate: string
+  timestamp: string
+  location: {
+    type: 'Point'
+    coordinates: [number, number]
+  }
+  speed: number
+}
