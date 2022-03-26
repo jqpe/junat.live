98be5f7e0edae6a74876379957870ae1aaef9c27
Jasper Nykänen <nykanen@hey.com>
2022-03-26 02:22:47 +0200

    feat: export types

--- packages/digitraffic-mqtt/src/index.ts
+++ packages/digitraffic-mqtt/src/index.ts
@@ -1,2 +1,7 @@
 export * from './subscribe_to_station'
 export * from './subscribe_to_trains'
+
+export type { TrainsMqttClient } from './types/train_mqtt_client'
+export type { StationMqttClient } from './types/station_mqtt_client'
+export type { SubscribeToTrainsOptions } from './types/subscribe_to_trains_options'
+export type { MessageGeneratorResult } from './types/message_generator_result'
