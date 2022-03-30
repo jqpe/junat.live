a12c3f64c37d8ff9255e8e0dca3fea64b23bea80
Jasper Nykänen <nykanen@hey.com>
2022-03-30 11:37:58 +0300

    feat: add digitraffic api slice

---
+++ site/src/features/digitraffic/digitraffic_slice.ts
@@ -0,0 +1,34 @@
+import type { BaseQueryFn } from '@reduxjs/toolkit/query/react'
+import type { GetTrainsOptions, Train } from '~digitraffic'
+
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { getLiveTrains } from '~digitraffic'
+
+interface TrainsEndpoint {
+  options?: GetTrainsOptions
+  stationShortCode: string
+}
+
+const trainsBaseQuery: BaseQueryFn<TrainsEndpoint> = async args => {
+  try {
+    const trains = await getLiveTrains(args.stationShortCode, args.options)
+
+    return { data: trains }
+  } catch (error) {
+    return { error }
+  }
+}
+
+export const digitrafficApi = createApi({
+  reducerPath: 'digitraffic',
+  baseQuery: trainsBaseQuery,
+  endpoints(build) {
+    return {
+      trains: build.query<Train[], TrainsEndpoint>({
+        query: options => options
+      })
+    }
+  }
+})
+
+export const { useTrainsQuery } = digitrafficApi
