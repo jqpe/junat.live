0e02225a62705735e50a04ccfa01d1fcccd8f719
Jasper Nykänen <nykanen@hey.com>
2022-03-30 11:39:26 +0300

    feat: add redux provider

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps as NextAppProps } from 'next/app'
-import type { FC } from 'react'
+import type { FC, ReactChild } from 'react'
+
+import { Provider } from 'react-redux'
+
+import { store } from '../app/store'
 
 import '../sass/globals.scss'
 
@@ -7,14 +11,18 @@ interface AppProps extends NextAppProps {
   Component: NextAppProps['Component'] & { layout?: FC }
 }
 
+const provider = (children: ReactChild | ReactChild[]) => {
+  return <Provider store={store}>{children}</Provider>
+}
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   if (Component.layout) {
-    return (
+    return provider(
       <Component.layout>
         <Component {...pageProps} />
       </Component.layout>
     )
   }
 
-  return <Component {...pageProps} />
+  return provider(<Component {...pageProps} />)
 }
