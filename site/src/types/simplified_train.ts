bae3e838cb79ef9251a8d6d9f0e959bd22a5c05e
Jasper Nykänen <nykanen@hey.com>
2022-03-31 10:25:48 +0300

    feat: add simplified train interface

---
+++ site/src/types/simplified_train.ts
@@ -0,0 +1,16 @@
+/**
+ * Train type that only contains application critical properties.
+ *
+ * Storing full `Train`s get's expensive when they're cached in memory by Redux.
+ */
+export interface SimplifiedTrain {
+  destination: Partial<Record<'fi' | 'en' | 'sv', string>>
+  departureDate: string
+  scheduledTime: string
+  liveEstimateTime?: string
+  track?: string
+  commuterLineID?: string
+  trainNumber: number
+  trainType: string
+  version: number
+}
