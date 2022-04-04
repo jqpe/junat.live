30ff334f959305eb7786773c1fdb47fa4d18b071
Jasper Nykänen <nykanen@hey.com>
2022-04-04 08:52:16 +0300

    feat: add geolocation button that navigates to nearest station
    TODO: Error handling (e.g. user denied access)
    TODO: Hide the button on devices where geolocation is not supported (https://caniuse.com/?search=geolocation)

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -3,14 +3,20 @@ import type { LocalizedStation } from '~digitraffic'
 
 import { getStationPath } from '~digitraffic'
 
-import { getStations } from '../../lib/get_stations'
-
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useMemo } from 'react'
+
+import { getStations } from '../../lib/get_stations'
+
+import GeolocationButton from '@components/GeolocationButton'
+
+import useGeolocation from '@hooks/use_geolocation.hook'
 
 import Page from '@layouts/Page'
 
 import { getLocaleOrThrow } from '@utils/get_locale_or_throw'
+import getNearestStation from '@utils/get_nearest_station'
 
 interface HomePageProps {
   stations: LocalizedStation[]
@@ -20,8 +26,30 @@ export default function HomePage({ stations }: HomePageProps) {
   const router = useRouter()
   const locale = getLocaleOrThrow(router.locale)
 
+  const geolocation = useGeolocation()
+
+  useMemo(() => {
+    if (!(geolocation.position && locale)) {
+      return
+    }
+
+    const nearestStation = getNearestStation(stations, geolocation.position)
+
+    const stationPath = getStationPath(
+      nearestStation.stationName[locale] as string
+    )
+
+    router.push(`/${stationPath}`)
+  }, [geolocation.position, locale, stations, router])
+
   return (
     <main>
+      <nav>
+        <GeolocationButton
+          handleClick={geolocation.getCurrentPosition}
+          alt=""
+        />
+      </nav>
       <ul>
         {stations.map(station => (
           <li key={station.stationShortCode}>
