c97eb868419f720ac0aa853cea6755c801d67595
Jasper Nykänen <nykanen@hey.com>
2022-04-04 08:50:11 +0300

    feat: add get distance util

---
+++ site/src/utils/get_distance.ts
@@ -0,0 +1,34 @@
+const EARTH_RADIUS = 6378137
+const toRadians = (degrees: number) => degrees * (Math.PI / 180)
+
+// Math.acos only allows x ∈ [-1, 1]
+const tweakAcos = (n: number) => (n > 1 ? 1 : n < -1 ? -1 : n)
+
+/**
+ * Returns an inaccurate position by calculating the distance between two points on a 2D plane.
+ */
+export default function getDistance({
+  from,
+  to,
+  accuracy = 1
+}: {
+  from: Pick<GeolocationCoordinates, 'latitude' | 'longitude'>
+  to: Pick<GeolocationCoordinates, 'latitude' | 'longitude'>
+  accuracy?: number
+}) {
+  const [sinToLat, sinFromLat, cosToLat, cosFromLat, cosLongDifference] = [
+    Math.sin(toRadians(to.latitude)),
+    Math.sin(toRadians(from.latitude)),
+    Math.cos(toRadians(to.latitude)),
+    Math.cos(toRadians(from.latitude)),
+    Math.cos(toRadians(from.longitude) - toRadians(to.longitude))
+  ]
+
+  const tweakedAcos = tweakAcos(
+    sinToLat * sinFromLat + cosToLat * cosFromLat * cosLongDifference
+  )
+
+  const distance = Math.acos(tweakedAcos) * EARTH_RADIUS
+
+  return Math.round(distance / accuracy) * accuracy
+}
