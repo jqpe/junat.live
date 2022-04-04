ac29f4401dca9cf979204e52422b1fda90e9be7a
Jasper Nykänen <nykanen@hey.com>
2022-04-04 08:50:24 +0300

    feat: add get nearest station util

---
+++ site/src/utils/get_nearest_station.ts
@@ -0,0 +1,32 @@
+import type { LocalizedStation, Station } from '~digitraffic'
+
+import getDistance from '@utils/get_distance'
+
+export default function getNearestStation<T extends LocalizedStation | Station>(
+  stations: T[],
+  position: Pick<GeolocationPosition, 'coords'>
+): T {
+  return stations.reduce((prev, curr) => {
+    if (
+      getDistance({
+        from: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        },
+        to: { latitude: curr.latitude, longitude: curr.longitude },
+        accuracy: position.coords.accuracy
+      }) <
+      getDistance({
+        from: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        },
+        to: { latitude: prev.latitude, longitude: prev.longitude },
+        accuracy: position.coords.accuracy
+      })
+    ) {
+      return curr
+    }
+    return prev
+  })
+}
