4cb478539e0cae2547161e1849efd8f953e915ac
Jasper Nykänen <nykanen@hey.com>
2022-04-13 23:08:14 +0300

    feat: pass props common to layouts

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import type { AppProps as NextAppProps } from 'next/app'
-import type { FC, ReactChild } from 'react'
+import type { LayoutProps } from '@typings/layout_props'
+
+import { useRouter } from 'next/router'
+import { ReactChild } from 'react'
 
 import { Provider } from 'react-redux'
 
 import { store } from '../app/store'
 
+import constants from 'src/constants'
+
 import '../sass/globals.scss'
+import { getLocaleOrThrow } from '@utils/get_locale_or_throw'
 
 interface AppProps extends NextAppProps {
-  Component: NextAppProps['Component'] & { layout?: FC }
+  Component: NextAppProps['Component'] & {
+    layout?: ({ children, layoutProps }: LayoutProps) => JSX.Element
+  }
 }
 
 const provider = (children: ReactChild | ReactChild[]) => {
@@ -16,9 +24,16 @@ const provider = (children: ReactChild | ReactChild[]) => {
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   if (Component.layout) {
+    const layoutProps = {
+      locale: getLocaleOrThrow(router.locale),
+      ...constants
+    }
+
     return provider(
-      <Component.layout>
+      <Component.layout layoutProps={layoutProps}>
         <Component {...pageProps} />
       </Component.layout>
     )
