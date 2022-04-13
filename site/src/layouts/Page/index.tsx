bc9d5c0941da6361fba70f5d5f45fec22d6410b3
Jasper Nykänen <nykanen@hey.com>
2022-04-13 23:10:44 +0300

    feat: add app footer to layout and use layout props

--- site/src/layouts/Page/index.tsx
+++ site/src/layouts/Page/index.tsx
@@ -1,11 +1,13 @@
-import { ReactChild } from 'react'
+import AppFooter from '@components/AppFooter'
+import type { LayoutProps } from '@typings/layout_props'
 
 import styles from './Page.module.scss'
 
-export default function Page({
-  children
-}: {
-  children: ReactChild | ReactChild[]
-}) {
-  return <div className={styles.page}>{children}</div>
+export default function Page({ children, layoutProps }: LayoutProps) {
+  return (
+    <div className={styles.page}>
+      <div className={styles.content}>{children}</div>
+      <AppFooter licenseHtml={layoutProps.LICENSE[layoutProps.locale]} />
+    </div>
+  )
 }
