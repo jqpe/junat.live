204380f7b1b535f67b7bd1b09fb290521dbdd98c
Jasper Nykänen <nykanen@hey.com>
2022-04-13 23:07:28 +0300

    feat: add licenses

--- site/src/constants/index.ts
+++ site/src/constants/index.ts
@@ -1,5 +1,19 @@
 const constants = {
-  SITE_NAME: 'Junat.live'
+  SITE_NAME: 'Junat.live',
+  LICENSE: {
+    fi: `\
+    Liikennetietojen lähde <a href="https://www.fintraffic.fi/fi" target="_blank">Fintraffic</a> /\
+    <a href="https://www.digitraffic.fi/" target="_blank">digitraffic.fi</a>,\
+    lisenssi <a href="https://creativecommons.org/licenses/by/4.0/deed.fi" target="_blank">CC 4.0 BY</a>`,
+    en: `\
+    Traffic data source <a href="https://www.fintraffic.fi/en" target="_blank">Fintraffic</a> /\
+    <a href="https://www.digitraffic.fi/en/" target="_blank">digitraffic.fi</a>,\
+    license <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC 4.0 BY</a>`,
+    sv: `\
+    Trafikdatakälla <a href="https://www.fintraffic.fi/sv" target="_blank">Fintraffic</a> /\
+    <a href="https://www.digitraffic.fi/en/" target="_blank">digitraffic.fi</a>,\
+    licens <a href="https://creativecommons.org/licenses/by/4.0/deed.sv" target="_blank">CC 4.0 BY</a>`
+  }
 }
 
 export default constants
