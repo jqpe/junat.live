b9ead68eb1df5040d376772eeaa18d7d6232d811
Jasper Nykänen <nykanen@hey.com>
2022-04-14 10:31:27 +0300

    fix: catch any errors clear_stale_cache might throw

--- site/scripts/clear_stale_cache.js
+++ site/scripts/clear_stale_cache.js
@@ -2,23 +2,22 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 ;(async () => {
   const cacheDirectory = path.join(process.cwd(), '.cache')
+  try {
+    const files = await fs.readdir(cacheDirectory)
 
-  const files = await fs.readdir(cacheDirectory).catch(error => {
-    console.error("Couldn't read .cache directory", error)
-  })
-
-  if (!files) {
-    return
-  }
-
-  const dateYyyyMmDd = new Date().toISOString().split('T')[0]
-  const dateRegexp = new RegExp(`${dateYyyyMmDd}\\.\\w+$`)
-
-  for (const file of files) {
-    if (!dateRegexp.test(file)) {
-      await fs.rm(path.join(cacheDirectory, file)).catch(error => {
-        console.error(`Couldn't remove file ${file}:\n`, error)
-      })
+    if (!files) {
+      return
     }
-  }
+
+    const dateYyyyMmDd = new Date().toISOString().split('T')[0]
+    const dateRegexp = new RegExp(`${dateYyyyMmDd}\\.\\w+$`)
+
+    for (const file of files) {
+      if (!dateRegexp.test(file)) {
+        await fs.rm(path.join(cacheDirectory, file)).catch(error => {
+          console.error(`Couldn't remove file ${file}:\n`, error)
+        })
+      }
+    }
+  } catch {}
 })()
