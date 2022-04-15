eeec6e305b4de9707c6505c4bd804ae9ad15a1c1
Jasper Nykänen <nykanen@hey.com>
2022-04-15 08:19:50 +0300

    feat: add webmanifest and icons

--- site/src/pages/_document.tsx
+++ site/src/pages/_document.tsx
@@ -1,9 +1,34 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import constants from 'src/constants'
+
 export default function Document() {
   return (
     <Html>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="apple-mobile-web-app-title" content={constants.SITE_NAME} />
+        <meta name="application-name" content={constants.SITE_NAME} />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="theme-color" content="#9100F9" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
