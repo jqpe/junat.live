4d132e3dd461e9fd0d6e5b62073a2c805a84e296
Jasper Nykänen <nykanen@hey.com>
2022-04-16 13:44:50 +0300

    feat: add use color scheme hook

---
+++ site/src/hooks/use_color_scheme.hook.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * Checks the user's color scheme preference with `window.matchMedia` and listens to updates.
+ */
+export default function useColorScheme() {
+  const [colorScheme, setColorScheme] = useState<'light' | 'dark'>('light')
+
+  useEffect(() => {
+    // See https://caniuse.com/matchmedia for supported user agents.
+    if (!window.matchMedia) return
+
+    const matchMedia = window.matchMedia('(prefers-color-scheme: dark)')
+    setColorScheme(matchMedia.matches ? 'dark' : 'light')
+
+    const controller = new AbortController()
+
+    matchMedia.addEventListener(
+      'change',
+      event => {
+        setColorScheme(event.matches ? 'dark' : 'light')
+      },
+      { signal: controller.signal }
+    )
+
+    return function cleanup() {
+      controller.abort()
+    }
+  }, [])
+
+  return { colorScheme, setColorScheme }
+}
