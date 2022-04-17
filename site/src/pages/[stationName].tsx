04757b6d951b060a2d796120cf09dbd66f6a0a39
Jasper Nykänen <nykanen@hey.com>
2022-04-17 18:35:53 +0300

    refactor: |station page| move next server logic to server/lib/pages

--- site/src/pages/[stationName].tsx
+++ site/src/pages/[stationName].tsx
@@ -1,28 +1,14 @@
-import type {
-  GetStaticPathsResult,
-  GetStaticPropsContext,
-  GetStaticPropsResult
-} from 'next'
-import type { ParsedUrlQuery } from 'node:querystring'
-import type { Station, LocalizedStation } from '~digitraffic'
+import type { LocalizedStation } from '~digitraffic'
 import type { StationScreenTranslations } from '@typings/station_screen_translations'
 
 import Head from 'next/head'
 import { useEffect, useMemo, useRef } from 'react'
-
-import { getStationPath } from '~digitraffic'
-
-import { getStations } from '@server/lib/get_stations'
 
 import FetchTrainsButton from '@components/FetchTrainsButton'
 import StationPageHeader from '@components/StationPageHeader'
 import Timetable from '@components/Timetable'
 
 import Page from '@layouts/Page'
-
-import { getLocaleOrThrow } from '@utils/get_locale_or_throw'
-import { interpolateString } from '@utils/interpolate_string'
-import { camelCaseKeys } from '@utils/camel_case_keys'
 
 import { useTrainsQuery } from 'src/features/digitraffic/digitraffic_slice'
 import { useAppDispatch, useAppSelector } from 'src/app/hooks'
@@ -38,9 +24,8 @@ import {
 } from '../features/station_page/station_page_slice'
 import { useRouter } from 'next/router'
 import useLiveTrains from '@hooks/use_live_trains.hook'
-import { sortSimplifiedTrains } from '@utils/sort_simplified_trains'
 
-interface StationPageProps {
+export interface StationPageProps {
   station: LocalizedStation
   translation: StationScreenTranslations
   locale: 'fi' | 'en' | 'sv'
@@ -156,103 +141,4 @@ export default function StationPage({
 
 StationPage.layout = Page
 
-export const getStaticPaths = async (
-  context: GetStaticPropsContext
-): Promise<GetStaticPathsResult> => {
-  let paths: {
-    params: ParsedUrlQuery
-    locale?: string | undefined
-  }[] = []
-
-  if (context.locales) {
-    const supportedLocales = context.locales.filter(locale =>
-      /(fi)|(en)|(sv)/.test(locale)
-    ) as ['fi' | 'en' | 'sv']
-
-    for (const locale of supportedLocales) {
-      paths = [
-        ...paths,
-        ...(
-          await getStations<LocalizedStation[]>({ locale, omitInactive: false })
-        ).map(station => ({
-          params: {
-            stationName: getStationPath(station.stationName[locale]!)
-          },
-          locale
-        }))
-      ]
-    }
-  } else {
-    paths = (await getStations<Station[]>({ omitInactive: false })).map(
-      station => ({
-        params: { stationName: getStationPath(station.stationName) },
-        locale: context.defaultLocale
-      })
-    )
-  }
-
-  return { paths, fallback: false }
-}
-
-export const getStaticProps = async (
-  context: GetStaticPropsContext
-): Promise<GetStaticPropsResult<StationPageProps>> => {
-  const params = context.params
-  const locale = getLocaleOrThrow(context.locale)
-
-  if (
-    !params ||
-    !(params.stationName && typeof params.stationName === 'string')
-  ) {
-    return { notFound: true }
-  }
-  const stations = await getStations<LocalizedStation[]>({
-    locale,
-    omitInactive: false
-  })
-
-  const station = stations.find(
-    station =>
-      getStationPath(station.stationName[locale]!) === params.stationName
-  )
-
-  if (!station) {
-    return { notFound: true }
-  }
-
-  if (!process.env.CMS_TOKEN) {
-    throw new Error('CMS_TOKEN environment variable must be a value.')
-  }
-
-  const headers = new Headers({
-    Authorization: `Bearer ${process.env.CMS_TOKEN}`
-  })
-
-  const response = await fetch(
-    'https://cms.junat.live/items/station_screen_translations',
-    { headers }
-  )
-
-  const json: { data: StationScreenTranslations[] } = await response.json()
-
-  const data = json.data.find(translation => translation.language === locale)
-
-  if (!data) {
-    throw new Error(`Couldn't get translation for ${locale}`)
-  }
-  const content = camelCaseKeys<StationScreenTranslations>(data)
-
-  const translation = Object.assign(content, {
-    title: interpolateString(content.title, {
-      stationName: station.stationName[locale]
-    }),
-    notFound: interpolateString(content.notFound, {
-      stationName: station.stationName[locale]
-    }),
-    description: interpolateString(content.description, {
-      stationName: station.stationName[locale]
-    })
-  })
-
-  return { props: { station, translation, locale } }
-}
+export { getStaticPaths, getStaticProps } from '@server/lib/pages/station'
