ebcba76973bea5fddb174ae31298f79c3ac84fae
Jasper Nykänen <nykanen@hey.com>
2022-04-25 16:36:38 +0300

    feat: update license

--- site/src/constants/index.ts
+++ site/src/constants/index.ts
@@ -2,16 +2,13 @@ const constants = {
   SITE_NAME: 'Junat.live',
   LICENSE: {
     fi: `\
-    Liikennetietojen lähde <a href="https://www.fintraffic.fi/fi" target="_blank">Fintraffic</a> /\
-    <a href="https://www.digitraffic.fi/" target="_blank">digitraffic.fi</a>,\
+    Liikennetietojen lähde <a href="https://www.fintraffic.fi/fi" target="_blank">Fintraffic</a>,\
     lisenssi <a href="https://creativecommons.org/licenses/by/4.0/deed.fi" target="_blank">CC 4.0 BY</a>`,
     en: `\
-    Traffic data source <a href="https://www.fintraffic.fi/en" target="_blank">Fintraffic</a> /\
-    <a href="https://www.digitraffic.fi/en/" target="_blank">digitraffic.fi</a>,\
+    Traffic data source <a href="https://www.fintraffic.fi/en" target="_blank">Fintraffic</a>,\
     license <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC 4.0 BY</a>`,
     sv: `\
-    Trafikdatakälla <a href="https://www.fintraffic.fi/sv" target="_blank">Fintraffic</a> /\
-    <a href="https://www.digitraffic.fi/en/" target="_blank">digitraffic.fi</a>,\
+    Trafikdatakälla <a href="https://www.fintraffic.fi/sv" target="_blank">Fintraffic</a>,\
     licens <a href="https://creativecommons.org/licenses/by/4.0/deed.sv" target="_blank">CC 4.0 BY</a>`
   }
 }
