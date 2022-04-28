0cfd6417beb663ce61c18bd2bda0e1937d1de6cf
Jasper Nykänen <nykanen@hey.com>
2022-04-28 08:38:17 +0300

    style: formatting
    [no ci]

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "apps": [
-        {
-            script: "node_modules/next/dist/bin/next",
-            args: "start",
-            name: "junat.live",
-            interpreter: "none",
-            exec_mode: "cluster",
-            instances: "max",
-            cwd: "./site",
-        }
-    ],
+  apps: [
+    {
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      name: 'junat.live',
+      interpreter: 'none',
+      exec_mode: 'cluster',
+      instances: 'max',
+      cwd: './site'
+    }
+  ]
 }
