2bed2b737da36ce63270e20bd056b845707c6a5b
Jasper Nykänen <nykanen@hey.com>
2022-05-01 21:42:46 +0300

    fix: always sort trains by their scheduled time

--- site/src/utils/sort_simplified_trains.ts
+++ site/src/utils/sort_simplified_trains.ts
@@ -2,9 +2,6 @@ import { SimplifiedTrain } from '@typings/simplified_train'
 
 export const sortSimplifiedTrains = (trains: SimplifiedTrain[]) => {
   return trains.sort((aTrain, bTrain) => {
-    return (
-      +Date.parse(aTrain.liveEstimateTime || aTrain.scheduledTime) -
-      +Date.parse(bTrain.liveEstimateTime || bTrain.scheduledTime)
-    )
+    return Date.parse(aTrain.scheduledTime) - Date.parse(bTrain.scheduledTime)
   })
 }
