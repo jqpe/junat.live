4b64b05465cc8cd80156b9e3b32f2eaee1f2648a
Jasper Nykänen <nykanen@hey.com>
2022-05-02 08:30:18 +0300

    feat: remove server side fetching of trains (rate limits)

--- site/server/lib/pages/train.ts
+++ site/server/lib/pages/train.ts
@@ -1,7 +1,4 @@
 import type { GetServerSidePropsContext } from 'next'
-import type { Train } from '~digitraffic'
-
-import { getSingleTrain } from '~digitraffic'
 
 import { getLocaleOrThrow } from '@utils/get_locale_or_throw'
 import { directus } from '@server/lib/cms/directus'
@@ -11,22 +8,12 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     getLocaleOrThrow(context.locale)
   )
   const departureDate = context.query.date as unknown as string
-
-  const train: Train = await getSingleTrain({
-    date: departureDate,
-    trainNumber: Number(context.query.trainNumber)
-  }).then(trains => trains[0])
-
-  const longName = json.data.find(longName => longName.code === train.trainType)
-
-  if (!longName) {
-    return { notFound: true }
-  }
+  const trainNumber = context.query.trainNumber as unknown as string
 
   return {
     props: {
-      longName,
-      train,
+      longNames: json.data,
+      trainNumber,
       departureDate
     }
   }
