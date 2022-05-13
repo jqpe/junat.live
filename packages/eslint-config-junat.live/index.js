b9ec485c2eade0acf945f3fe310ff44059ef4a94
Jasper Nykänen <nykanen@hey.com>
2022-05-13 17:57:22 +0300

    build(lint): use sonarjs rules

--- packages/eslint-config-junat.live/index.js
+++ packages/eslint-config-junat.live/index.js
@@ -2,10 +2,11 @@ module.exports = {
   extends: [
     'plugin:unicorn/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:sonarjs/recommended',
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'sonarjs'],
   rules: {
     'unicorn/prevent-abbreviations': 'off',
     'unicorn/prefer-number-properties': ['error', { checkInfinity: false }],
