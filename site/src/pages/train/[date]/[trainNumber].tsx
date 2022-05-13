7992ddaae7276d2f719101bab850a60948c09aa4
Jasper Nykänen <nykanen@hey.com>
2022-05-13 07:58:11 +0300

    refactor: move train page nextjs methods to route

--- site/src/pages/train/[date]/[trainNumber].tsx
+++ site/src/pages/train/[date]/[trainNumber].tsx
@@ -1,4 +1,5 @@
 import type { TrainLongName } from '@typings/train_long_name'
+import type { GetServerSidePropsContext } from 'next'
 
 import Head from 'next/head'
 import DefaultError from 'next/error'
@@ -6,11 +7,17 @@ import { useRouter } from 'next/router'
 
 import { useMemo } from 'react'
 
-import constants from 'src/constants'
-import Page from '@layouts/Page'
+import { getTrainLongNames } from '@junat/cms'
+
 import SingleTimetable from '@components/SingleTimetable'
 import useLiveTrain from '@hooks/use_live_train.hook'
 import WebmanifestMeta from '@components/WebmanifestMeta'
+
+import Page from '@layouts/Page'
+
+import { getLocaleOrThrow } from '@utils/get_locale_or_throw'
+
+import constants from 'src/constants'
 
 interface TrainPageProps {
   longNames: TrainLongName[]
@@ -62,4 +69,21 @@ export default function TrainPage({
 
 TrainPage.layout = Page
 
-export { getServerSideProps } from '@server/lib/pages/train'
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const longNames = await getTrainLongNames(getLocaleOrThrow(context.locale))
+  const departureDate = context.query.date as unknown as string
+  const trainNumber = context.query.trainNumber as unknown as string
+
+  context.res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=31536000, stale-while-revalidate'
+  )
+
+  return {
+    props: {
+      longNames,
+      trainNumber,
+      departureDate
+    }
+  }
+}
