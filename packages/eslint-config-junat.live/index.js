583c76f1b0a6563ea21307a05873602a54b8c38f
Jasper Nykänen <nykanen@hey.com>
2022-05-13 14:07:40 +0300

    build: lint file names

--- packages/eslint-config-junat.live/index.js
+++ packages/eslint-config-junat.live/index.js
@@ -2,6 +2,15 @@ module.exports = {
   extends: ['next', 'prettier', 'plugin:unicorn/recommended'],
   rules: {
     'unicorn/prevent-abbreviations': 'off',
-    'unicorn/prefer-number-properties': ['error', { checkInfinity: false }]
+    'unicorn/prefer-number-properties': ['error', { checkInfinity: false }],
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          snakeCase: true,
+          camelCase: true
+        }
+      }
+    ]
   }
 }
