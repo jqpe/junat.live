9fc5240069fcaf7428a2da213f7d5ad707bf24ec
Jasper Nykänen <nykanen@hey.com>
2022-05-17 15:48:06 +0300

    feat: highlight latest station the user clicked
    Before navigating to another screen.

--- site/src/components/TimetableRow/index.tsx
+++ site/src/components/TimetableRow/index.tsx
@@ -3,8 +3,13 @@ import type { SimplifiedTrain } from '@typings/simplified_train'
 
 import { getStationPath } from '~digitraffic'
 import Link from 'next/link'
+import React from 'react'
 
 import { formatTrainTime } from '@utils/format_train_time'
+import { useAppDispatch, useAppSelector } from 'src/app/hooks'
+import { setLastStationId } from 'src/features/station_page/station_page_slice'
+
+import { motion } from 'framer-motion'
 
 export interface TimetableRowProps {
   train: SimplifiedTrain
@@ -24,8 +29,28 @@ export default function TimetableRow({
       : undefined
   }
 
+  const stationId = `${train.scheduledTime}-${train.trainNumber}`
+  const dispatch = useAppDispatch()
+  const lastStationId = useAppSelector(
+    ({ stationPage }) => stationPage.lastStationId
+  )
+
+  const [isLastStation, setIsLastStation] = React.useState(false)
+
+  useRestoreScrollPosition(lastStationId, stationId, setIsLastStation)
+
+  const animate = isLastStation
+    ? {
+        background: ['#c779ff', 'hsla(0, 0%, 0%, 0)']
+      }
+    : {}
+
   return (
-    <tr>
+    <motion.tr
+      data-id={stationId}
+      animate={animate}
+      transition={{ type: 'spring' }}
+    >
       <td>
         <Link href={`/${getStationPath(train.destination[locale])}`}>
           {train.destination[locale]}
@@ -54,9 +79,35 @@ export default function TimetableRow({
             train.trainNumber
           }`}
         >
-          {train.commuterLineID || `${train.trainType}${train.trainNumber}`}
+          <a onClick={() => dispatch(setLastStationId(stationId))}>
+            {train.commuterLineID || `${train.trainType}${train.trainNumber}`}
+          </a>
         </Link>
       </td>
-    </tr>
+    </motion.tr>
   )
+}
+
+/**
+ * Restores the scroll position from cache and if the cache key and the components key match scrolls to the component.
+ */
+function useRestoreScrollPosition(
+  lastStationId: string,
+  stationId: string,
+  setIsLastStation: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  const renders = React.useRef(0)
+
+  React.useEffect(() => {
+    if (lastStationId === stationId && renders.current === 0) {
+      const lastStationElem = document.querySelector(`[data-id="${stationId}"]`)
+      const rect = lastStationElem?.getBoundingClientRect()
+
+      lastStationElem?.scrollIntoView({
+        block: rect && rect.top > window.innerHeight ? 'center' : 'end'
+      })
+      setIsLastStation(true)
+    }
+    renders.current += 1
+  }, [lastStationId, setIsLastStation, stationId])
 }
