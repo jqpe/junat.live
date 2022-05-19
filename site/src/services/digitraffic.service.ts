435cfe78c7d3c44fa2ba0d9fbdedc674d02738b6
Jasper Nykänen <nykanen@hey.com>
2022-05-19 19:15:23 +0300

    feat: add digitraffic service

---
+++ site/src/services/digitraffic.service.ts
@@ -0,0 +1,31 @@
+import type { GetTrainsOptions, LocalizedStation } from '~digitraffic'
+
+import { getLiveTrains, getStations } from '~digitraffic'
+
+import { simplifyTrains } from '@utils/simplify_train'
+
+type FetchDigitrafficProps = {
+  stationShortCode: string
+  localizedStations: LocalizedStation[]
+}
+
+interface FetchDigitrafficWithOptions
+  extends FetchDigitrafficProps,
+    GetTrainsOptions {}
+
+export async function fetchLiveTrains({
+  stationShortCode,
+  localizedStations,
+  ...opts
+}: FetchDigitrafficProps | FetchDigitrafficWithOptions) {
+  const trains = await getLiveTrains(stationShortCode, { ...opts })
+
+  return simplifyTrains(trains, stationShortCode, localizedStations)
+}
+
+export async function fetchStations() {
+  return await getStations<LocalizedStation[]>({
+    omitInactive: false,
+    locale: ['fi', 'en', 'sv']
+  })
+}
