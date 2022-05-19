059833ec4ef5364eb12e23dbe4ed5c19f38e753d
Jasper Nykänen <nykanen@hey.com>
2022-05-19 19:14:55 +0300

    feat: remove rtk query reducers (in favor of react-query)

--- site/src/app/store.ts
+++ site/src/app/store.ts
@@ -1,20 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { digitrafficApi } from 'src/features/digitraffic/digitraffic_slice'
-import { stationsApi } from 'src/features/stations/stations_slice'
 import { stationPage } from 'src/features/station_page/station_page_slice'
 
 export const store = configureStore({
   reducer: {
-    [stationsApi.reducerPath]: stationsApi.reducer,
-    stationPage: stationPage.reducer,
-    [digitrafficApi.reducerPath]: digitrafficApi.reducer
+    stationPage: stationPage.reducer
   },
   middleware: getDefaultMiddleware => {
-    return [
-      ...getDefaultMiddleware(),
-      digitrafficApi.middleware,
-      stationsApi.middleware
-    ]
+    return [...getDefaultMiddleware()]
   },
   devTools: process?.env?.NODE_ENV === 'development'
 })
