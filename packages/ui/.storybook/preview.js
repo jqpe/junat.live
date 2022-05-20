3bc301304d0ea2970e7518d41674b222cce42989
Jasper Nykänen <nykanen@hey.com>
2022-05-20 19:32:03 +0300

    build(ui): better storybook preview experience

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -3,3 +3,13 @@ import './reset.css'
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
 }
+
+export const decorators = [
+  Story => {
+    return (
+      <div style={{ maxWidth: '550px', margin: 'auto' }}>
+        <Story />
+      </div>
+    )
+  }
+]
