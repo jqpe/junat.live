3ff7fe1e1cf248c4e60c28c3fbcfcf05ba8d244f
Jasper Nykänen <nykanen@hey.com>
2022-05-20 19:33:11 +0300

    feat(ui): add timetable component

---
+++ packages/ui/components/Timetable/Timetable.stories.tsx
@@ -0,0 +1,92 @@
+import type { ComponentMeta } from '@storybook/react'
+
+import type { TimetableRowTrain } from '../TimetableRow'
+
+import { Timetable } from '.'
+import { TimetableTranslations } from './Timetable'
+
+const date = new Date()
+const getLiveEstimate = () => {
+  const liveEstimateDate = new Date(date)
+  liveEstimateDate.setMinutes(liveEstimateDate.getMinutes() + 1)
+  return liveEstimateDate
+}
+
+const trains: TimetableRowTrain[] = [
+  {
+    departureDate: date.toISOString(),
+    destination: { fi: 'Helsinki', en: 'Helsinki', sv: 'Helsingfors' },
+    scheduledTime: date.toISOString(),
+    trainNumber: 1,
+    trainType: '',
+    version: 10321031031,
+    commuterLineID: 'R',
+    liveEstimateTime: getLiveEstimate().toISOString(),
+    track: '1'
+  },
+  {
+    departureDate: date.toISOString(),
+    destination: { fi: 'Riihimäki', en: 'Riihimäki', sv: 'Riihimäki' },
+    scheduledTime: date.toISOString(),
+    trainNumber: 125,
+    trainType: 'HDM',
+    version: 10321031031,
+    commuterLineID: undefined,
+    track: '1'
+  }
+]
+
+const translation: Record<'fi' | 'en' | 'sv', TimetableTranslations> = {
+  fi: {
+    train: 'Juna',
+    destination: 'Määränpää',
+    departureTime: 'Lähtöaika',
+    track: 'Raide'
+  },
+  en: {
+    train: 'Train',
+    destination: 'Destination',
+    departureTime: 'Departure time',
+    track: 'Track'
+  },
+  sv: {
+    train: 'Tåg',
+    destination: 'Destination',
+    departureTime: 'Avgångstid',
+    track: 'Spår'
+  }
+}
+
+const Template = (args: ComponentMeta<typeof Timetable>['args']) => {
+  const locale = args.locale || 'fi'
+
+  return (
+    <Timetable
+      lastStationId={`${date.toISOString()}`}
+      setLastStationId={id => alert(id)}
+      locale={locale}
+      trains={trains}
+      translation={translation[locale]}
+      {...args}
+    />
+  )
+}
+
+export const Default = Template.bind({})
+Default.animate = async () => {}
+
+export default {
+  name: 'Timetable/Timetable',
+  component: Timetable,
+  argTypes: {
+    trains: {
+      control: false
+    },
+    translation: {
+      control: false
+    },
+    lastStationId: {
+      control: false
+    }
+  }
+} as ComponentMeta<typeof Timetable>
---
+++ packages/ui/components/Timetable/Timetable.tsx
@@ -0,0 +1,100 @@
+import type {
+  TimetableRowTrain,
+  TimetableRowProps,
+  TimetableRowTranslations
+} from '../TimetableRow'
+
+import { styled } from '@junat/stitches'
+
+import { TimetableRow } from '../TimetableRow'
+
+export interface TimetableTranslations extends TimetableRowTranslations {
+  destination: string
+  departureTime: string
+  track: string
+  train: string
+}
+
+export interface TimetableProps {
+  trains: TimetableRowTrain[]
+  locale: 'fi' | 'en' | 'sv'
+  translation: TimetableTranslations
+
+  lastStationId: TimetableRowProps['lastStationId']
+  setLastStationId: TimetableRowProps['setLastStationId']
+}
+
+const StyledTimetable = styled('table', {
+  textDecoration: 'none',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap'
+})
+
+const StyledTimetableBody = styled('tbody', {
+  display: 'flex',
+  flexDirection: 'column'
+})
+
+const StyledTimetableHead = styled('thead', {
+  fontSize: '$mobile-caption',
+  lineHeight: '$md',
+  '@large': {
+    fontSize: '$pc-caption'
+  },
+  color: '$slateGray700',
+  '@dark': {
+    color: '$slateGray300'
+  }
+})
+
+const StyledTimetableRow = styled('tr', {
+  display: 'grid',
+  gridTemplateColumns: 'min(35%, 30vw) 1fr 0.4fr 0.4fr',
+  gap: '0.5vw'
+})
+
+const CenteredTd = styled('td', {
+  display: 'flex',
+  justifyContent: 'center'
+})
+
+export function Timetable({
+  trains,
+  translation,
+  locale,
+  ...id
+}: TimetableProps) {
+  if (trains.length === 0) {
+    return <></>
+  }
+
+  return (
+    <StyledTimetable>
+      <StyledTimetableHead>
+        <StyledTimetableRow>
+          <td>{translation.destination}</td>
+          <td>{translation.departureTime}</td>
+          <CenteredTd>{translation.track}</CenteredTd>
+          <CenteredTd>{translation.train}</CenteredTd>
+        </StyledTimetableRow>
+      </StyledTimetableHead>
+      <StyledTimetableBody>
+        {trains.map(train => {
+          return (
+            <TimetableRow
+              lastStationId={id.lastStationId}
+              setLastStationId={id.setLastStationId}
+              translation={translation}
+              locale={locale}
+              train={train}
+              key={`${train.trainNumber}-${train.departureDate}.${train.scheduledTime}`}
+            />
+          )
+        })}
+      </StyledTimetableBody>
+    </StyledTimetable>
+  )
+}
