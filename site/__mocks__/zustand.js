925a9d4df84bcfb33fae7ba306fd03a1adb1edf4
Jasper Nykänen <nykanen@hey.com>
2022-05-22 02:51:51 +0300

    tests: add zustand mock

---
+++ site/__mocks__/zustand.js
@@ -0,0 +1,27 @@
+import actualCreate from 'zustand'
+import { act } from 'react-dom/test-utils'
+
+import { afterEach } from 'vitest'
+
+// a variable to hold reset functions for all stores declared in the app
+const storeResetFns = new Set()
+
+// when creating a store, we get its initial state, create a reset function and add it in the set
+const create = createState => {
+  const store = actualCreate(createState)
+  const initialState = store.getState()
+  storeResetFns.add(() => store.setState(initialState, true))
+
+  return store
+}
+
+// Reset all stores after each test run
+afterEach(() => {
+  act(() => {
+    for (const resetFn of storeResetFns) {
+      resetFn()
+    }
+  })
+})
+
+export default create
