afbee0a194adc494ea4d7b33bcf954dee88c75c1
Jasper Nykänen <nykanen@hey.com>
2022-05-22 02:52:02 +0300

    tests: add use station page hook test

---
+++ site/tests/src/hooks/use_station_page.hook.test.ts
@@ -0,0 +1,45 @@
+import { useStationPage } from '../../../src/hooks/use_station_page.hook'
+
+import { describe, it, expect, vi, afterAll, beforeAll } from 'vitest'
+
+import { renderHook, act } from '@testing-library/react-hooks'
+
+describe('use station page', () => {
+  beforeAll(() => {
+    // Resets the state on each import, see __mocks__/zustand.js
+    vi.mock('zustand')
+  })
+
+  const { result: stationPage } = renderHook(() => useStationPage())
+
+  it('starts with empty object by default', () => {
+    // @ts-expect-error stations is an internal value
+    expect(stationPage.current.stations).toStrictEqual({})
+  })
+
+  it('returns zero for undefined key', () => {
+    expect(stationPage.current.getCount('invalid')).toStrictEqual(0)
+  })
+
+  it('can set the count for a key', () => {
+    act(() => {
+      stationPage.current.setCount(1, '/helsinki')
+    })
+
+    expect(stationPage.current.getCount('/helsinki')).toStrictEqual(1)
+  })
+
+  it('can set the count for multiple keys', () => {
+    act(() => {
+      stationPage.current.setCount(2, '/helsinki')
+      stationPage.current.setCount(1, '/ainola')
+    })
+
+    expect(stationPage.current.getCount('/helsinki')).toStrictEqual(2)
+    expect(stationPage.current.getCount('/ainola')).toStrictEqual(1)
+  })
+
+  afterAll(() => {
+    vi.unmock('zustand')
+  })
+})
