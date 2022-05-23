b6d4b38580d669d3189370cd08154111b527964e
Jasper Nykänen <nykanen@hey.com>
2022-05-23 20:24:27 +0300

    tests(digitraffic): add get station path test

---
+++ packages/digitraffic/tests/utils/get_ station_path.test.ts
@@ -0,0 +1,22 @@
+import { getStationPath } from '../../src/utils/get_station_path'
+
+import { it, expect, describe } from 'vitest'
+
+describe('removes non uri-safe characters found in finnish alphabet', () => {
+  it.each([
+    ['ä', 'Järvenpää', 'jarvenpaa'],
+    ['ö', 'Kyrö', 'kyro']
+  ])('%s', (_, actual, expected) => {
+    expect(getStationPath(actual)).toStrictEqual(expected)
+  })
+})
+
+it('replaces spaces with underscore', () => {
+  expect(getStationPath('Helsinki asema')).toStrictEqual('helsinki_asema')
+})
+
+it('replaces spaces and dashes with underscore', () => {
+  expect(getStationPath('Pasila autojuna-asema')).toStrictEqual(
+    'pasila_autojuna_asema'
+  )
+})
