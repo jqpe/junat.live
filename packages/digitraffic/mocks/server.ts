68ba5fd1f760450af2f66adfdb94949b93765d22
Jasper Nykänen <nykanen@hey.com>
2022-05-24 02:51:07 +0300

    tests(digitraffic): add single train mock data

--- packages/digitraffic/mocks/server.ts
+++ packages/digitraffic/mocks/server.ts
@@ -2,6 +2,7 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 
 import { liveTrains } from './live_trains'
+import { train1 } from './single_train'
 import { stations } from './stations'
 
 export const server = setupServer(
@@ -15,6 +16,15 @@ export const server = setupServer(
     'https://rata.digitraffic.fi/api/v1/live-trains/station/:stationShortCode',
     (_req, res, ctx) => {
       return res(ctx.status(200), ctx.json(liveTrains))
+    }
+  ),
+  rest.get(
+    'https://rata.digitraffic.fi/api/v1/trains/:date/:trainNumber',
+    (req, res, ctx) => {
+      const tn = req.params.trainNumber
+      if (typeof tn === 'string' && tn === '1') {
+        return res(ctx.status(200), ctx.json([train1]))
+      }
     }
   )
 )
