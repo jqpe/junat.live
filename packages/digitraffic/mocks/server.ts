6e72a490f27e40e426f2984383a01c0376745947
Jasper Nykänen <nykanen@hey.com>
2022-05-24 01:09:02 +0300

    tests(digitraffic): add live_trains mocks

--- packages/digitraffic/mocks/server.ts
+++ packages/digitraffic/mocks/server.ts
@@ -1,6 +1,7 @@
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 
+import { liveTrains } from './live_trains'
 import { stations } from './stations'
 
 export const server = setupServer(
@@ -8,6 +9,12 @@ export const server = setupServer(
     'https://rata.digitraffic.fi/api/v1/metadata/stations',
     (_req, res, ctx) => {
       return res(ctx.status(200), ctx.json(stations))
+    }
+  ),
+  rest.get(
+    'https://rata.digitraffic.fi/api/v1/live-trains/station/:stationShortCode',
+    (_req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(liveTrains))
     }
   )
 )
