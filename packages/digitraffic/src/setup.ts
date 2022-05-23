b9387267a811b281cc0e2865400dcb9bc759e990
Jasper Nykänen <nykanen@hey.com>
2022-05-24 02:19:23 +0300

    tests(digitraffic): use fetch polyfill

--- packages/digitraffic/src/setup.ts
+++ packages/digitraffic/src/setup.ts
@@ -2,12 +2,7 @@ import { beforeAll, afterEach, afterAll } from 'vitest'
 
 import { server } from '../mocks/server'
 
-import fetch from 'node-fetch'
-
-// see https://github.com/mswjs/interceptors/issues/246
-
-//@ts-expect-error Incompatible types
-globalThis.fetch = fetch
+import './polyfills/fetch'
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterAll(() => server.close())
