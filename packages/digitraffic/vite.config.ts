9095556c106ac5f79648b3789cdad86807f27e14
Jasper Nykänen <nykanen@hey.com>
2022-05-23 10:38:27 +0300

    chore(digitraffic): add coverage options

--- packages/digitraffic/vite.config.ts
+++ packages/digitraffic/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    setupFiles: './src/setup.ts'
+    setupFiles: './src/setup.ts',
+    coverage: {
+      all: true,
+      include: ['src']
+    }
   }
 })
