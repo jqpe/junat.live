52a02ac83f76068cd3ad1b0c2f4d80961492c197
Jasper Nykänen <nykanen@hey.com>
2022-05-25 02:29:39 +0300

    tests(digitraffic): add create handler test

---
+++ packages/digitraffic/tests/base/create_handler.test.ts
@@ -0,0 +1,14 @@
+import { it, expect, vi } from 'vitest'
+
+import { createHandler } from '../../src/base/create_handler'
+
+it('returns the function passed to it', () => {
+  const fn = () => {}
+  expect(createHandler(fn)).toBe(fn)
+})
+
+it("doesn't call the function passed to it", () => {
+  const fn = vi.fn()
+  expect(createHandler(fn)).toBe(fn)
+  expect(fn).toBeCalledTimes(0)
+})
