4d6710a7deb21b730b4f822065f177c1ddce0095
Jasper Nykänen <nykanen@hey.com>
2022-05-25 09:27:15 +0300

    tests(mqtt): add close test

---
+++ packages/digitraffic-mqtt/tests/utils/close.test.ts
@@ -0,0 +1,24 @@
+import { vi, it, expect } from 'vitest'
+
+import { close } from '../../src/utils/close'
+
+it('calls client.end if client is not disconnected already', async () => {
+  const client: any = {
+    disconnected: false,
+    end: vi
+      .fn()
+      .mockImplementation((_force: boolean, cb: (error?: Error) => void) => {
+        cb()
+      })
+  }
+
+  expect(async () => await close(client)).not.toThrow()
+
+  expect(client.end).toHaveBeenCalledOnce()
+})
+
+it('returns true if client is disconnected', async () => {
+  const client: any = { disconnected: true }
+
+  expect(await close(client)).toStrictEqual(true)
+})
