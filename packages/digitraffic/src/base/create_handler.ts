bf3ac8f5f12637e8588ffb61878ecd4841914c73
Jasper Nykänen <nykanen@hey.com>
2022-05-25 17:25:19 +0300

    feat(digitraffic): throw if global fetch is not defined

--- packages/digitraffic/src/base/create_handler.ts
+++ packages/digitraffic/src/base/create_handler.ts
@@ -1,4 +1,14 @@
+/**
+ * Ensures that fetch is defined otherwise halts.
+ * @internal
+ */
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const createHandler = <T extends Function>(fn: T) => {
+  if (!globalThis.fetch) {
+    throw new TypeError(
+      'Fetch is not defined. You can install a polyfill and write to globalThis.fetch or use a later node version (v17-v18).'
+    )
+  }
+
   return fn
 }
