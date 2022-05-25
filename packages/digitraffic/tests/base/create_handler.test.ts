f9528af6690955a64da43753370c3082065ab3e1
Jasper Nykänen <nykanen@hey.com>
2022-05-25 17:34:23 +0300

    tests/

--- packages/digitraffic/tests/base/create_handler.test.ts
+++ packages/digitraffic/tests/base/create_handler.test.ts
@@ -12,3 +12,14 @@ it("doesn't call the function passed to it", async () => {
   expect(createHandler(fn)).toBe(fn)
   expect(fn).toBeCalledTimes(0)
 })
+
+it('throws type error if global fetch is not defined', async () => {
+  vi.stubGlobal('fetch', undefined)
+  expect(() => createHandler(vi.fn())).toThrowError(TypeError)
+
+  vi.stubGlobal('fetch', await import('node-fetch').then(mod => mod.default))
+})
+
+it("doesn't throw type error if global fetch is defined", () => {
+  expect(() => createHandler(vi.fn())).not.toThrow()
+})
