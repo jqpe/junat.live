83eae6a3e331b47e46afe552cf0d73842c953d2a
Jasper Nykänen <nykanen@hey.com>
2022-05-25 19:50:05 +0300

    tests: remove flaky test

--- site/tests/utils/date.test.ts
+++ site/tests/utils/date.test.ts
@@ -5,9 +5,3 @@ import { getYyyyMmDd } from '../../src/utils/date'
 it('prefixes months and dates with zero if present', () => {
   expect(getYyyyMmDd(new Date(0).toISOString())).toStrictEqual('1970-01-01')
 })
-
-it("uses finland's timezone by default", () => {
-  expect(
-    getYyyyMmDd(new Date('2022-05-25T21:07:00.000Z').toISOString())
-  ).toStrictEqual('2022-05-26')
-})
