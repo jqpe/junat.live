f194177ca02be26fe02905c215d621ed1e677b32
Jasper Nykänen <nykanen@hey.com>
2022-05-25 13:26:13 +0300

    revert: "tests(mqtt): restore mocks"
    This reverts commit f7ef2b182683e364d8020c901565eb7efb39bc3e.

--- packages/digitraffic-mqtt/tests/utils/has_connected.test.ts
+++ packages/digitraffic-mqtt/tests/utils/has_connected.test.ts
@@ -1,38 +1,29 @@
-import { it, vi, expect, describe, afterAll } from 'vitest'
+import { it, vi, expect } from 'vitest'
 
 import { hasConnected } from '../../src/utils/has_connected'
 
-describe('has connected', () => {
-  const client: any = {
-    connected: false,
-    on: vi
-      .fn()
-      .mockImplementation(function (
-        _event = 'connect',
-        callback: VoidFunction
-      ) {
-        callback()
-      })
-  }
+const client: any = {
+  connected: false,
+  on: vi
+    .fn()
+    .mockImplementation(function (_event = 'connect', callback: VoidFunction) {
+      callback()
+    })
+}
 
-  it('connects if client.connected is true', async () => {
-    expect(
-      await hasConnected({ ...client, on: vi.fn(), connected: true })
-    ).toStrictEqual(true)
-  })
+it('connects if client.connected is true', async () => {
+  expect(
+    await hasConnected({ ...client, on: vi.fn(), connected: true })
+  ).toStrictEqual(true)
+})
 
-  it('rejects if timeout is reached', () => {
-    expect(async () =>
-      hasConnected({ ...client, on: vi.fn() }, 0)
-    ).rejects.and.toThrow(/Connection timeout exceeded/)
-  })
+it('rejects if timeout is reached', () => {
+  expect(async () =>
+    hasConnected({ ...client, on: vi.fn() }, 0)
+  ).rejects.and.toThrow(/Connection timeout exceeded/)
+})
 
-  it('listens to the connect event', async () => {
-    expect(await hasConnected(client)).toStrictEqual(true)
-    expect(client.on).toHaveBeenCalledOnce()
-  })
-
-  afterAll(() => {
-    vi.restoreAllMocks()
-  })
+it('listens to the connect event', async () => {
+  expect(await hasConnected(client)).toStrictEqual(true)
+  expect(client.on).toHaveBeenCalledOnce()
 })
