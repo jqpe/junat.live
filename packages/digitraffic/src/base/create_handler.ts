743f4f87f0cc3185bf63ec92291cfaf308716d4d
Jasper Nykänen <nykanen@hey.com>
2022-05-26 14:39:47 +0300

    feat(digitraffic): add interface common for all handlers

--- packages/digitraffic/src/base/create_handler.ts
+++ packages/digitraffic/src/base/create_handler.ts
@@ -1,3 +1,12 @@
+export interface HandlerOptions {
+  /**
+   * Signal used to abort the fetch if needed.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/AbortController
+   */
+  signal?: AbortSignal
+}
+
 /**
  * Ensures that fetch is defined otherwise halts.
  * @internal
