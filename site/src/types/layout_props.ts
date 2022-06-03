df534b6817a1a767abe308ecfd9f0c08bda18a7e
Jasper Nykänen <nykanen@hey.com>
2022-06-03 22:35:40 +0300

    refactor: remove deprecated react child type

--- site/src/types/layout_props.ts
+++ site/src/types/layout_props.ts
@@ -1,7 +1,7 @@
 import type constants from 'src/constants'
-import type { ReactChild } from 'react'
+import type { ReactNode } from 'react'
 
 export interface LayoutProps {
-  children: ReactChild | ReactChild[]
+  children: ReactNode | ReactNode[]
   layoutProps: typeof constants & { locale: 'fi' | 'en' | 'sv' }
 }
