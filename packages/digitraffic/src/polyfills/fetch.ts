dd05369d3b7f1727d7edc2c3ad1daac8a3213e11
Jasper Nykänen <nykanen@hey.com>
2022-06-04 23:11:53 +0300

    refactor(digitraffic): ignore overlap ts error

--- packages/digitraffic/src/polyfills/fetch.ts
+++ packages/digitraffic/src/polyfills/fetch.ts
@@ -1,8 +1,8 @@
 import fetch, { Headers, Request, Response } from 'node-fetch'
 
 if (!globalThis.fetch || process.env.NODE_ENV === 'test') {
-  globalThis.fetch = fetch as typeof globalThis.fetch
+  globalThis.fetch = fetch as unknown as typeof globalThis.fetch
   globalThis.Headers = Headers as typeof globalThis.Headers
   globalThis.Request = Request as unknown as typeof globalThis.Request
-  globalThis.Response = Response as typeof globalThis.Response
+  globalThis.Response = Response as unknown as typeof globalThis.Response
 }
