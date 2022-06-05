efeed4c282500b1e13817a64712ff4d646ef12b8
Jasper Nykänen <nykanen@hey.com>
2022-06-05 10:45:32 +0300

    refactor: improve `getNearestStation` readability

--- site/src/utils/get_nearest_station.ts
+++ site/src/utils/get_nearest_station.ts
@@ -2,31 +2,33 @@ import type { LocalizedStation, Station } from '@junat/digitraffic/types'
 
 import getDistance from '@utils/get_distance'
 
+/**
+ * From a list of stations, return the station nearest to position.
+ */
 export default function getNearestStation<T extends LocalizedStation | Station>(
   stations: T[],
   position: Pick<GeolocationPosition, 'coords'>
 ): T {
+  const { latitude, longitude, accuracy } = position.coords
+
   return stations.reduce((prev, curr) => {
-    if (
-      getDistance({
-        from: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        },
-        to: { latitude: curr.latitude, longitude: curr.longitude },
-        accuracy: position.coords.accuracy
-      }) <
-      getDistance({
-        from: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        },
-        to: { latitude: prev.latitude, longitude: prev.longitude },
-        accuracy: position.coords.accuracy
-      })
-    ) {
-      return curr
-    }
-    return prev
+    const prevDistance = getDistance({
+      from: {
+        latitude,
+        longitude
+      },
+      to: { latitude: prev.latitude, longitude: prev.longitude },
+      accuracy
+    })
+    const currDistance = getDistance({
+      from: {
+        latitude,
+        longitude
+      },
+      to: { latitude: curr.latitude, longitude: curr.longitude },
+      accuracy: accuracy
+    })
+
+    return currDistance < prevDistance ? curr : prev
   })
 }
