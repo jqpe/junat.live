16ef71804e1319cb707ef608332b9215ae8a9ca8
Jasper Nykänen <nykanen@hey.com>
2022-06-07 09:36:59 +0300

    feat: rewrite search bar in css-in-js
    - removes box shadow

--- site/src/components/SearchBar/index.tsx
+++ site/src/components/SearchBar/index.tsx
@@ -8,7 +8,61 @@ import type {
 import { createRef } from 'react'
 import Search from '@components/icons/Search.svg'
 
-import styles from './SearchBar.module.scss'
+import { styled } from '@junat/stitches'
+
+const StyledSearchBar = styled('nav', {
+  border: '1px solid $slateGray200',
+  borderRadius: '30px',
+  padding: '4px 16px',
+  marginInline: '-16px',
+  marginBottom: '16px',
+  '@dark': {
+    borderColor: '$slateGray800'
+  }
+})
+
+const Form = styled('form', {
+  display: 'flex',
+  justifyContent: 'space-between'
+})
+
+const SearchIcon = styled(Search, {
+  transition: 'fill 150ms cubic-bezier(0.39, 0.575, 0.565, 1)',
+  fill: '$slateGray500',
+  '&:hover': {
+    fill: '$primary600'
+  },
+  '@dark': {
+    fill: '$slateGray600',
+    '&:hover': {
+      fill: '$primary500'
+    }
+  }
+})
+
+const SubmitButton = styled('button', {
+  outline: '1px solid transparent !important',
+  width: 'fit-content',
+  display: 'flex',
+  transition: 'box-shadow 150ms cubic-bezier(0.39, 0.575, 0.565, 1)',
+  padding: '4px',
+  borderRadius: '30px',
+  '&:focus': {
+    transform: 'scale(1.05)',
+    backgroundColor: '$secondary300',
+    [`& ${SearchIcon}`]: {
+      fill: '$secondary800'
+    }
+  },
+  '@dark': {
+    '&:focus,&:hover': {
+      backgroundColor: '$secondary600',
+      [`& ${SearchIcon}`]: {
+        fill: '$primary100'
+      }
+    }
+  }
+})
 
 export interface SearchBarProps {
   handleChange: (
@@ -31,22 +85,17 @@ export default function SearchBar({
   const inputRef = createRef<HTMLInputElement>()
 
   return (
-    <nav className={styles.searchBar}>
-      <form
+    <StyledSearchBar>
+      <Form
         onFocus={event => handleFocus(event)}
         onChange={event => handleChange(event, inputRef)}
         onSubmit={handleSubmit}
-        className={styles.searchForm}
       >
         <input type="text" ref={inputRef} placeholder={placeholder} />
-        <button
-          type="submit"
-          className={styles.submitButton}
-          aria-label={ariaLabel}
-        >
-          <Search width="24" height="24" className={styles.searchIcon} />
-        </button>
-      </form>
-    </nav>
+        <SubmitButton type="submit" aria-label={ariaLabel}>
+          <SearchIcon width="24" height="24" />
+        </SubmitButton>
+      </Form>
+    </StyledSearchBar>
   )
 }
