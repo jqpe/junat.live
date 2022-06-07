9540ee3e7dc3b1fbab265ea4c1475f9d8842e765
Jasper Nykänen <nykanen@hey.com>
2022-06-07 08:31:33 +0300

    feat(stitches): add alpha channels

---
+++ packages/stitches-config/colors/index.ts
@@ -0,0 +1,3 @@
+export * from './primary'
+export * from './secondary'
+export * from './slate_gray'
---
+++ packages/stitches-config/colors/primary.ts
@@ -0,0 +1,23 @@
+export const primary = {
+  primary100: '#fdfaff',
+  primary200: '#f6eaff',
+  primary300: '#eacdff',
+  primary400: '#d292ff',
+  primary500: '#c779ff',
+  primary600: '#9100f9',
+  primary700: '#7100c2',
+  primary800: '#420071',
+  primary900: '#09000f'
+}
+
+export const primaryA = {
+  primaryA100: 'hsla(275, 100%, 99%, 0.016)',
+  primaryA200: 'hsla(275, 100%, 95.9%, 0.040)',
+  primaryA300: 'hsla(275, 100%, 90.2%, 0.071)',
+  primaryA400: 'hsla(275, 100%, 78.7%, 0.118)',
+  primaryA500: 'hsla(275, 100%, 73.7%, 0.189)',
+  primaryA600: 'hsla(275, 100%, 48.8%, 0.283))',
+  primaryA700: 'hsla(275, 100%, 38%, 0.412)',
+  primaryA800: 'hsla(275, 100%, 22.2%, 0.632)',
+  primaryA900: 'hsla(275, 100%, 3%, 0.980)'
+}
