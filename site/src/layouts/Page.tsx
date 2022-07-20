92f7f793ca6a0fdd00d9ca88aaf2f2ca55d55632
Jasper Nykänen <nykanen@hey.com>
2022-07-20 15:32:39 +0300

    refactor: use css-in-js for page layout

---
+++ site/src/layouts/Page.tsx
@@ -0,0 +1,28 @@
+import Footer from '@components/common/AppFooter'
+import { styled } from '@junat/stitches'
+import type { LayoutProps } from '@typings/layout_props'
+
+const StyledPage = styled('div', {
+  paddingTop: '1.875rem',
+  width: '100%',
+  margin: 'auto',
+  '& header': {
+    marginBottom: '20px'
+  }
+})
+
+const StyledContent = styled('div', {
+  paddingInline: '1.875rem',
+  margin: 'auto',
+  maxWidth: '500px',
+  minHeight: '100vh'
+})
+
+export default function Page({ children, layoutProps }: LayoutProps) {
+  return (
+    <StyledPage>
+      <StyledContent>{children}</StyledContent>
+      <Footer licenseHtml={layoutProps.LICENSE[layoutProps.locale]} />
+    </StyledPage>
+  )
+}
