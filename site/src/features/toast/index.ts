915c606ddefe1e8bb75c1bcfdbf8ceb97e45223c
Jasper Nykänen <nykanen@hey.com>
2022-07-22 03:28:52 +0300

    refactor: add use toast hook to simplify creating new toasts

---
+++ site/src/features/toast/index.ts
@@ -0,0 +1,6 @@
+export { useToast } from './stores/toast'
+
+export { Provider as ToastProvider } from './components/Provider'
+export { Toast } from './components/Toast'
+
+export type { ToastProps } from './components/Toast'
---
+++ site/src/features/toast/stores/toast.ts
@@ -0,0 +1,66 @@
+import type { StoreApi, UseBoundStore } from 'zustand'
+import type { ReactNode } from 'react'
+
+import create from 'zustand'
+
+interface Toast {
+  title: ReactNode
+  /**
+   * Duration in milliseconds > 0
+   *
+   * @default 3000
+   */
+  duration?: number
+  id: string
+}
+
+export interface Store {
+  /**
+   * Clear previous toast and create a new one.
+   */
+  toast: (toast: string | Omit<Toast, 'id'>) => void
+  close: () => void
+  readonly _controller?: AbortController
+  readonly current?: Toast
+}
+
+export const useToast: UseBoundStore<StoreApi<Store>> = create<Store>(
+  (set, get) => ({
+    current: undefined,
+    _controller: new AbortController(),
+    close: () => {
+      // Clear timeout on previous instance, if one exists.
+      get()._controller?.abort?.()
+
+      set(() => ({ current: undefined, _controller: new AbortController() }))
+    },
+    toast: toast => {
+      get().close()
+
+      const id = (Math.random() * 100).toString(36)
+
+      set(() => ({
+        current:
+          typeof toast === 'string' ? { title: toast, id } : { ...toast, id }
+      }))
+
+      const duration =
+        typeof toast === 'string'
+          ? 3000
+          : Math.floor(toast.duration || 0) || 3000
+
+      new Promise((resolve, reject) => {
+        setTimeout(resolve, duration)
+        get()._controller?.signal.addEventListener('abort', reject)
+      })
+        .then(() => {
+          const notClosed = get().current?.id === id
+
+          if (notClosed) {
+            get().close()
+          }
+        })
+        .catch(() => void 0)
+    }
+  })
+)
