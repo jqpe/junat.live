41ea9a35348a4a29a1f1d0d0a1bf658348aededb
Jasper Nykänen <nykanen@hey.com>
2022-07-26 19:05:30 +0300

    build: add vite-plugin-svgr

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), svgr({ exportAsDefault: true })],
   test: {
     environment: 'jsdom',
     setupFiles: ['tests/_setup.ts'],
