6affd7ba3b2db08983ca9b7da592f95668a3c2e5
Jasper Nykänen <nykanen@hey.com>
2022-07-26 18:43:02 +0300

    build: add @vitejs/plugin-react

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     environment: 'jsdom',
     setupFiles: ['tests/_setup.ts'],
