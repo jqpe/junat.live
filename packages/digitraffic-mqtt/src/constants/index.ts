1b8e51959e28e9b3a5b63a404b3ac37b50b3473b
Jasper Nykänen <nykanen@hey.com>
2022-08-15 16:34:25 +0300

    feat(mqtt): use node_env to determine mqtt url

--- packages/digitraffic-mqtt/src/constants/index.ts
+++ packages/digitraffic-mqtt/src/constants/index.ts
@@ -1,1 +1,7 @@
-export const MQTT_URL = 'wss://rata.digitraffic.fi/mqtt'
+export const MQTT_URL = (() => {
+  if ('process' in globalThis && /(test|ci)/i.test(`${process.env.NODE_ENV}`)) {
+    return 'mqtt://127.0.0.1:1883'
+  }
+
+  return 'wss://rata.digitraffic.fi/mqtt'
+})()
