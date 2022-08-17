3dfcd54b4f637ba4b2692adaf1a886433720147e
Jasper Nykänen <nykanen@hey.com>
2022-08-17 20:34:51 +0300

    refactor: move geolocation logic to hook

--- site/src/features/geolocation/hooks/use_geolocation.ts
+++ site/src/features/geolocation/hooks/use_geolocation.ts
@@ -1,24 +1,85 @@
-import { useCallback, useState } from 'react'
+import type { LocalizedStation } from '@junat/digitraffic/types'
+import type { Locale } from '@typings/common'
 
-export function useGeolocation({
-  handleError,
-  handlePosition
-}: {
-  handleError?: PositionErrorCallback
-  handlePosition?: PositionCallback
-}) {
-  const [position, setPosition] = useState<GeolocationPosition>()
+import React from 'react'
 
-  const getCurrentPosition = useCallback(() => {
+import { useRouter } from 'next/router'
+
+import { getStationPath } from '@junat/digitraffic/utils'
+
+import { useToast } from '@features/toast'
+import { useStations } from '@hooks/use_stations'
+
+import { getNearbyStations } from '../utils/get_nearby_stations'
+
+type Translations = {
+  geolocationPositionUnavailableError: string
+  geolocationPositionTimeoutError: string
+  geolocationPositionError: string
+  badGeolocationAccuracy: string
+}
+
+const getError = (
+  error: GeolocationPositionError,
+  translations: Translations
+) => {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      return translations.geolocationPositionError
+
+    case error.POSITION_UNAVAILABLE:
+      return translations.geolocationPositionUnavailableError
+
+    case error.TIMEOUT:
+      return translations.geolocationPositionTimeoutError
+
+    default:
+      return translations.geolocationPositionError
+  }
+}
+
+interface Props {
+  translations: Translations
+  locale: Locale
+  setStations: (stations: LocalizedStation[]) => unknown
+}
+
+/**
+ *
+ */
+export function useGeolocation(props: Props) {
+  const { translations, locale, setStations } = props
+
+  const { data: stations } = useStations()
+  const router = useRouter()
+  const toast = useToast(state => state.toast)
+
+  const getCurrentPosition = React.useCallback(() => {
+    if (typeof stations === 'undefined') {
+      return
+    }
+
     if (typeof window !== 'undefined') {
-      const onSuccess: PositionCallback = sPosition => {
-        setPosition(sPosition)
-        handlePosition?.(sPosition)
+      const onSuccess: PositionCallback = position => {
+        const station = getNearbyStations(position, {
+          locale,
+          stations
+        })
+
+        if (Array.isArray(station)) {
+          setStations(station)
+
+          toast(translations.badGeolocationAccuracy)
+        } else {
+          router.push(getStationPath(station.stationName[locale]))
+        }
       }
 
-      navigator.geolocation.getCurrentPosition(onSuccess, handleError)
+      navigator.geolocation.getCurrentPosition(onSuccess, error => {
+        toast(getError(error, translations))
+      })
     }
-  }, [handleError, handlePosition])
+  }, [locale, router, setStations, stations, toast, translations])
 
-  return { position, getCurrentPosition }
+  return { getCurrentPosition }
 }
