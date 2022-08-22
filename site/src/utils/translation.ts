98c413796383be8b354295ff77ce722ff77a08a6
Jasper Nykänen <nykanen@hey.com>
2022-08-22 15:55:17 +0300

    feat: add translation function

---
+++ site/src/utils/translation.ts
@@ -0,0 +1,55 @@
+import type { Locale } from '@typings/common'
+import constants from '../constants'
+
+type Base = typeof import('../locales/en.json')
+type Rcrd = Record<string, unknown>
+
+/**
+ * @returns a function that can be used to get translated values for `locale`
+ */
+const translate = (locale: Locale | 'all') => {
+  /**
+   * By convention all values requiring interpolation are prefixed with $,
+   * see `locales/en.json` for whats interpolated and `utils/interpolate_string` to see how to interpolate.
+   */
+  return <
+    Key extends keyof Base,
+    D1 extends Base[Key] extends Rcrd ? keyof Base[Key] : never,
+    D2 extends Base[Key][D1] extends Rcrd
+      ? Base[Key][D1] extends Rcrd
+        ? keyof Base[Key][D1]
+        : never
+      : never
+  >(
+    key: Key,
+    depth1?: D1,
+    depth2?: D2
+  ) => {
+    const getLocale = (localeName: string = locale) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires, unicorn/prefer-module
+      const json = require(`../locales/${localeName}.json`)
+
+      if (depth2) {
+        return json[key][depth1][depth2]
+      }
+
+      return depth1 ? json[key][depth1] : json[key]
+    }
+
+    if (locale === 'all') {
+      const locales = constants.LOCALES
+
+      if (!locales) {
+        throw new TypeError('Expected locales to be defined')
+      }
+
+      return Object.fromEntries(
+        locales.map(locale => [locale, getLocale(locale)])
+      )
+    }
+
+    return getLocale()
+  }
+}
+
+export default translate
