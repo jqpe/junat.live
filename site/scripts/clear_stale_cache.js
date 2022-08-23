85165cb57ba968ad9b531eae3ee7b1bfd765feb2
Jasper Nykänen <nykanen@hey.com>
2022-08-23 21:25:50 +0300

    fix: consider cache stale if it doesn't have all locales

--- site/scripts/clear_stale_cache.js
+++ site/scripts/clear_stale_cache.js
@@ -1,5 +1,7 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
+
+import { LOCALES } from '../src/constants/locales.js'
 ;(async () => {
   const cacheDirectory = path.join(process.cwd(), '.cache')
   try {
@@ -13,7 +15,16 @@ import path from 'node:path'
     const dateRegexp = new RegExp(`${calendarDate}\\.\\w+$`)
 
     for (const file of files) {
-      if (!dateRegexp.test(file)) {
+      const contents = await fs.readFile(path.join(cacheDirectory, file), {
+        encoding: 'utf-8'
+      })
+
+      const stationName = JSON.parse(contents)[0].stationName
+
+      if (
+        !dateRegexp.test(file) ||
+        !Object.keys(stationName).every(locale => LOCALES.includes(locale))
+      ) {
         await fs.rm(path.join(cacheDirectory, file)).catch(error => {
           console.error(`Couldn't remove file ${file}:\n`, error)
         })
