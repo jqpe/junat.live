196d8917c64fd58b1e2f445b93c050023ec7349b
Jasper Nykänen <nykanen@hey.com>
2022-09-22 10:23:09 +0300

    refactor: use array and convert it to union
    Converting an union to array is flaky as the position of the unions matters and thus it's not consistent.

--- site/tests/utils/get_train_type.test.ts
+++ site/tests/utils/get_train_type.test.ts
@@ -1,28 +1,10 @@
-import type { Code } from '@utils/get_train_type'
+import type { Codes } from '@utils/get_train_type'
 
 import { getTrainType } from '@utils/get_train_type'
 
 import { expect, it } from 'vitest'
 
 import { LOCALES } from '../../src/constants/locales'
-
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-  k: infer I
-) => void
-  ? I
-  : never
-
-type UnionToOvlds<U> = UnionToIntersection<
-  U extends any ? (f: U) => void : never
->
-
-type PopUnion<U> = UnionToOvlds<U> extends (a: infer A) => void ? A : never
-
-type IsUnion<T> = [T] extends [UnionToIntersection<T>] ? false : true
-
-type UnionToArray<T, A extends unknown[] = []> = IsUnion<T> extends true
-  ? UnionToArray<Exclude<T, PopUnion<T>>, [PopUnion<T>, ...A]>
-  : [T, ...A]
 
 it.each([
   'AE',
@@ -48,7 +30,7 @@ it.each([
   'VEV',
   'VLI',
   'V'
-] as UnionToArray<Code>)('%s works', code => {
+] as Codes)('%s works', code => {
   for (const locale of LOCALES) {
     expect(() => getTrainType(code, locale)).not.toThrow()
   }
