4d9c7db9f829060e07b8d9b7ac1baf11f2ed067a
Jasper Nykänen <nykanen@hey.com>
2022-09-26 11:28:12 +0300

    feat: use global decorator
    Previously a facade was used as the styles needed to be transpiled. Now that they're transpiled beforehand using a global decorator is possible

--- site/.storybook/preview.cjs
+++ site/.storybook/preview.cjs
@@ -1,3 +1,8 @@
+const React = require('react')
+
+const styles = require('@junat/design/dist/styles')
+const { getCssText } = require('@junat/design')
+
 const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -8,4 +13,21 @@ const parameters = {
   }
 }
 
-module.exports = { parameters }
+/**
+ * Includes CSS globals and CSS reset
+ */
+const STYLES_DECORATOR = Story => {
+  styles.global()
+  styles.reset()
+
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('style', {}, getCssText()),
+    Story()
+  )
+}
+
+const decorators = [STYLES_DECORATOR]
+
+module.exports = { parameters, decorators }
