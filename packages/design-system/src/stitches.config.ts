76f230500401499c064f891b01e872278a04da3c
Jasper Nykänen <nykanen@hey.com>
2022-09-26 15:11:45 +0300

    fix: stitches types not inferred
    Use of Node16 module resolution caused @stitches/react types to not be inferred.

--- packages/design-system/src/stitches.config.ts
+++ packages/design-system/src/stitches.config.ts
@@ -1,11 +1,5 @@
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
-import type * as _Stitches from '../node_modules/@stitches/react/types/stitches.js'
-import type * as _StyledComponent from '../node_modules/@stitches/react/types/styled-component.js'
-import type * as _Util from '../node_modules/@stitches/react/types/util.js'
-import type * as _CssUtil from '../node_modules/@stitches/react/types/css-util.js'
-import type * as _Theme from '../node_modules/@stitches/react/types/theme.js'
-
 import {
   primary,
   primaryA,
