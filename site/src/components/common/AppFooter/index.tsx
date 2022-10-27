1b251028ea9249f165cdcc0207df0e4e43be3d6a
Jasper Nykänen <nykanen@hey.com>
2022-10-27 17:02:50 +0300

    refactor: separate styles from app footer component

---
+++ site/src/components/common/AppFooter/index.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from 'next/router'
+
+import translate from '@utils/translate'
+import { getLocale } from '@utils/get_locale'
+
+import { FINTRAFFIC } from '@constants'
+
+import dynamic from 'next/dynamic'
+
+import { StyledFooter } from './styles'
+import React from 'react'
+
+const LanguageSelect = dynamic(() => import('@components/LanguageSelect'))
+
+const Anchor = (
+  props: React.PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement>>
+) => (
+  <a {...props} target="_blank" rel="noreferrer">
+    {props.children}
+  </a>
+)
+
+export default function AppFooter() {
+  const router = useRouter()
+  const locale = getLocale(router.locale)
+
+  const t = translate(locale)
+
+  const path = locale === 'fi' || locale === 'sv' ? locale : 'en'
+
+  return (
+    <StyledFooter>
+      <section>
+        <LanguageSelect router={router} />
+      </section>
+      <section>
+        <small>
+          {`${t('trafficDataSource')} `}
+          <Anchor href={`${FINTRAFFIC.URL}${path}`}>Fintraffic</Anchor>{' '}
+          {`${t('license')} `}
+          <Anchor href={`${FINTRAFFIC.LICENSE_URL}${locale}`}>
+            {FINTRAFFIC.LICENSE}
+          </Anchor>
+        </small>
+      </section>
+    </StyledFooter>
+  )
+}
---
+++ site/src/components/common/AppFooter/styles.tsx
@@ -0,0 +1,49 @@
+import { styled } from '@junat/design'
+
+const HIGH_CONTRAST = '$slateGray900' as const
+const ACCENT = '$slateGray800' as const
+
+const backgroundImage = [
+  linearGradient(45, HIGH_CONTRAST, '25%', 'transparent', '20%'),
+  linearGradient(-45, HIGH_CONTRAST, '25%', 'transparent', '25%'),
+  linearGradient(45, 'transparent', '75%', HIGH_CONTRAST, '75%'),
+  linearGradient(-45, 'transparent', '75%', HIGH_CONTRAST, '75%'),
+  linearGradient(-45, 'transparent', '', ACCENT, '25%'),
+  linearGradient(-45, 'transparent', '', HIGH_CONTRAST, '15%')
+].join(',')
+
+export const StyledFooter = styled('footer', {
+  marginTop: '3rem',
+  padding: '1rem 5vw',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
+  right: 0,
+  left: 0,
+  backgroundColor: '$slateGray700',
+  color: '$slateGray500',
+  fontSize: '$mobile-caption',
+  '@large': {
+    fontSize: '$pc-caption'
+  },
+  background: 'linear-gradient($slateGray900, $slateGray900)',
+  backgroundImage,
+  backgroundSize: '15px 5px',
+  backgroundPosition: '0 0, 0 10px, 10px -10px, -10px 0px, -10px 0, 10px 12px',
+  '& a': {
+    color: '$primary500',
+    '&:hover,&:focus': {
+      color: '$primary200'
+    }
+  }
+})
+
+function linearGradient(
+  d: number,
+  color: string,
+  colorPos: string,
+  color2: string,
+  colorPos2: string
+) {
+  return `linear-gradient(${d}deg, ${color} ${colorPos}, ${color2} ${colorPos2})`
+}
