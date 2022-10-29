264826a84cb883849f372b5b8586eaf42d192ce0
Jasper Nykänen <nykanen@hey.com>
2022-10-29 18:43:38 +0300

    refactor: separate styles, lazy load footer

---
+++ site/src/layouts/page/styles.tsx
@@ -0,0 +1,14 @@
+import { styled } from '@junat/design'
+
+export const StyledPage = styled('div', {
+  paddingTop: '1.875rem',
+  width: '100%',
+  margin: 'auto'
+})
+
+export const StyledContent = styled('div', {
+  paddingInline: '1.875rem',
+  margin: 'auto',
+  maxWidth: '500px',
+  minHeight: '100vh'
+})
