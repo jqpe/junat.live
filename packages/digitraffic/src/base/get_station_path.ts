c3937b3b19dd6cbfe08efe0f7ccf9125b676a55e
Jasper Nykänen <nykanen@hey.com>
2022-11-20 18:37:22 +0200

    refactor: move get_station_path to base

---
+++ packages/digitraffic/src/base/get_station_path.ts
@@ -0,0 +1,10 @@
+/**
+ * Gets an uri safe path from a station name by removing characters such as ä, ö, dashes and whitespace.
+ */
+export const getStationPath = (stationName: string) => {
+  return stationName
+    .toLowerCase()
+    .replace(/ä/g, 'a')
+    .replace(/[åö]/g, 'o')
+    .replace(/\s|-/g, '_')
+}
