b02d86cf3f2cf689df3fb0ff03f77fcde28b0c21
Jasper Nykänen <nykanen@hey.com>
2022-11-26 22:36:00 +0200

    feat: add head component

--- site/src/components/common/head/helpers.tsx
+++ site/src/components/common/head/helpers.tsx
@@ -1,9 +1,27 @@
 import type { Locale } from '~/types/common'
 
-import { SITE_VALID_URL } from '~/constants'
+import { SITE_FULL_URL } from '~/constants'
 
-export const fullUrl = (path: string, locale?: Locale) => {
-  const pathWithLocale = [locale, path].join('')
+export const fullUrl = (
+  path: string,
+  args?: {
+    locale?: Locale
+    currentLocale?: Locale
+    replace?: Record<Locale, Record<string, string>>
+  }
+) => {
+  let pathWithLocale = [args?.locale, path].join('')
 
-  return new URL(pathWithLocale, SITE_VALID_URL).toString()
+  if (args?.replace && args.locale && args.currentLocale) {
+    const elements = args.replace[args.locale]
+
+    for (const key of Object.keys(elements)) {
+      pathWithLocale = pathWithLocale.replace(
+        args.replace[args.currentLocale][key],
+        elements[key]
+      )
+    }
+  }
+
+  return new URL(pathWithLocale, SITE_FULL_URL).toString()
 }
--- site/src/components/common/head/index.tsx
+++ site/src/components/common/head/index.tsx
@@ -12,19 +12,53 @@ interface Props {
 
   children?: ReactNode | ReactNode[]
   replace?: Record<Locale, Record<string, string>>
+  locale?: Locale
 }
 
+/**
+ * Inject elements into `<head>` by passing props or children.
+ *
+ * ---
+ * ## Alternate links
+ *
+ * Adds `<link rel="alternate"/>` -elements by leveraging the {@link DEFAULT_LOCALE} and {@link LOCALES} constants.
+ * `DEFAULT_LOCALE` will be "x-default" and other locales are considered alternative.
+ * ---
+ *
+ * ## Replace localized path segments
+ *
+ * Some paths might have localizations. In this case, you can pass `replace` and `locale` parameters.
+ * - `replace` contains all possible translations in the form of: `Record<Locale, Record<string, string>>`.
+ * - `locale` is the current locale; it's used to determine the string to replace.
+ *
+ * @example
+ * ```ts
+ * {
+ *   replace: { [DEFAULT_LOCALE]: { train: 'juna' }, en: { train: 'train' } }, /// ...
+ *   locale: DEFAULT_LOCALE,
+ * }
+ * ```
+ *
+ * Above, `path` is expected to have string 'juna' somewhere in it. For `x-default` it will remain unchanged, but for 'en' alternative it will be replaced with 'train'.
+ * E.g. `/juna/1` would become `/train/1`. Note that only values specified in {@link LOCALES} will work.
+ *
+ * ---
+ */
 export const Head = (props: Props) => {
-  const alternateLocales = LOCALES.filter(locale => locale !== DEFAULT_LOCALE)
+  const alternativeLocales = LOCALES.filter(locale => locale !== DEFAULT_LOCALE)
 
   return (
     <NextHead>
-      {alternateLocales.map(locale => (
+      {alternativeLocales.map(locale => (
         <link
           key={locale}
           rel="alternate"
           hrefLang={locale}
-          href={fullUrl(props.path, locale)}
+          href={fullUrl(props.path, {
+            locale,
+            currentLocale: props.locale,
+            replace: props.replace
+          })}
         />
       ))}
       <link rel="alternate" hrefLang="x-default" href={fullUrl(props.path)} />
