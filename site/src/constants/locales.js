e12ef403ebc8e6afa6aa579199e8e79aaabbd04c
Jasper Nykänen <nykanen@hey.com>
2022-11-26 18:51:19 +0200

    refactor: add routes table constant

--- site/src/constants/locales.js
+++ site/src/constants/locales.js
@@ -1,4 +1,21 @@
+// @ts-check
+
 /**
  * @type {readonly ["fi", "en", "sv"]}
  */
 export const LOCALES = ['fi', 'en', 'sv']
+
+/**
+ * @type {Record<typeof LOCALES[number], Record<import("@typings/common").Routes, string>>}
+ */
+export const ROUTES = {
+  fi: {
+    train: 'juna'
+  },
+  en: {
+    train: 'train'
+  },
+  sv: {
+    train: 'tog'
+  }
+}
