5b112f4b492523591db29ccfc1f32bf005072149
Jasper Nykänen <nykanen@hey.com>
2022-11-27 08:26:11 +0200

    feat: handle edge case
    If locale was defined and path didn't start with a slash the string returned would look like <locale><path> and not <locale>/<path>.
    
    Add documentation, rename variables.

--- site/src/components/common/head/helpers.ts
+++ site/src/components/common/head/helpers.ts
@@ -2,26 +2,56 @@ import type { Locale } from '~/types/common'
 
 import { SITE_FULL_URL } from '~/constants'
 
+/**
+ * Prefixes a path with {@link SITE_FULL_URL} and optionally `locale`
+ *
+ * @returns
+ * - A full url with `path` prefixed with `locale` if given as an argument
+ * - A full url with `path` with word(s) replaced according to `replace` if `currentLocale` and `locale` are also defined
+ */
 export const fullUrl = (
   path: string,
-  args?: {
+  {
+    locale,
+    currentLocale,
+    replace
+  }: {
+    /**
+     * Will be used to prefix path
+     */
     locale?: Locale
+    /**
+     * Locale present in `path` if `path` is a localized string. Only needed with `replace`
+     */
     currentLocale?: Locale
+    /**
+     * If `currentLocale` and `locale` are defined, replace words defined in `replace` if they are found in `path`
+     *
+     * `path` is expected to be a localized string with `currentLocale` the locale it's written in.
+     *
+     * @example
+     * ```ts
+     * fullUrl("/x", { currentLocale: "fi", locale: "en", replace: {fi: {}, en: {x: "y"}, sv: {}}})
+     * // -> <url>/en/y
+     * ```
+     */
     replace?: Record<Locale, Record<string, string>>
-  }
+  } = {}
 ) => {
-  let localePrefixedPath = [args?.locale, path].join('')
+  let maybeLocalePrefixedPath = [locale, path].join(
+    locale && !path.startsWith('/') ? '/' : ''
+  )
 
-  if (args?.replace && args.locale && args.currentLocale) {
-    const elements = args.replace[args.locale]
+  if (replace && currentLocale && locale) {
+    const elements: Record<string, string> = replace[locale]
 
     for (const key of Object.keys(elements)) {
-      localePrefixedPath = localePrefixedPath.replace(
-        args.replace[args.currentLocale][key],
+      maybeLocalePrefixedPath = maybeLocalePrefixedPath.replace(
+        replace[currentLocale][key],
         elements[key]
       )
     }
   }
 
-  return new URL(localePrefixedPath, SITE_FULL_URL).toString()
+  return new URL(maybeLocalePrefixedPath, SITE_FULL_URL).toString()
 }
