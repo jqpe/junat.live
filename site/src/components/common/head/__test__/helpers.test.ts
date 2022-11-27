4a92b1fb34c3eb247e77d08defd66703837516ad
Jasper Nykänen <nykanen@hey.com>
2022-11-27 08:26:24 +0200

    test: add head helpers test

---
+++ site/src/components/common/head/__test__/helpers.test.ts
@@ -0,0 +1,30 @@
+import { SITE_FULL_URL } from '~/constants'
+import { it, expect } from 'vitest'
+
+import { fullUrl } from '../helpers'
+
+it('joins site_full_url with path (string without slash)', () => {
+  expect(fullUrl('path')).toStrictEqual(`${SITE_FULL_URL}/path`)
+})
+
+it('joins site_full_url with path (string _with_ slash)', () => {
+  expect(fullUrl('/path')).toStrictEqual(`${SITE_FULL_URL}/path`)
+})
+
+it('replaces words according to `replace` argument', () => {
+  expect(
+    fullUrl('/x', {
+      currentLocale: 'fi',
+      locale: 'en',
+      replace: { fi: { x: 'x' }, en: { x: 'y' }, sv: {} }
+    })
+  ).toStrictEqual(`${SITE_FULL_URL}/en/y`)
+})
+
+it('has slash if locale is defined', () => {
+  expect(
+    fullUrl('x', {
+      locale: 'en'
+    })
+  ).toStrictEqual(`${SITE_FULL_URL}/en/x`)
+})
