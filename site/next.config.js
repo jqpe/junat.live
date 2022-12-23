9281ce2dc9bac38f32c4021437ab765ae67961a6
Jasper Nykänen <nykanen@hey.com>
2022-12-23 22:51:31 +0200

    fix: wrong type

--- site/next.config.js
+++ site/next.config.js
@@ -67,6 +67,7 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true'
 })
 
+/** @type {import('next').NextConfig} */
 export default withSentryConfig(withPwa(withBundleAnalyzer(nextConfig)), {
   silent: true
 })
