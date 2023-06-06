8dd6b39cc4f6c64edfcece3000f6645aba273e4b
Jasper Nykänen <nykanen@hey.com>
2023-06-06 21:35:31 +0300

    revert: remove unrelated change

--- site/src/components/errors/digitraffic/styles.tsx
+++ site/src/components/errors/digitraffic/styles.tsx
@@ -103,7 +103,7 @@ export const StyledListItem = styled('li', {
 export const StyledButton = styled('button', {
   background: '$primary900',
   color: '$primary100',
-  padding: '$xxs $s',
+  padding: '$xss $s',
   borderRadius: '9999px',
   '&:hover': {
     cursor: 'pointer'
