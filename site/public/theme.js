704f17c0b386f9f08e1e358e9e82b423b1c75023
Jasper Nykänen <nykanen@hey.com>
2023-06-23 14:20:47 +0300

    feat: add class if media query matches when mode is auto

--- site/public/theme.js
+++ site/public/theme.js
@@ -1,10 +1,20 @@
-const preferences = window.localStorage.getItem('preferences')
-const localTheme = JSON.parse(preferences).state.theme
+try {
+  const preferences = window.localStorage.getItem('preferences')
+  const localTheme = JSON.parse(preferences).state.theme
 
-if (localTheme === 'light' || localTheme === 'auto') {
-  document.documentElement.classList.remove('dark')
-}
+  if (localTheme === 'light') {
+    document.documentElement.classList.remove('dark')
+  }
 
-if (localTheme === 'dark') {
-  document.documentElement.classList.add('dark')
-}
+  if (localTheme === 'dark') {
+    document.documentElement.classList.add('dark')
+  }
+
+  if (localTheme === 'auto') {
+    const action = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'add'
+      : 'remove'
+
+    document.documentElement.classList[action]('dark')
+  }
+} catch {}
