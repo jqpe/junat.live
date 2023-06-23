26705bd5a543c11109efd5a1019f2aa25920c687
Jasper Nykänen <nykanen@hey.com>
2023-06-23 13:38:57 +0300

    refactor: create link component rather than relying on global styles

--- packages/design-tokens/src/styles/global.ts
+++ packages/design-tokens/src/styles/global.ts
@@ -35,16 +35,6 @@ export const global = globalCss({
     outlineOffset: '$xxs'
   },
 
-  a: {
-    textDecoration: 'underline',
-    color: '$slateGray800',
-    cursor: 'pointer',
-    '&:hover,&:focus': {
-      color: '$primary600',
-      transition: 'color 150ms cubic-bezier(0.075, 0.82, 0.165, 1)'
-    }
-  },
-
   '::selection': {
     color: '$primary300',
     backgroundColor: '$slateGray800'
@@ -52,12 +42,9 @@ export const global = globalCss({
 
   ':root.dark': {
     body: {
+      colorScheme: 'dark',
       backgroundColor: '$slateGray900',
       color: '$slateGray100'
-    },
-
-    a: {
-      color: '$slateGray200'
     }
   }
 })
