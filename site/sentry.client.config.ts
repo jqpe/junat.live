4b2fac7037e0cf7caf182ec248e4a26b593e5ae3
Jasper Nykänen <nykanen@hey.com>
2023-09-18 11:26:06 +0300

    feat: remove session replays

--- site/sentry.client.config.ts
+++ site/sentry.client.config.ts
@@ -3,13 +3,5 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1,
-  debug: false,
-  replaysOnErrorSampleRate: 1,
-  replaysSessionSampleRate: process.env.NODE_ENV === 'development' ? 1 : 0.1,
-  integrations: [
-    new Sentry.Replay({
-      maskAllText: true,
-      blockAllMedia: true
-    })
-  ]
+  debug: false
 })
