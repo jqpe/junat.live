47d19a1979ada15777c643d65f9c40ef166d5182
Jasper Nykänen <nykanen@hey.com>
2023-09-18 12:12:12 +0300

    refactor: remove sentry e2e pages

--- site/src/pages/api/sentry-example-api.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable unicorn/filename-case*/
-
-// A faulty API route to test Sentry's error monitoring
-export default function handler() {
-  throw new Error('Sentry Example API Route Error')
-}
