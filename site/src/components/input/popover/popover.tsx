364b32f1dac81213193400616bfdcffcf7839cb6
Jasper Nykänen <nykanen@hey.com>
2023-09-19 14:41:06 +0300

    feat: add popover component

---
+++ site/src/components/input/popover/popover.tsx
@@ -0,0 +1,46 @@
+import * as P from '@radix-ui/react-popover'
+import React, { ReactNode } from 'react'
+
+import {
+  CirclesHorizontal,
+  Close,
+  Flex,
+  IconButton,
+  PopoverArrow,
+  PopoverClose,
+  PopoverContent,
+  Text
+} from './styles'
+
+type Props = {
+  label: string
+  children: ReactNode | ReactNode[]
+}
+
+export const Popover = (props: Props) => {
+  return (
+    <P.Root>
+      <P.Trigger asChild>
+        <IconButton aria-label="Update dimensions">
+          <CirclesHorizontal />
+        </IconButton>
+      </P.Trigger>
+      <P.Portal>
+        <PopoverContent
+          sideOffset={5}
+          // Disables focus moving to trigger when closing popover
+          onCloseAutoFocus={event => event.preventDefault()}
+        >
+          <Flex css={{ flexDirection: 'column', gap: 10 }}>
+            <Text css={{ marginBottom: 10 }}>{props.label}</Text>
+            {props.children}
+          </Flex>
+          <PopoverClose aria-label="Close">
+            <Close />
+          </PopoverClose>
+          <PopoverArrow />
+        </PopoverContent>
+      </P.Portal>
+    </P.Root>
+  )
+}
