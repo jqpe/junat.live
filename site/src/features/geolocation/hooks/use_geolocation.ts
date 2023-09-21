25fe09edab6f58c9cc4fe88c31af549359ca66cf
Jasper Nykänen <nykanen@hey.com>
2023-09-21 12:55:05 +0300

    refactor: rewrite digitraffic import

--- site/src/features/geolocation/hooks/use_geolocation.ts
+++ site/src/features/geolocation/hooks/use_geolocation.ts
@@ -5,7 +5,7 @@ import React from 'react'
 
 import { useRouter } from 'next/router'
 
-import { getStationPath } from '@junat/digitraffic'
+import { getStationPath } from '~/lib/digitraffic'
 
 import { useToast } from '@features/toast'
 import { useStations } from '@hooks/use_stations'
