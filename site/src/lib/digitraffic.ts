be4d6f6d61ef415b21b904905d5a84a9b8f9fb10
Jasper Nykänen <nykanen@hey.com>
2023-09-21 12:46:44 +0300

    refactor: move to lib/digitraffic/types.ts

--- site/src/lib/digitraffic.ts
+++
@@ -1,4 +0,0 @@
-import type { Locale } from '@typings/common'
-import type { LocalizedStation as ILocalizedStation } from '@junat/digitraffic/types'
-
-export type LocalizedStation = ILocalizedStation<Locale, true>
