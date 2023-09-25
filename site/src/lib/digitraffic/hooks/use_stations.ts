5d5ea1b9d2bbfcd6cdd379672efee9883d4983a7
Jasper Nykänen <nykanen@hey.com>
2023-09-25 10:09:14 +0300

    feat: use rest for stations
    - browser caching
    - service worker caching
    
    Using GraphQL does not make sense for static data. Changes with the production branch:
    
    - Don't include non passenger stations

--- site/src/lib/digitraffic/hooks/use_stations.ts
+++ site/src/lib/digitraffic/hooks/use_stations.ts
@@ -1,40 +1,27 @@
 import type { LocalizedStation } from '@lib/digitraffic'
+import type { GetStationsOptions } from '@junat/digitraffic'
 
+import { fetchStations } from '@junat/digitraffic'
 import { useQuery } from '@tanstack/react-query'
 
-import translate from '~/utils/translate'
+import translate from '@utils/translate'
 
-import { stations } from '../queries/stations'
-import { translateStations, normalizedStations } from '../helpers/station'
-import { client } from '../helpers/graphql_request'
+import { INACTIVE_STATIONS } from 'src/constants'
 
-export const useStations = () => {
+export const useStations = (opts?: GetStationsOptions) => {
   return useQuery<LocalizedStation[]>(
     ['stations '],
-    async () => {
-      const result = await client.request(stations)
-
-      if (!result.stations) {
-        throw new TypeError('stations should not be undefined')
-      }
-
-      type NonNullStations = NonNullable<(typeof result.stations)[number]>[]
-
-      return translateStations(
-        normalizedStations(
-          <NonNullStations>result.stations.filter(station => {
-            if (station === null) {
-              throw new TypeError('station was null')
-            }
-
-            return station
-          })
-        ),
-        translate('all')('stations')
-      )
+    () => {
+      return fetchStations({
+        inactiveStations: INACTIVE_STATIONS,
+        betterNames: true,
+        i18n: translate('all')('stations'),
+        proxy: true,
+        ...opts
+      })
     },
     {
-      staleTime: Infinity
+      cacheTime: Infinity
     }
   )
 }
