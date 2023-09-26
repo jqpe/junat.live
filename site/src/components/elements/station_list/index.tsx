06ba28f312f1703d002d044c0c7b37ff0b9f33e7
Jasper Nykänen <nykanen@hey.com>
2023-09-26 15:35:51 +0300

    refactor: remove import of large interface
    contains unused properties making it more difficult to use the component

--- site/src/components/elements/station_list/index.tsx
+++ site/src/components/elements/station_list/index.tsx
@@ -1,5 +1,3 @@
-import type { LocalizedStation } from '@lib/digitraffic'
-
 import { getStationPath } from '~/lib/digitraffic'
 
 import { Locale } from '@typings/common'
@@ -7,7 +5,7 @@ import { Locale } from '@typings/common'
 import Link from 'next/link'
 
 interface StationListProps {
-  stations: LocalizedStation[]
+  stations: { stationShortCode: string; stationName: Record<Locale, string> }[]
   locale: Locale
 }
 
