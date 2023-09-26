b76b763831ec37ddd41f379c341b84caf0245f7b
Jasper Nykänen <nykanen@hey.com>
2023-09-26 15:59:24 +0300

    refactor(digitraffic): add error typings

--- packages/digitraffic/src/base/classes/digitraffic_error.ts
+++ packages/digitraffic/src/base/classes/digitraffic_error.ts
@@ -1,14 +1,35 @@
-/**
- * @internal
- */
 type DigitrafficErrorOpts = {
+  /**
+   * The requested path
+   */
   path: string
+  /**
+   * `Response.status`
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Response/status
+   */
   status: number
+  /**
+   * `Response.statusText`
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Response/statusText
+   */
   statusText: string
+  /**
+   * `Response.type`
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Response/type
+   */
   type: string
+  /**
+   * Body as text
+   */
   body?: string
 }
 
+/**
+ * Error class thrown by all handler functions when request failed for any reason
+ */
 export class DigitrafficError implements DigitrafficErrorOpts {
   readonly path
   readonly body
