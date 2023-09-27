96506df3ee118fe20a6427a1beb8416ddd65df62
Jasper Nykänen <nykanen@hey.com>
2023-09-27 15:44:54 +0300

    feat(sb): add header story
    skip-deploy

---
+++ site/src/components/common/header/header.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryFn } from '@storybook/react'
+import type { ComponentPropsWithoutRef } from 'react'
+
+import { Header } from './'
+
+type Props = ComponentPropsWithoutRef<typeof Header>
+
+export const Default: StoryFn<Props> = args => {
+  return <Header {...args}></Header>
+}
+
+export default {
+  component: Header,
+  args: {
+    heading: 'Header'
+  }
+} satisfies Meta<typeof Header>
