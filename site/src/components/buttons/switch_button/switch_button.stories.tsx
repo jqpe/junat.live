889984596c0dacfddec9402071857090ed852a60
Jasper Nykänen <nykanen@hey.com>
2023-09-27 20:43:07 +0300

    feat(sb): add switch button story

---
+++ site/src/components/buttons/switch_button/switch_button.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryFn } from '@storybook/react'
+import type { ComponentPropsWithoutRef } from 'react'
+
+import { SwitchButton } from './'
+import HeartFilled from '~/components/icons/heart_filled.svg'
+import HeartOutline from '~/components/icons/heart_outline.svg'
+import React from 'react'
+
+type Props = ComponentPropsWithoutRef<typeof SwitchButton>
+
+export const Default: StoryFn<Props> = args => {
+  const [checked, setChecked] = React.useState(args.checked)
+
+  return (
+    <SwitchButton {...args} checked={checked} onCheckedChange={setChecked} />
+  )
+}
+
+export default {
+  component: SwitchButton,
+  args: {
+    checked: false,
+    children: [HeartOutline(), HeartFilled()],
+    disabled: false
+  },
+  argTypes: {
+    ...Object.fromEntries(
+      ['children', 'id', 'onCheckedChange', 'checked'].map(key => [
+        key,
+        { table: { disable: true } }
+      ])
+    ),
+
+    disabled: {
+      defaultValue: false,
+      type: 'boolean'
+    }
+  }
+} satisfies Meta<Props>
