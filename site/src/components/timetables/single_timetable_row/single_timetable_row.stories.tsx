717a9fdb395ccc4df951425edb1bc88b5ddbe06b
Jasper Nykänen <nykanen@hey.com>
2023-09-28 22:54:58 +0300

    feat: update story with cancelled and late states

--- site/src/components/timetables/single_timetable_row/single_timetable_row.stories.tsx
+++ site/src/components/timetables/single_timetable_row/single_timetable_row.stories.tsx
@@ -1,28 +1,75 @@
-import { Meta, StoryFn } from '@storybook/react'
+import { Meta, StoryObj, StoryFn } from '@storybook/react'
 
 import { SingleTimetableRow, SingleTimetableRowProps } from './index'
 import translate from '~/utils/translate'
 
-export const Default: StoryFn<SingleTimetableRowProps> = args => {
-  return <SingleTimetableRow {...args} />
+const TIMETABLE_ROW = {
+  scheduledTime: new Date().toISOString(),
+  type: 'DEPARTURE',
+  cancelled: false,
+  stationShortCode: 'HKI'
+} as const
+
+const STATIONS = [
+  {
+    stationShortCode: 'HKI',
+    stationName: {
+      en: 'Helsinki airport',
+      fi: 'Lentoasema',
+      sv: 'Helsingfors flygplats'
+    }
+  }
+]
+
+export const Default: StoryObj<SingleTimetableRowProps> = {}
+
+export const Cancelled: StoryFn<SingleTimetableRowProps> = args => {
+  const cancelledText = translate(args.locale)('cancelled')
+  const props = {
+    ...args,
+    cancelledText,
+    timetableRow: { ...TIMETABLE_ROW, cancelled: true }
+  }
+
+  return <SingleTimetableRow {...props} />
+}
+
+export const LiveEstimate: StoryObj<SingleTimetableRowProps> = {
+  args: {
+    timetableRow: {
+      ...TIMETABLE_ROW,
+      liveEstimateTime: (() => {
+        const twoMinutes = 2 * 60 * 1000
+        const date = Date.now() + twoMinutes
+
+        return new Date(date).toISOString()
+      })()
+    }
+  }
 }
 
 export default {
   component: SingleTimetableRow,
   args: {
-    timetableRow: {
-      scheduledTime: new Date().toISOString(),
-      type: 'DEPARTURE',
-      cancelled: false,
-      stationShortCode: 'HKI'
-    },
-    stations: [
-      {
-        stationShortCode: 'HKI',
-        stationName: { en: 'Helsinki', fi: 'Helsinki', sv: 'Helsingfors' }
+    timetableRow: TIMETABLE_ROW,
+    stations: STATIONS,
+    locale: 'en'
+  },
+  argTypes: {
+    stations: {
+      table: {
+        disable: true
       }
-    ],
-    locale: 'en',
-    cancelledText: translate('en')('cancelled')
+    },
+    timetableRow: {
+      table: {
+        disable: true
+      }
+    },
+    cancelledText: {
+      table: {
+        disable: true
+      }
+    }
   }
 } satisfies Meta<SingleTimetableRowProps>
