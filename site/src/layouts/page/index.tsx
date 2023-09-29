997351f09b235d2797d3469b866fd5b1f1216641
Jasper Nykänen <nykanen@hey.com>
2023-09-29 12:01:20 +0300

    feat(sb): add footer story (#246)
    skip-deploy

--- site/src/layouts/page/index.tsx
+++ site/src/layouts/page/index.tsx
@@ -3,14 +3,21 @@ import type { LayoutProps } from '@typings/layout_props'
 import dynamic from 'next/dynamic'
 
 import { StyledContent, StyledPage } from './styles'
+import { useRouter } from 'next/router'
+import { useStations } from '~/lib/digitraffic'
 
-const Footer = dynamic(() => import('@components/common/footer'))
+const Footer = dynamic(() => {
+  return import('@components/common/footer').then(mod => mod.AppFooter)
+})
 
 export default function Page({ children }: LayoutProps) {
+  const router = useRouter()
+  const { data: stations = [] } = useStations()
+
   return (
     <StyledPage>
       <StyledContent>{children}</StyledContent>
-      <Footer />
+      <Footer router={router} stations={stations} />
     </StyledPage>
   )
 }
