d901e94c0ddcf5cec698940f6505d5af17bbfc68
Jasper Nykänen <nykanen@hey.com>
2023-10-01 17:11:41 +0300

    refactor: remove @storybook/addon-mdx-gfm

--- site/.storybook/main.ts
+++ site/.storybook/main.ts
@@ -9,7 +9,6 @@ const config = {
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-mdx-gfm'),
     getAbsolutePath('@storybook/addon-coverage'),
     getAbsolutePath('msw-storybook-addon')
   ],
