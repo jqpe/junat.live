bf34cb010d2fe39f34f9ab050461fe4a0d179397
Jasper Nykänen <nykanen@hey.com>
2023-10-02 20:08:07 +0300

    refactor: move error feature to pages dir

--- site/src/pages/_error.tsx
+++ site/src/pages/_error.tsx
@@ -1,1 +1,15 @@
-export { Error as default } from '@features/pages/error'
+import type { ErrorProps } from 'next/error'
+import type { NextPageContext } from 'next'
+
+import * as Sentry from '@sentry/nextjs'
+import NextErrorComponent from 'next/error'
+
+export default function Error(props: ErrorProps) {
+  return <NextErrorComponent statusCode={props.statusCode} />
+}
+
+Error.getInitialProps = async (contextData: NextPageContext) => {
+  await Sentry.captureUnderscoreErrorException(contextData)
+
+  return NextErrorComponent.getInitialProps(contextData)
+}
