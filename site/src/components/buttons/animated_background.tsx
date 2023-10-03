63a31cd313497a665da72762e8682dd6abecaaf6
Jasper Nykänen <nykanen@hey.com>
2023-10-03 14:57:33 +0300

    refactor: write animated_background in tailwind css

---
+++ site/src/components/buttons/animated_background.tsx
@@ -0,0 +1,142 @@
+import type { HTMLMotionProps } from 'framer-motion'
+import type React from 'react'
+
+import { AnimatePresence, motion } from 'framer-motion'
+
+import { useColorScheme } from '@hooks/use_color_scheme'
+
+interface AnimatedButtonProps
+  extends React.PropsWithChildren<HTMLMotionProps<'button'>> {
+  disabled: boolean
+  isLoading: boolean
+  handleClick: VoidFunction
+  loadingText: string
+  visible: boolean
+}
+
+export default function AnimatedButton(props: AnimatedButtonProps) {
+  const { colorScheme } = useColorScheme()
+  const {
+    visible,
+    handleClick,
+    loadingText,
+    isLoading,
+    children,
+    ...buttonProps
+  } = props
+
+  return (
+    <AnimatePresence>
+      {visible && (
+        <motion.button
+          className={`overflow-hidden relative select-none cursor-pointer z-[1] border-solid border-[1px]
+          border-primary-600 text-primary-800 py-[0.3125rem] px-[1.25rem] rounded-full disabled:cursor-not-allowed
+          disabled:bg-gray-100 disabled:border-primary-300 dark:bg-gray-900 dark:border-primary-400 dark:text-primary-200
+          dark:disabled:bg-gray-900 dark:disabled:border-primary-800`}
+          whileTap={{ scale: isLoading ? 1 : 1.1 }}
+          whileHover={{ scale: isLoading ? 1 : 1.05 }}
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ type: 'spring' }}
+          exit={{ opacity: 0, scale: 0, y: 20 }}
+          onClick={handleClick}
+          {...buttonProps}
+        >
+          <div
+            className="max-w-[inherit] absolute z-0 inset-0 animate-translate"
+            style={{ animation: isLoading ? undefined : 'none' }}
+          >
+            <Background style={colorScheme} />
+          </div>
+          <span style={{ zIndex: 1, position: 'relative' }}>
+            {isLoading ? loadingText : children}
+          </span>
+        </motion.button>
+      )}
+    </AnimatePresence>
+  )
+}
+
+function Background({ style }: { style: 'dark' | 'light' }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 700 700"
+      width="700"
+      height="700"
+    >
+      <defs>
+        <linearGradient
+          gradientTransform="rotate(234, 0.5, 0.5)"
+          x1="50%"
+          y1="0%"
+          x2="50%"
+          y2="100%"
+          id="gradient"
+        >
+          <stop
+            stopColor={style === 'light' ? '#f6eaff' : '#420071'}
+            stopOpacity="1"
+            offset="0%"
+          />
+          <stop
+            stopColor={style === 'light' ? '#fbfbfb' : '#030304'}
+            stopOpacity="1"
+            offset="100%"
+          />
+        </linearGradient>
+        <filter
+          id="filter"
+          x="-20%"
+          y="-20%"
+          width="140%"
+          height="140%"
+          filterUnits="objectBoundingBox"
+          primitiveUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.005 0.006"
+            numOctaves="2"
+            seed="86"
+            stitchTiles="stitch"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            result="turbulence"
+          ></feTurbulence>
+          <feGaussianBlur
+            stdDeviation="31 9"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            in="turbulence"
+            edgeMode="duplicate"
+            result="blur"
+          ></feGaussianBlur>
+          <feBlend
+            mode="hard-light"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            in="SourceGraphic"
+            in2="blur"
+            result="blend"
+          ></feBlend>
+        </filter>
+      </defs>
+      <rect
+        width="700"
+        height="700"
+        fill="url(#gradient)"
+        filter="url(#filter)"
+      ></rect>
+    </svg>
+  )
+}
--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -2,7 +2,52 @@
 export default {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        translate: {
+          '0%': { transform: 'translate3d(-500%, -500%, 0)' },
+          '100%': { transform: 'translate3d(0, 0, 0)' }
+        }
+      },
+      animation: {
+        translate: 'translate 1s infinite'
+      },
+      colors: {
+        primary: {
+          100: '#fdfaff',
+          200: '#f6eaff',
+          300: '#eacdff',
+          400: '#d292ff',
+          500: '#c779ff',
+          600: '#9100f9',
+          700: '#7100c2',
+          800: '#420071',
+          900: '#09000f'
+        },
+        secondary: {
+          100: '#f7fcff',
+          200: '#def3fe',
+          300: '#aae0fd',
+          400: '#39b7fa',
+          500: '#06a4f9',
+          600: '#046aa1',
+          700: '#03517b',
+          800: '#022d45',
+          900: '#000305'
+        },
+        gray: {
+          100: '#fbfbfc',
+          200: '#edeff3',
+          300: '#d4d8e1',
+          400: '#a5adc0',
+          500: '#929cb3',
+          600: '#59647f',
+          700: '#444d62',
+          800: '#252a35',
+          900: '#030304'
+        }
+      }
+    }
   },
   plugins: []
 }
