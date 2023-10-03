5407084700784909ac559de8a6e4f2611bb35c10
Jasper Nykänen <nykanen@hey.com>
2023-10-03 18:10:31 +0300

    refactor: primary button styles in tailwind

--- site/src/components/buttons/primary/index.tsx
+++ site/src/components/buttons/primary/index.tsx
@@ -1,11 +1,13 @@
-import type { ComponentProps } from 'react'
+import type { PropsWithChildren } from 'react'
 
-import { StyledPrimaryButton } from './styles'
-
-export type PrimaryButtonProps = ComponentProps<typeof StyledPrimaryButton> & {
-  as?: unknown
-}
-
-export const PrimaryButton = (props: PrimaryButtonProps) => {
-  return <StyledPrimaryButton {...props} />
+export const PrimaryButton = (props: PropsWithChildren) => {
+  return (
+    <button
+      className={`rounded-full border-[2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
+      [transition:border-color_250ms_ease-out_,_background_150ms_ease-out] focus:outline-none focus:border-[2px] focus:border-primary-500
+      text-[14px] p-[2px_15px] font-ui hover:bg-gray-700
+      `}
+      {...props}
+    />
+  )
 }
