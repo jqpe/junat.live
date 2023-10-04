4ab5fd79622acbb867bea8eee025f25677b5262e
Jasper Nykänen <nykanen@hey.com>
2023-10-04 21:52:41 +0300

    refactor: spinner styles in tailwind

--- site/src/components/elements/spinner/index.tsx
+++ site/src/components/elements/spinner/index.tsx
@@ -1,8 +1,15 @@
-import { StyledSpinner } from './styles'
-import type { ComponentPropsWithoutRef } from 'react'
+export type SpinnerProps = React.HTMLAttributes<HTMLDivElement> & {
+  fixedToCenter?: true
+}
 
-export type SpinnerProps = ComponentPropsWithoutRef<typeof StyledSpinner>
+export const Spinner = ({ fixedToCenter, ...props }: SpinnerProps) => {
+  const maybeFixedToCenter = fixedToCenter ? 'fixed left-1/2 top-1/2' : ''
 
-export const Spinner = (props?: SpinnerProps) => {
-  return <StyledSpinner {...props} />
+  return (
+    <div
+      className={`animate-spin duration-1000 ${maybeFixedToCenter} bg-secondary-500 w-6 p-[3px] aspect-[1] 
+      rounded-full [mask-composite:source-out] [mask:conic-gradient(#0000_10%,#000),_linear-gradient(#000_0_0)_content-box]`}
+      {...props}
+    />
+  )
 }
