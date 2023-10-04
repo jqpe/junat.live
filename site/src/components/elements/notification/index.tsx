4d2a48efc335eb4abb2e850880e5b85656a0179d
Jasper Nykänen <nykanen@hey.com>
2023-10-04 21:13:57 +0300

    refactor: notification styles in tailwind

--- site/src/components/elements/notification/index.tsx
+++ site/src/components/elements/notification/index.tsx
@@ -1,4 +1,4 @@
-import { StyledNotification } from './styles'
+import { motion } from 'framer-motion'
 
 type Props = {
   title: string
@@ -7,9 +7,13 @@ type Props = {
 
 export const Notification = (props: Props) => {
   return (
-    <StyledNotification initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+    <motion.aside
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="my-5 bg-secondaryA-400 rounded-[3px] p-[5px] relative shadow-[0px_0px_2px_1px_theme(colors.secondaryA.500)] flex flex-col gap-[15px]"
+    >
       <h4>{props.title}</h4>
       <p> {props.body}</p>
-    </StyledNotification>
+    </motion.aside>
   )
 }
