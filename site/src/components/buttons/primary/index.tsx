a2f1e31a56b59dd96eba7e8a3ebcfca3733cd1ca
Jasper Nykänen <nykanen@hey.com>
2023-10-04 13:49:48 +0300

    refactor: add optional 'as' property

--- site/src/components/buttons/primary/index.tsx
+++ site/src/components/buttons/primary/index.tsx
@@ -1,8 +1,13 @@
 import type { PropsWithChildren } from 'react'
+import React from 'react'
 
-export const PrimaryButton = (props: PropsWithChildren) => {
+export const PrimaryButton = (
+  props: PropsWithChildren<{ as?: React.ElementType }>
+) => {
+  const As = props.as ?? 'button'
+
   return (
-    <button
+    <As
       className={`rounded-full border-[2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
       [transition:border-color_250ms_ease-out_,_background_150ms_ease-out] focus:outline-none focus:border-[2px] focus:border-primary-500
       text-[14px] p-[2px_15px] font-ui hover:bg-gray-700
