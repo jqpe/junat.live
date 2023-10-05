7c3fe9bcb85c00ade034ace61898574ae575c023
Jasper Nykänen <nykanen@hey.com>
2023-10-05 20:25:21 +0300

    fix: primary button border

--- site/src/components/buttons/primary/index.tsx
+++ site/src/components/buttons/primary/index.tsx
@@ -11,7 +11,7 @@ export const PrimaryButton = (props: PrimaryButtonProps) => {
 
   return (
     <As
-      className={`rounded-full border-[2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
+      className={`rounded-full [border:2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
       [transition:border-color_250ms_ease-out_,_background_150ms_ease-out] focus:outline-none focus:border-[2px] focus:border-primary-500
       text-[14px] p-[2px_15px] font-ui hover:bg-gray-700
       `}
