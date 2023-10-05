0a2f19171c08a28eee0fd1687e10af58c646bd63
Jasper Nykänen <nykanen@hey.com>
2023-10-05 21:10:53 +0300

    refactor: polymorphic button

--- site/src/components/buttons/primary/index.tsx
+++ site/src/components/buttons/primary/index.tsx
@@ -1,21 +1,24 @@
-import type { PropsWithChildren } from 'react'
 import React from 'react'
 
-interface PrimaryButtonProps
-  extends PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>> {
-  as?: React.ElementType
+interface PrimaryButtonProps<T extends React.ElementType> {
+  as?: T
+  children?: React.ReactNode | React.ReactNode[]
 }
 
-export const PrimaryButton = (props: PrimaryButtonProps) => {
-  const As = props.as ?? 'button'
+export const PrimaryButton = <T extends React.ElementType = 'button'>({
+  as,
+  ...props
+}: PrimaryButtonProps<T> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof PrimaryButtonProps<T>>) => {
+  const As = as ?? 'button'
 
   return (
     <As
-      className={`rounded-full [border:2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
-      [transition:border-color_250ms_ease-out_,_background_150ms_ease-out] focus:outline-none focus:border-[2px] focus:border-primary-500
-      text-[14px] p-[2px_15px] font-ui hover:bg-gray-700
-      `}
       {...props}
+      className={`rounded-full border-[2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
+    [transition:border-color_250ms_ease-out_,_background_150ms_ease-out] focus:outline-none focus:border-[2px] focus:border-primary-500
+    text-[14px] p-[2px_15px] font-ui hover:bg-gray-700 ${props.className}
+    `}
     />
   )
 }
