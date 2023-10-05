44e5381429132784cebff644b11d5d9f41e83867
Jasper Nykänen <nykanen@hey.com>
2023-10-05 13:26:27 +0300

    refactor: footer styles in tailwind

--- site/src/components/common/footer/index.tsx
+++ site/src/components/common/footer/index.tsx
@@ -4,12 +4,11 @@ import dynamic from 'next/dynamic'
 
 import React from 'react'
 
-import translate from '@utils/translate'
 import { getLocale } from '@utils/get_locale'
+import translate from '@utils/translate'
 
 import { FINTRAFFIC } from '@constants'
 
-import { StyledFooter } from './styles'
 import { getFintrafficPath } from './helpers'
 
 const LanguageSelect = dynamic(
@@ -19,7 +18,12 @@ const LanguageSelect = dynamic(
 const Anchor = (
   props: React.PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement>>
 ) => (
-  <a {...props} target="_blank" rel="noreferrer">
+  <a
+    className="focus:text-primary-200 hover:text-primary-200 text-primary-500"
+    {...props}
+    target="_blank"
+    rel="noreferrer"
+  >
     {props.children}
   </a>
 )
@@ -39,7 +43,14 @@ export function AppFooter(props: AppFooterProps) {
   const path = getFintrafficPath(locale)
 
   return (
-    <StyledFooter>
+    <footer
+      className={`mt-[3rem] p-[1rem_5vw] flex flex-col gap-5 right-0 left-0 bg-gray-700 text-gray-500 text-[0.74rem] lg:text-[0.83rem]
+      bg-[position:0_0,0_10px,10px_-10px,-10px_0px,-10px_0,10px_12px] bg-[size:15px_5px] 
+      `}
+      style={{
+        backgroundImage: backgroundImage()
+      }}
+    >
       <section>
         <LanguageSelect router={props.router} stations={props.stations} />
       </section>
@@ -53,6 +64,30 @@ export function AppFooter(props: AppFooterProps) {
           </Anchor>
         </small>
       </section>
-    </StyledFooter>
+    </footer>
   )
+}
+
+function backgroundImage(): string {
+  const H /**IGH_CONTRAST */ = '#030304' as const
+  const A /**CCENT */ = '#252a35' as const
+  const T /**RANPARENT */ = 'transparent' as const
+
+  const D = 45 as const
+
+  const matrix = [
+    [D, H, '25%', T, '20%'],
+    [-D, H, '25%', T, '25%'],
+    [D, T, '75%', H, '25%'],
+    [-D, T, '75%', H, '25%'],
+    [-D, T, '', A, '25%'],
+    [-D, T, '', H, '25%']
+  ] as const
+
+  return matrix
+    .map(row => {
+      const [d, color, colorPos, color2, colorPos2] = row
+      return `linear-gradient(${d}deg, ${color} ${colorPos}, ${color2} ${colorPos2})`
+    })
+    .join(',')
 }
