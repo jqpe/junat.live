ccf0728f9b3807edbb2ca5d2a19508e27494a7dd
Jasper Nykänen <nykanen@hey.com>
2023-10-06 21:37:47 +0300

    fix: border style

--- site/src/components/buttons/primary/index.tsx
+++ site/src/components/buttons/primary/index.tsx
@@ -15,7 +15,7 @@ export const PrimaryButton = <T extends React.ElementType = 'button'>({
   return (
     <As
       {...props}
-      className={`rounded-full border-[2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
+      className={`rounded-full [border:2px_solid_transparent] bg-gray-800 text-gray-100 select-none cursor-pointer
     [transition:border-color_250ms_ease-out_,_background_150ms_ease-out] focus:outline-none focus:border-[2px] focus:border-primary-500
     text-[14px] p-[2px_15px] font-ui hover:bg-gray-700 ${props.className}
     `}
