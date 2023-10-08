ba129815f0147bd6384b33769c8db620996f17b5
Jasper Nykänen <nykanen@hey.com>
2023-10-08 21:01:18 +0300

    chore: add sentry session replay and console integrations

--- site/sentry.client.config.ts
+++ site/sentry.client.config.ts
@@ -1,7 +1,14 @@
+import { CaptureConsole } from '@sentry/integrations'
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  replaysOnErrorSampleRate: 1,
+  replaysSessionSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
+  integrations: [
+    new CaptureConsole({ levels: ['error', 'warn'] }),
+    new Sentry.Replay()
+  ],
   tracesSampleRate: 1,
   debug: false
 })
