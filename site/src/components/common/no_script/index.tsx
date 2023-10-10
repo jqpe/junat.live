17f8f64fdc6ee8ee807efdc4049ce400da677b47
Jasper Nykänen <nykanen@hey.com>
2023-10-10 14:40:24 +0300

    fix: use gray

--- site/src/components/common/no_script/index.tsx
+++ site/src/components/common/no_script/index.tsx
@@ -9,7 +9,7 @@ export function NoScript(
   const As = props.as ?? 'noscript'
 
   return (
-    <As className="bg-gray-900 text-gray-100 dark:bg-gray-100 dark:text-slate-900 p-[10px] flex flex-col justify-center items-center fixed top-0 right-0 left-0">
+    <As className="bg-gray-900 text-gray-100 dark:bg-gray-100 dark:text-gray-900 p-[10px] flex flex-col justify-center items-center fixed top-0 right-0 left-0">
       <p>{props.children}</p>
     </As>
   )
