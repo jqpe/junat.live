74322f1a1977fcc12f49dff6830d3a34f55ec932
Jasper Nykänen <nykanen@hey.com>
2023-10-12 17:08:18 +0300

    refactor: use recursive function

--- site/tests/locales.test.ts
+++ site/tests/locales.test.ts
@@ -7,6 +7,10 @@ const MODULES: Record<string, any> = import.meta.glob('/src/locales/*.json', {
   import: 'default'
 })
 
+// There are rare cases where we might want to skip testing for a particular object
+// Format: [[keys for depth 0], [keys for depth 1]...]
+const SKIP_KEY = [['stations']]
+
 test('localizations have same keys max depth = 3', () => {
   const keys = Object.keys(MODULES)
 
@@ -16,34 +20,33 @@ test('localizations have same keys max depth = 3', () => {
     const aObj = MODULES[keys[prev]]
     const bObj = MODULES[keys[i]]
 
-    const d0_a = Object.keys(aObj).sort()
-    const d0_b = Object.keys(bObj).sort()
+    compareKeys(aObj, bObj, 0, 3)
+  }
+})
 
-    const message = `Expected ${keys[prev]} to have same keys as ${keys[i]}`
+function compareKeys(
+  aObj: { [x: string]: any },
+  bObj: { [x: string]: any },
+  depth: number,
+  maxDepth: number
+) {
+  SKIP_KEY.at(depth)?.every(keyToSkip => {
+    delete aObj[keyToSkip]
+    delete bObj[keyToSkip]
+  })
 
-    expect(d0_a, message).toStrictEqual(d0_b)
+  const aKeys = Object.keys(aObj).sort()
+  const bKeys = Object.keys(bObj).sort()
 
-    const d1 = d0_a.filter(key => typeof aObj[key] === 'object')
-    const SKIP_KEYS = { d1: 'stations' }
+  expect(aKeys).toStrictEqual(bKeys)
 
-    for (const d of d1) {
-      if (d === SKIP_KEYS.d1) {
-        continue
-      }
-
-      const d1_a = Object.keys(aObj[d]).sort()
-      const d1_b = Object.keys(bObj[d]).sort()
-
-      expect(d1_a).toStrictEqual(d1_b)
-
-      const d2 = d1_a.filter(key => typeof aObj[d][key] === 'object')
-
-      for (const d_2 of d2) {
-        const a = Object.keys(aObj[d][d_2]).sort()
-        const b = Object.keys(bObj[d][d_2]).sort()
-
-        expect(a).toStrictEqual(b)
+  if (depth < maxDepth) {
+    for (const key of aKeys) {
+      if (typeof aObj[key] === 'object') {
+        const aChild = aObj[key]
+        const bChild = bObj[key]
+        compareKeys(aChild, bChild, depth + 1, maxDepth)
       }
     }
   }
-})
+}
