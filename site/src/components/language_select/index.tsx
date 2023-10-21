6c1f991a0e1f42e8eebed8bf404d95953945d5b6
Jasper Nykänen <nykanen@hey.com>
2023-10-21 14:52:46 +0300

    refactor: move select

--- site/src/components/language_select/index.tsx
+++ site/src/components/language_select/index.tsx
@@ -2,7 +2,7 @@ import type { NextRouter } from 'next/router'
 
 import translate from '@utils/translate'
 
-import { Select } from '@components/input/select'
+import { Select } from '~/components/select'
 import { useStationPage } from '@hooks/use_station_page'
 
 import { getLocale } from '@utils/get_locale'
