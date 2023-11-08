39aff191ec2e59e99e921610c58a885335bb565a
Jasper Nykänen <nykanen@hey.com>
2023-11-08 14:51:47 +0200

    refactor: remove unnecessary divs

--- site/src/components/station_dropdown_menu/filter_train.tsx
+++ site/src/components/station_dropdown_menu/filter_train.tsx
@@ -65,32 +65,28 @@ export const FilterTrain = (props: Props) => {
                   props.setFieldValue('destination', station?.stationShortCode)
                 }}
               >
-                <div className="w-[100%]">
-                  <div className="flex justify-between">
-                    <Combobox.Input
-                      autoComplete="off"
-                      autoCorrect="off"
-                      id="destination"
-                      name="destination"
-                      onChange={event => setQuery(event.target.value)}
-                      displayValue={(station: typeof selectedStation) =>
-                        station?.stationName[locale] ?? ''
-                      }
-                    />
-                  </div>
-                  <div>
-                    <Combobox.Options>
-                      {filteredStations.map(station => (
-                        <Combobox.Option
-                          key={station.stationShortCode}
-                          value={station}
-                        >
-                          {station.stationName[locale]}
-                        </Combobox.Option>
-                      ))}
-                    </Combobox.Options>
-                  </div>
-                </div>
+                <Combobox.Input
+                  className={'w-full'}
+                  autoComplete="off"
+                  autoCorrect="off"
+                  id="destination"
+                  name="destination"
+                  onChange={event => setQuery(event.target.value)}
+                  displayValue={(station: typeof selectedStation) =>
+                    station?.stationName[locale] ?? ''
+                  }
+                />
+                <Combobox.Options>
+                  {filteredStations.map(station => (
+                    <Combobox.Option
+                    className="select-none"
+                      key={station.stationShortCode}
+                      value={station}
+                    >
+                      {station.stationName[locale]}
+                    </Combobox.Option>
+                  ))}
+                </Combobox.Options>
               </Combobox>
               <PrimaryButton type="submit" className="mt-2 self-end">
                 {t('buttons', 'submit')}
