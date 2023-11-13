c150d27de24417d47207d0ad0d69f148be69ac64
Jasper Nykänen <nykanen@hey.com>
2023-11-13 09:13:40 +0200

    feat: new videos

--- site/src/features/pages/about/components/page.tsx
+++ site/src/features/pages/about/components/page.tsx
@@ -1,101 +1,96 @@
 import Link from 'next/link'
 import React from 'react'
+import { Head } from '~/components/head'
 
 export const AboutUsPage = () => {
-  const favoritesVideo = React.createRef<HTMLVideoElement>()
-
   React.useEffect(() => {
     document.documentElement.classList.add('dark')
   }, [])
 
   return (
-    <main className="flex flex-col max-w-[500px] m-auto p-2 mt-5">
-      <div className="mx-5 mb-2">
-        <h1>About Us</h1>
-        <p className="mt-2">
-          Junat.live was built as an alternative for proprietary train schedule
-          websites. Our values are privacy, reliability, and accessibility.
-          Junat.live is open-source software.
-        </p>
-        <h2 className="mt-4">Features</h2>
-        <p className="mt-2">
-          The core feature of Junat.live is accurate real-time departure
-          predictions. All station- and train pages use this functionality. It
-          just works. Below other features, use the web app to discover the
-          rest.
-        </p>
-      </div>
-      <Section id="favorites">
-        <SectionHeading>Favorites</SectionHeading>
-        <p>Save your frequntly used stations for easy access.</p>
-        <video
-          ref={favoritesVideo}
-          src="/videos/favorites_feature.mp4"
-          onClick={() => {
-            favoritesVideo.current?.paused
-              ? favoritesVideo.current.play()
-              : favoritesVideo.current?.pause()
-          }}
-          muted
-          autoPlay
-          playsInline
-          loop
-          preload="true"
-        />
-        <ol className="text-sm list-disc">
-          <li>Go to a station page</li>
-          <li>
-            Click the menu available on the page, the big button with three
-            dots.
-          </li>
-          <li>
-            Add a station to favorites from the submenu, it will be instantly
-            available on homepage for later use.
-          </li>
-          <li>
-            Toggle between favorites and all stations with the toggle button
-            below search bar.
-          </li>
-        </ol>
-      </Section>
-      <Section id="geolocation">
-        <SectionHeading>Geolocation</SectionHeading>
-        <p>
-          Get to the station closest to you with a click of a button. Your
-          location is provided by your browser so accuracy may vary. If your
-          advertised location is not accurate to 3km, we will just sort the
-          stations by distance. Use GPS and/or Wi-Fi for best results. This
-          functionality is only available on the homepage (the button on the
-          bottom right)
-        </p>
-      </Section>
-      <Section id="filtering">
-        <SectionHeading>Filtering</SectionHeading>
-        <p>
-          Filter trains on the station page by destination. The feature is smart
-          enough to also include trains that stop at that station, even if
-          it&apos;s not the final destination.
-        </p>
-        <ol className="text-sm list-disc my-4">
-          <li>Go to a station page</li>
-          <li>
-            Click the menu available on the page, the big button with three
-            dots.
-          </li>
-          <li>
-            Open the filter dialog from the submenu, type in the destination,
-            and submit the form.
-          </li>
-        </ol>
-        <p>
-          When a filter is active, a purple dot will appear next to the menu
-          button.
-        </p>
-      </Section>
-      <div className="flex gap-2 mb-4">
-        Ready when you are! <Link href="/">To home</Link>
-      </div>
-    </main>
+    <>
+      <Head
+        path="/about"
+        description="About Junat.live, the free train schedule site for Finnish train traffic."
+        title="About Us"
+      />
+      <main className="flex flex-col max-w-[500px] m-auto p-2 mt-5">
+        <div className="mx-5 mb-2">
+          <h1>About Us</h1>
+          <p className="mt-2">
+            Junat.live was built as an alternative for proprietary train
+            schedule websites. Our values are privacy, reliability, and
+            accessibility. Junat.live is open-source software.
+          </p>
+          <h2 className="mt-4">Features</h2>
+          <p className="mt-2">
+            The core feature of Junat.live is accurate real-time departure
+            predictions. All station- and train pages use this functionality. It
+            just works. Below other features, use the web app to discover the
+            rest.
+          </p>
+        </div>
+        <Section id="favorites">
+          <SectionHeading>Favorites</SectionHeading>
+          <p>Save your frequntly used stations for easy access.</p>
+          <Video src="/videos/favorites.mp4" />
+          <ol className="text-sm list-disc">
+            <li>Go to a station page</li>
+            <li>
+              Click the menu available on the page, the big button with three
+              dots.
+            </li>
+            <li>
+              Add a station to favorites from the submenu, it will be instantly
+              available on homepage for later use.
+            </li>
+            <li>
+              Toggle between favorites and all stations with the toggle button
+              below search bar.
+            </li>
+          </ol>
+        </Section>
+        <Section id="geolocation">
+          <SectionHeading>Geolocation</SectionHeading>
+          <p>
+            Get to the station closest to you with a click of a button. Your
+            location is provided by your browser so accuracy may vary. If your
+            advertised location is not accurate to 3km, we will just sort the
+            stations by distance. Use GPS and/or Wi-Fi for best results. This
+            functionality is only available on the homepage (the button on the
+            bottom right)
+          </p>
+          <Video src="/videos/geolocation.mp4 " />
+        </Section>
+        <Section id="filtering">
+          <SectionHeading>Filtering</SectionHeading>
+          <p>
+            Filter trains on the station page by destination. The feature is
+            smart enough to also include trains that stop at that station, even
+            if it&apos;s not the final destination.
+          </p>
+          <Video src="/videos/filters.mp4" />
+          <ol className="text-sm list-disc my-4">
+            <li>Go to a station page</li>
+            <li>
+              Click the menu available on the page, the big button with three
+              dots.
+            </li>
+            <li>
+              Open the filter dialog from the submenu, type in the destination,
+              and submit the form.
+            </li>
+          </ol>
+          <p>
+            When a filter is active, a purple dot will appear next to the menu
+            button.
+          </p>
+        </Section>
+        <div className="flex gap-2 mb-4">
+          Ready when you are! <Link href="/">To home</Link>
+        </div>
+      </main>
+    </>
   )
 }
 
@@ -108,5 +103,25 @@ const Section = (props: React.PropsWithChildren<{ id: string }>) => {
     <section id={props.id} className="p-5 my-2">
       {props.children}
     </section>
+  )
+}
+
+const Video = (props: React.PropsWithoutRef<{ src: string }>) => {
+  const video = React.createRef<HTMLVideoElement>()
+  const togglePlay = () => {
+    video.current?.paused ? video.current.play() : video.current?.pause()
+  }
+
+  return (
+    <video
+      ref={video}
+      src={props.src}
+      onClick={togglePlay}
+      muted
+      autoPlay
+      playsInline
+      loop
+      preload="true"
+    />
   )
 }
