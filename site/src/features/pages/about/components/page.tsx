ed53e3880c4fa248a2b64d41dc18993e7adf7d10
Jasper Nykänen <nykanen@hey.com>
2023-11-14 17:58:04 +0200

    feat: localization

--- site/src/features/pages/about/components/page.tsx
+++ site/src/features/pages/about/components/page.tsx
@@ -1,8 +1,18 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
+
 import { Head } from '~/components/head'
+import { getLocale } from '~/utils/get_locale'
+
+import translate from '~/utils/translate'
 
 export const AboutUsPage = () => {
+  const router = useRouter()
+
+  const locale = getLocale(router.locale)
+  const t = translate(locale)
+
   React.useEffect(() => {
     document.documentElement.classList.add('dark')
   }, [])
@@ -11,83 +21,54 @@ export const AboutUsPage = () => {
     <>
       <Head
         path="/about"
-        description="About Junat.live, the free train schedule site for Finnish train traffic."
-        title="About Us"
+        description={t("aboutPage", "metadescription")}
+        title={t("aboutPage", "topLevelHeading")}
       />
       <main className="flex flex-col max-w-[500px] m-auto p-2 mt-5">
         <div className="mx-5 mb-2">
-          <h1>About Us</h1>
-          <p className="mt-2">
-            Junat.live was built as an alternative for proprietary train
-            schedule websites. Our values are privacy, reliability, and
-            accessibility. Junat.live is open-source software.
-          </p>
-          <h2 className="mt-4">Features</h2>
-          <p className="mt-2">
-            The core feature of Junat.live is accurate real-time departure
-            predictions. All station- and train pages use this functionality. It
-            just works. Below other features, use the web app to discover the
-            rest.
-          </p>
+          <h1>{t('aboutPage', 'topLevelHeading')}</h1>
+          <p className="mt-2">{t('aboutPage', 'description')}</p>
+          <h2 className="mt-4">{t('features')}</h2>
+          <p className="mt-2">{t('aboutPage', 'highLevelFeatures')}</p>
         </div>
+
         <Section id="favorites">
-          <SectionHeading>Favorites</SectionHeading>
-          <p>Save your frequntly used stations for easy access.</p>
+          <SectionHeading>
+            {t('aboutPage', 'favorites', 'heading')}
+          </SectionHeading>
+          <p>{t('aboutPage', 'favorites', 'description')}</p>
           <Video src="/videos/favorites.mp4" />
           <ol className="text-sm list-disc">
-            <li>Go to a station page</li>
-            <li>
-              Click the menu available on the page, the big button with three
-              dots.
-            </li>
-            <li>
-              Add a station to favorites from the submenu, it will be instantly
-              available on homepage for later use.
-            </li>
-            <li>
-              Toggle between favorites and all stations with the toggle button
-              below search bar.
-            </li>
+            {t('aboutPage', 'favorites', 'steps').map((step: string) => (
+              <li key={step}>{step}</li>
+            ))}
           </ol>
         </Section>
+
         <Section id="geolocation">
-          <SectionHeading>Geolocation</SectionHeading>
-          <p>
-            Get to the station closest to you with a click of a button. Your
-            location is provided by your browser so accuracy may vary. If your
-            advertised location is not accurate to 3km, we will just sort the
-            stations by distance. Use GPS and/or Wi-Fi for best results. This
-            functionality is only available on the homepage (the button on the
-            bottom right)
-          </p>
+          <SectionHeading>
+            {t('aboutPage', 'geolocation', 'heading')}
+          </SectionHeading>
+          <p>{t('aboutPage', 'geolocation', 'description')}</p>
           <Video src="/videos/geolocation.mp4 " />
         </Section>
+
         <Section id="filtering">
-          <SectionHeading>Filtering</SectionHeading>
-          <p>
-            Filter trains on the station page by destination. The feature is
-            smart enough to also include trains that stop at that station, even
-            if it&apos;s not the final destination.
-          </p>
+          <SectionHeading>
+            {t('aboutPage', 'filters', 'heading')}
+          </SectionHeading>
+          <p>{t('aboutPage', 'filters', 'description')}</p>
           <Video src="/videos/filters.mp4" />
           <ol className="text-sm list-disc my-4">
-            <li>Go to a station page</li>
-            <li>
-              Click the menu available on the page, the big button with three
-              dots.
-            </li>
-            <li>
-              Open the filter dialog from the submenu, type in the destination,
-              and submit the form.
-            </li>
+            {t('aboutPage', 'filters', 'steps').map((step: string) => (
+              <li key={step}>{step}</li>
+            ))}
           </ol>
-          <p>
-            When a filter is active, a purple dot will appear next to the menu
-            button.
-          </p>
+          <p>{t('aboutPage', 'filters', 'postscript')}</p>
         </Section>
+
         <div className="flex gap-2 mb-4">
-          Ready when you are! <Link href="/">To home</Link>
+          {t('readyWhenYouAre')} <Link href="/">{t('toHome')}</Link>
         </div>
       </main>
     </>
@@ -121,7 +102,7 @@ const Video = (props: React.PropsWithoutRef<{ src: string }>) => {
       autoPlay
       playsInline
       loop
-      preload="true"
+      preload="metadata"
     />
   )
 }
