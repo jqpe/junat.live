6dde3914b644d0e203487cfda0faa81a66391803
Jasper Nykänen <nykanen@hey.com>
2024-02-17 11:40:00 +0200

    refactor: always return array

--- site/src/features/geolocation/utils/get_nearby_stations.ts
+++ site/src/features/geolocation/utils/get_nearby_stations.ts
@@ -3,24 +3,16 @@ import type { Locale } from '@typings/common'
 import { getNearestStation } from './get_nearest_station'
 import { sortStationsByDistance } from './sort_stations_by_distance'
 
+interface Station {
+  latitude: number
+  longitude: number
+}
+
 interface GetNearbyStationsProps {
-  /**
-   * If position data was accurate to 1000 meters return the nearest station.
-   * Otherwise return stations sorted by their distance to `position`.
-   * Use `ignorePoorAccuracy` parameter to disable this behavior and always return an array.
-   */
-  <T extends { latitude: number; longitude: number }>(
+  <T extends Station>(
     position: GeolocationPosition,
-    opts: { stations: T[]; ignorePoorAccuracy?: boolean }
-  ): T | T[]
-  <T extends { latitude: number; longitude: number }>(
-    position: GeolocationPosition,
-    opts: {
-      stations: T[]
-      ignorePoorAccuracy?: boolean
-      locale: Locale
-    }
-  ): T | T[]
+    opts: { stations: T[]; locale?: Locale }
+  ): T[]
 }
 
 export const getNearbyStations: GetNearbyStationsProps = (position, opts) => {
@@ -37,23 +29,12 @@ export const getNearbyStations: GetNearbyStationsProps = (position, opts) => {
     position
   )
 
-  const poorAccuracy = opts.ignorePoorAccuracy
-    ? true
-    : position.coords.accuracy > 1000
-
-  if (poorAccuracy) {
-    return sortStationsByDistance<(typeof opts.stations)[number]>(
-      opts.stations,
-      {
-        ...position,
-        coords: {
-          ...position.coords,
-          latitude: nearestStation.latitude,
-          longitude: nearestStation.longitude
-        }
-      }
-    )
-  }
-
-  return nearestStation
+  return sortStationsByDistance<(typeof opts.stations)[number]>(opts.stations, {
+    ...position,
+    coords: {
+      ...position.coords,
+      latitude: nearestStation.latitude,
+      longitude: nearestStation.longitude
+    }
+  })
 }
