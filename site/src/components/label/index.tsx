b3893dfc1793907f7e2a1e648a5eeafcd0900c09
Jasper Nykänen <nykanen@hey.com>
2024-02-22 13:53:52 +0200

    feat: disable label text selection

--- site/src/components/label/index.tsx
+++ site/src/components/label/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
   return (
     <label
-      className="text-gray-700 text-[0.74rem] lg:text-[0.83rem] dark:text-gray-300"
+      className="text-gray-700 text-[0.74rem] lg:text-[0.83rem] dark:text-gray-300 select-none"
       {...props}
     />
   )
