b21ed5654df410b20b05e01ae42053a63ce51ab6
Jasper Nykänen <nykanen@hey.com>
2024-02-25 08:42:10 +0200

    fix: field bottom border dark mode styles

--- site/src/components/field/index.tsx
+++ site/src/components/field/index.tsx
@@ -6,8 +6,8 @@ import { VariantProps, cva } from 'cva'
 export type FieldProps<T = unknown> = FieldAttributes<T>
 
 const field = cva({
-  base: `border-solid border-b-[1px] border-x-0 border-t-0 border-b-gray-200 text-gray-800 dark:border-b-gray-800 dark:text-gray-200
-  [&::-webkit-input-placeholder]:text-gray-500`,
+  base: `border-solid border-b-[1px] border-x-0 border-t-0 border-b-gray-200 text-gray-800 dark:border-b-gray-700 dark:text-gray-200
+  [&::-webkit-input-placeholder]:text-gray-500 `,
   variants: {
     component: {
       textarea: 'w-full resize-y min-h-fit'
