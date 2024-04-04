248a67cd9ce3fa84cf76630f7e8d7304c3eaa82f
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-04 12:36:17 +0300

    feat: add hamburger menu

---
+++ site/src/components/animated_hamburger_menu/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+import { motion, type Variants } from 'framer-motion'
+import translate from '~/utils/translate'
+import { useRouter } from 'next/router'
+import { getLocale } from '~/utils/get_locale'
+
+type Props = {
+  onOpenChange: (open: boolean) => void
+}
+
+export const HamburgerMenu = (props: Props) => {
+  const [open, setOpen] = React.useState(false)
+  const router = useRouter()
+
+  const handleOnClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    props.onOpenChange(!open)
+    setOpen(!open)
+  }
+
+  const t = translate(getLocale(router.locale))
+
+  const createAnimatedLine = ({ y, deg }: { y: number; deg: number }) => {
+    return (
+      <motion.line
+        x1="4"
+        y1={y}
+        x2="20"
+        y2={y}
+        strokeLinecap="round"
+        stroke="white"
+        strokeWidth="2"
+        custom={{ y, deg, open }}
+        variants={icon}
+        animate={['pan', 'rotate']}
+      />
+    )
+  }
+
+  return (
+    <button
+      id="menu"
+      onClick={handleOnClick}
+      className="flex"
+      aria-label={
+        open
+          ? t('menu', 'navbar', 'labelExpanded')
+          : t('menu', 'navbar', 'labelCollapsed')
+      }
+    >
+      <svg
+        role="img"
+        aria-labelledby="menu"
+        className="dark:fill-white"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {createAnimatedLine({ y: 6, deg: -45 })}
+        {createAnimatedLine({ y: 16, deg: 45 })}
+      </svg>
+    </button>
+  )
+}
+
+const DELAY_BETWEEN_VARIANTS = 0.4
+
+const icon: Variants = {
+  pan: ({ open, y }: { y: number; open: boolean }) => ({
+    y1: open ? 12 : y,
+    y2: open ? 12 : y,
+    transition: {
+      delay: open ? 0 : DELAY_BETWEEN_VARIANTS,
+      type: 'spring',
+      bounce: 0
+    }
+  }),
+  rotate: ({ deg, open }: { deg: number; open: boolean }) => ({
+    rotate: open ? deg : 0,
+    // Use rems instead of a number to not use relative values, framer motion uses percentages by default.
+    // Root element is used to work across different zooms, yields 12px (.75 * 16 = 12 = 24/2 = middle of svg viewport)
+    originX: open ? '0.75rem' : '0rem',
+    originY: open ? '0.75rem' : '0rem',
+    transition: {
+      delay: open ? DELAY_BETWEEN_VARIANTS : 0,
+      type: 'spring',
+      bounce: 0
+    }
+  })
+}
