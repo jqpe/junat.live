bd996866f6053cd3f76aa0048537597b45180111
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-04 15:16:27 +0300

    feat(wip): add global navigation menu
    - needs a11y improvements (e.g. opening the menu should move the focus)
    - needs actual items
    - needs light mode variant

--- site/src/layouts/page.tsx
+++ site/src/layouts/page.tsx
@@ -6,15 +6,20 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import constants from '~/constants'
 import { useStations } from '~/lib/digitraffic'
+import { RemoveScroll } from 'react-remove-scroll'
+
+import { motion } from 'framer-motion'
 
 import Train from '~/components/icons/train.svg'
 import { HamburgerMenu } from '~/components/animated_hamburger_menu'
+import React, { PropsWithChildren } from 'react'
 
 const Footer = dynamic(() => {
   return import('~/components/footer').then(mod => mod.AppFooter)
 })
 
 export default function Page({ children }: LayoutProps) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
   const router = useRouter()
   const { data: stations = [] } = useStations()
 
@@ -37,15 +42,44 @@ export default function Page({ children }: LayoutProps) {
             </Link>
           </li>
           <li>
-            <HamburgerMenu onOpenChange={() => {}} />
+            <HamburgerMenu onOpenChange={setIsMenuOpen} />
           </li>
         </ul>
       </header>
+
+      <RemoveScroll enabled={isMenuOpen}>
+        <motion.div
+          aria-hidden={!isMenuOpen}
+          initial={{ opacity: 0 }}
+          animate={isMenuOpen ? { opacity: 1 } : { opacity: 0 }}
+          style={{
+            pointerEvents: isMenuOpen ? 'all' : 'none',
+            visibility: isMenuOpen ? 'visible' : 'hidden'
+          }}
+          className="fixed inset-0 h-full top-[42px] bg-[#000] bg-opacity-70 backdrop-blur-sm z-40"
+        >
+          <div className="px-[1.875rem] max-w-[500px] m-auto py-9 flex flex-col gap-10">
+            <MenuItem href="/contact">Contact</MenuItem>
+            <MenuItem href="/settings">Settings</MenuItem>
+          </div>
+        </motion.div>
+      </RemoveScroll>
 
       <div className="px-[1.875rem] max-w-[500px] m-auto min-h-screen pt-[50px]">
         {children}
       </div>
       <Footer router={router} stations={stations} />
     </div>
+  )
+}
+
+const MenuItem = (props: PropsWithChildren<{ href: string }>) => {
+  return (
+    <Link
+      className="text-2xl text-gray-200 font-bold tracking-wider decoration-transparent hover:text-white"
+      href={props.href}
+    >
+      {props.children}
+    </Link>
   )
 }
