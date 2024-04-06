03533ef1f24ea5bc0f1ea2b69cd06c25925aa7bc
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-06 14:34:54 +0300

    refactor: remove hardcoded header height

--- site/src/components/menu/drawer.tsx
+++ site/src/components/menu/drawer.tsx
@@ -62,7 +62,7 @@ export const MenuDrawer = ({
           pointerEvents: isOpen ? 'all' : 'none',
           visibility: isOpen ? 'visible' : 'hidden'
         }}
-        className="fixed inset-0 h-full top-[42px] dark:bg-gray-900 dark:bg-opacity-30
+        className="fixed inset-0 h-full top-[var(--header-height)] dark:bg-gray-900 dark:bg-opacity-30
      bg-gray-800 bg-opacity-5 backdrop-blur-lg backdrop-brightness-110 dark:backdrop-brightness-75 z-[2] select-none"
       >
         <ul className="px-[1.875rem] max-w-[500px] m-auto py-9 flex flex-col gap-10">
