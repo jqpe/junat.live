2835dc741f4d3ea642b2c25d21b39b3db536c42b
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-07 19:39:05 +0300

    feat: adjust light mode header styles

--- site/src/components/menu/header.tsx
+++ site/src/components/menu/header.tsx
@@ -18,22 +18,22 @@ export const MenuHeader = ({
   return (
     <header>
       <nav
-        className="h-[var(--header-height)] flex items-center fixed inset-0 backdrop-blur-lg z-[2] bg-gray-800 
-  bg-opacity-5 dark:bg-opacity-20 border-b-[.33px] dark:border-grayA-800 border-grayA-400 select-none"
+        className="h-[var(--header-height)] flex items-center fixed inset-0 backdrop-blur-lg z-[2] dark:bg-gray-800 
+  bg-gray-100 bg-opacity-80 dark:bg-opacity-20 border-b-[.33px] dark:border-grayA-800 border-grayA-400 select-none"
       >
         <ul className="max-w-[500px] px-[1.875rem] w-full m-auto flex justify-between items-center">
           <li>
             <Link
               aria-current={router.asPath === '/' ? 'page' : 'false'}
               href="/"
-              className="text-md text-gray-800 flex tracking-tight font-ui items-center py-2 gap-1 leading-3 dark:text-gray-300 no-underline hover:text-gray-900 dark:hover:text-gray-200
+              className="text-md text-gray-900 flex tracking-tight font-ui items-center py-2 gap-1 leading-3 dark:text-gray-300 no-underline hover:text-gray-900 dark:hover:text-gray-200
               focus:text-[initial]"
             >
               <Train
                 width="16"
                 height="16"
                 viewBox="0 0 24 24"
-                className="dark:fill-gray-300 fill-gray-800"
+                className="dark:fill-gray-300 fill-gray-900"
               />
               {constants.SITE_NAME}
             </Link>
