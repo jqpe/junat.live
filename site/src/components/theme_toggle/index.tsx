f290316c65ebb579b0b483e84d5b780e57622272
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-08 10:33:54 +0300

    refactor: create theme toggle from a reusable component

--- site/src/components/theme_toggle/index.tsx
+++ site/src/components/theme_toggle/index.tsx
@@ -1,29 +1,9 @@
-import { Root, Item } from '@radix-ui/react-radio-group'
-
-import { motion } from 'framer-motion'
 import { useRouter } from 'next/router'
-import { getLocale } from '~/utils/get_locale'
+
+import { RadioGroup } from '~/components/radio_group'
+
 import translate from '~/utils/translate'
-
-const ThemeToggleItem = (props: { item: string; value: string }) => {
-  const id = `theme-radio-item-${props.item}`
-
-  return (
-    <motion.div className="data-[checked=true]:bg-white text-center grid grid-cols-1 min-w-fit">
-      <Item
-        value={props.item}
-        id={id}
-        className="[grid-column-start:1] [grid-row-start:1] z-[0] focus:outline-none data-[state=checked]:bg-primary-500 rounded-full "
-      />
-      <label
-        className="[grid-column-start:1] [grid-row-start:1] text-sm z-[1] px-2 py-0.5 pointer-events-none"
-        htmlFor={id}
-      >
-        {props.value}
-      </label>
-    </motion.div>
-  )
-}
+import { getLocale } from '~/utils/get_locale'
 
 export const ThemeToggle = () => {
   const defaultValue = localStorage.getItem('theme') ?? 'system'
@@ -31,27 +11,15 @@ export const ThemeToggle = () => {
   const t = translate(getLocale(router.locale))
 
   return (
-    <Root
-      asChild
+    <RadioGroup
       defaultValue={defaultValue}
+      values={{
+        light: t('themeVariants', 'light'),
+        dark: t('themeVariants', 'dark'),
+        system: t('themeVariants', 'system')
+      }}
       onValueChange={onValueChange}
-      className="flex rounded-full border-primary-500 border-[1px] max-w-max overflow-clip bg-gray-100 dark:bg-transparent"
-    >
-      <motion.div
-        animate={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-        transition={{
-          type: 'spring',
-          damping: 25,
-          stiffness: 50,
-          mass: 2
-        }}
-      >
-        <ThemeToggleItem item="light" value={t('themeVariants', 'light')} />
-        <ThemeToggleItem item="dark" value={t('themeVariants', 'dark')} />
-        <ThemeToggleItem item="system" value={t('themeVariants', 'system')} />
-      </motion.div>
-    </Root>
+    />
   )
 }
 
@@ -66,10 +34,8 @@ const onValueChange = (value: string) => {
     return
   }
 
-  try {
-    window.__setPreferredTheme()
-    localStorage.removeItem('theme')
-  } catch {}
+  window.__setPreferredTheme()
+  localStorage.removeItem('theme')
 
   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
 
