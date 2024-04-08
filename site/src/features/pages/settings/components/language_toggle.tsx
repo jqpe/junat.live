f8891186bff7248980045444d0caacdc7359f69c
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-08 12:58:41 +0300

    feat: replace with rewrite

--- site/src/features/pages/settings/components/language_toggle.tsx
+++ site/src/features/pages/settings/components/language_toggle.tsx
@@ -1,19 +1,25 @@
+import type { Locale } from '~/types/common'
+
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
 
 import { RadioGroup } from '~/components/radio_group'
+import { ROUTES } from '~/constants/locales'
 import { getLocale } from '~/utils/get_locale'
 import translate from '~/utils/translate'
 
 export const LanguageToggle = () => {
   const router = useRouter()
+  const locale = getLocale(router.locale)
 
   return (
     <RadioGroup
-      defaultValue={getLocale(router.locale)}
+      defaultValue={locale}
       values={translate('all')('locale')}
       onValueChange={locale => {
-        router.replace(router.pathname, router.asPath, { locale })
+        router.replace(router.pathname, ROUTES[locale as Locale]['settings'], {
+          locale
+        })
 
         Cookies.set('NEXT_LOCALE', locale, {
           sameSite: 'Lax',
