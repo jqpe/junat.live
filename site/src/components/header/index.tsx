f018c59f380ac403ccd55f93a75d5c0b57ac0de5
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-09 14:54:44 +0300

    fix: only use one header element in pages (#348)

--- site/src/components/header/index.tsx
+++ site/src/components/header/index.tsx
@@ -1,9 +1,9 @@
 export function Header(props: { heading: string; visuallyHidden?: boolean }) {
   return (
-    <header className="mb-2">
-      <h1 className={props.visuallyHidden ? 'visually-hidden' : undefined}>
+    <div className="mb-2">
+      <h1 className={props.visuallyHidden ? 'visually-hidden' : ''}>
         {props.heading}
       </h1>
-    </header> 
+    </div>
   )
 }
