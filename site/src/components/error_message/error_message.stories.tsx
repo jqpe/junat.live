c439e989da048191112017316081d2e49447a2e4
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-18 17:23:28 +0300

    refactor: use new msw api

--- site/src/components/error_message/error_message.stories.tsx
+++ site/src/components/error_message/error_message.stories.tsx
@@ -1,7 +1,7 @@
 import { DigitrafficError } from '@junat/digitraffic'
 import { Meta, StoryFn } from '@storybook/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { rest } from 'msw'
+import { http, HttpResponse } from 'msw'
 import { ErrorMessage } from './'
 
 export const DigitrafficNetworkError: StoryFn<typeof ErrorMessage> = args => {
@@ -40,32 +40,27 @@ export const DigitrafficUnknownError: StoryFn<typeof ErrorMessage> = args => {
 DigitrafficUnknownError.parameters = {
   msw: {
     handlers: [
-      rest.get(
-        'https://status.digitraffic.fi/api/v2/summary.json',
-        (_req, res, ctx) => {
-          return res(
-            ctx.json({
-              components: [
-                {
-                  id: 'nfys4zwym2wz',
-                  name: 'Rail MQTT',
-                  status: 'major_outage'
-                },
-                {
-                  id: '9vty2wtf2tdz',
-                  name: '/api/v1/metadata/stations',
-                  status: 'under_maintenance'
-                },
-                {
-                  id: '2m8xs6g8chhd',
-                  name: 'Rail GraphQL',
-                  status: 'operational'
-                }
-              ]
-            })
-          )
-        }
-      )
+      http.get('https://status.digitraffic.fi/api/v2/summary.json', () => {
+        return HttpResponse.json({
+          components: [
+            {
+              id: 'nfys4zwym2wz',
+              name: 'Rail MQTT',
+              status: 'major_outage'
+            },
+            {
+              id: '9vty2wtf2tdz',
+              name: '/api/v1/metadata/stations',
+              status: 'under_maintenance'
+            },
+            {
+              id: '2m8xs6g8chhd',
+              name: 'Rail GraphQL',
+              status: 'operational'
+            }
+          ]
+        })
+      })
     ]
   }
 }
