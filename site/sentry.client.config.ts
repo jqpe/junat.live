d58e5249c5b29531ffa564a673a36e1156a9521a
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-04-22 19:56:38 +0300

    feat: remove sentry replay integration (#355)

--- site/sentry.client.config.ts
+++ site/sentry.client.config.ts
@@ -3,12 +3,7 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  replaysOnErrorSampleRate: 1,
-  replaysSessionSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
-  integrations: [
-    new CaptureConsole({ levels: ['error', 'warn'] }),
-    new Sentry.Replay()
-  ],
+  integrations: [new CaptureConsole({ levels: ['error', 'warn'] })],
   tracesSampleRate: 1,
   debug: false
 })
