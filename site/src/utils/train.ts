d6b112a4daca71eda6e3b8e6b89e2fe0fcd1fc5a
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-03 08:50:09 +0300

    refactor: remove simplified train utils
    Also:
    - use underscore before custom train interface to help differentiate with the actual type
    - change trains in future to use train.departureDate (train no longer has scheduledDate baked in.) This behaviour should be tested though, because the departure date is from the origin station, not the current station in the application.
    - getNewTrains no longer requires stations as parameter

--- site/src/utils/train.ts
+++ site/src/utils/train.ts
@@ -1,18 +1,10 @@
-import type { SimplifiedTrain } from '@typings/simplified_train'
 import type { Locale } from '@typings/common'
 
 // Not polyfilled by Next.js
 // See https://caniuse.com/mdn-javascript_builtins_array_at
 import 'core-js/actual/array/at'
 
-import { getDestinationTimetableRow } from '@utils/get_destination_timetable_row'
-
 import translate from './translate'
-import { LocalizedStation } from '~/lib/digitraffic'
-
-export interface ITrain extends Partial<SimplifiedTrain> {
-  scheduledTime: string
-}
 
 export type Codes = [
   'AE',
@@ -42,7 +34,7 @@ export type Codes = [
 
 export type Code = Codes[number]
 
-interface Train {
+interface _Train {
   timeTableRows: {
     stationShortCode: string
     type: 'DEPARTURE' | 'ARRIVAL'
@@ -59,80 +51,8 @@ interface Train {
   departureDate: string
 }
 
-export const sortSimplifiedTrains = <T extends ITrain>(
-  trains: Readonly<T[]>
-) => {
-  return [...trains].sort((aTrain, bTrain) => {
-    return Date.parse(aTrain.scheduledTime) - Date.parse(bTrain.scheduledTime)
-  })
-}
-
-export const simplifyTrains = <
-  T extends Parameters<typeof simplifyTrain>[2][number]
->(
-  trains: Train[],
-  stationShortCode: string,
-  stations: T[]
-) => {
-  return trains.map(train => simplifyTrain(train, stationShortCode, stations))
-}
-
-/**
- * Returns a train with only necessary values used in the application.
- *
- * Used with digitraffic trains endpoint to reduce data allocated; this reduces memory usage significantly.
- */
-export const simplifyTrain = <
-  T extends { stationShortCode: string; stationName: Record<Locale, string> }
->(
-  train: Train,
-  stationShortCode: string,
-  stations: T[]
-): SimplifiedTrain => {
-  const destinationTimetableRow = getDestinationTimetableRow(
-    train,
-    stations.find(station => station.stationShortCode === stationShortCode)
-      ?.stationShortCode
-  )
-
-  const timetableRow = getFutureTimetableRow(
-    stationShortCode,
-    train.timeTableRows
-  )
-
-  const destinationStation = stations.find(station => {
-    return (
-      station.stationShortCode === destinationTimetableRow?.stationShortCode
-    )
-  })
-
-  if (!destinationStation) {
-    throw new Error(
-      `Station could not be found for ${destinationTimetableRow?.stationShortCode}`
-    )
-  }
-
-  if (!timetableRow) {
-    throw new Error(`Couldn't find timetable row for ${stationShortCode}.`)
-  }
-
-  return {
-    commercialStop: timetableRow.commercialStop,
-    destination: destinationStation.stationName,
-    scheduledTime: timetableRow.scheduledTime,
-    liveEstimateTime: timetableRow.liveEstimateTime,
-    trainNumber: train.trainNumber,
-    trainType: train.trainType,
-    version: train.version,
-    commuterLineID: train.commuterLineID,
-    track: timetableRow.commercialTrack,
-    departureDate: train.departureDate,
-    cancelled: timetableRow.cancelled
-  }
-}
-
 export const getTrainType = (code: Code, locale: Locale): string => {
-  type TrainKeys = keyof typeof import('../locales/en.json')['trainTypes']
+  type TrainKeys = keyof (typeof import('../locales/en.json'))['trainTypes']
 
   const tr = translate(locale)
   const t = (train: TrainKeys) => tr('trainTypes', train)
@@ -191,40 +111,41 @@ export const getFutureTimetableRow = <
   )
 }
 
-type TrainWithTime = { liveEstimateTime?: string; scheduledTime: string }
-
 /**
  * From a list of trains return only those whose current time is greater than current date.
  */
-export const trainsInFuture = <T extends TrainWithTime>(newTrains: T[]) => {
+export const trainsInFuture = <T extends { departureDate: string }>(
+  newTrains: T[]
+) => {
   return newTrains.filter(train => {
-    const trainTime = train.liveEstimateTime || train.scheduledTime
-
-    return Date.parse(trainTime) > Date.now()
+    return Date.parse(train.departureDate) > Date.now()
   })
 }
 
-export const getNewTrains = (
-  trains: SimplifiedTrain[],
-  updatedTrain: Train,
+export const getNewTrains = <T extends _Train>(
+  trains: T[],
+  updatedTrain: T,
   stationShortCode: string,
-  stations: LocalizedStation[],
   type: 'ARRIVAL' | 'DEPARTURE' = 'DEPARTURE'
 ) => {
   return trains.map(train => {
-    const updatedTimetableRow = getFutureTimetableRow(
+    const updated = getFutureTimetableRow(
       stationShortCode,
       updatedTrain.timeTableRows,
       type
     )
+    const original = getFutureTimetableRow(
+      stationShortCode,
+      train.timeTableRows,
+      type
+    )
 
-    if (
-      train.trainNumber === updatedTrain.trainNumber &&
-      train.scheduledTime === updatedTimetableRow?.scheduledTime
-    ) {
-      const t = simplifyTrain(updatedTrain, stationShortCode, stations)
+    const sameTrainNumber = train.trainNumber === updatedTrain.trainNumber
 
-      return { ...train, ...t }
+    const sameScheduledTime = original?.scheduledTime === updated?.scheduledTime
+
+    if (sameTrainNumber && sameScheduledTime) {
+      return { ...train, ...updatedTrain }
     }
 
     return train
