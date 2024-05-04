059b4d1c82c243da5269d4b449ff191c5dbb7213
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-04 19:56:55 +0300

    refactor: pass required type param

--- site/src/lib/digitraffic/hooks/use_live_trains_subscription.ts
+++ site/src/lib/digitraffic/hooks/use_live_trains_subscription.ts
@@ -107,5 +107,5 @@ export const updateMatchingTrains = (
 
   const newTrains = getNewTrains(trains, updatedTrain, stationShortCode, type)
 
-  return trainsInFuture(newTrains, stationShortCode)
+  return trainsInFuture(newTrains, stationShortCode, type)
 }
