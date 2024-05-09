a55ad9ffa7777ae059969cb7c2634b1b480c2d3e
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-09 22:38:39 +0300

    test(sb): add single train page story

---
+++ site/src/features/pages/single_train/single_train.stories.tsx
@@ -0,0 +1,111 @@
+import { TimeTableRowType, type SimpleTrainFragment } from '~/generated/graphql'
+
+import { Meta, StoryFn } from '@storybook/react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { graphql, HttpResponse } from 'msw'
+import constants from '~/constants'
+import Page from '~/layouts/page'
+
+import { getCalendarDate } from '~/utils/date'
+import { TrainPage } from './components/page'
+
+export const Default: StoryFn<typeof TrainPage> = () => {
+  return <TrainPage />
+}
+
+Default.parameters = {
+  msw: {
+    handlers: [
+      /** See ~/lib/digitraffic/queries/single_train */
+      graphql.query('singleTrain', () => {
+        return HttpResponse.json({
+          data: { train: [TRAIN] }
+        })
+      })
+    ]
+  }
+}
+
+// The BlankState is show when a train was fetched and train is still undefined
+export const BlankState = Default.bind({})
+BlankState.parameters = {
+  msw: {
+    handlers: [
+      /** See ~/lib/digitraffic/queries/single_train */
+      graphql.query('singleTrain', () => {
+        return HttpResponse.json({
+          data: {}
+        })
+      })
+    ]
+  }
+}
+
+const today = new Date()
+const newDate = (hours: number, minutes: number) => {
+  return new Date(
+    Date.UTC(
+      today.getUTCFullYear(),
+      today.getUTCMonth(),
+      today.getUTCDate(),
+      today.getUTCHours() + hours,
+      today.getUTCMinutes() + minutes
+    )
+  )
+}
+
+const ROW: NonNullable<SimpleTrainFragment['timeTableRows']>[number] = {
+  commercialStop: true,
+  scheduledTime: today.toISOString(),
+  type: TimeTableRowType.Departure,
+  commercialTrack: '5',
+  cancelled: false,
+  liveEstimateTime: null,
+  station: {
+    shortCode: 'HKI',
+    passengerTraffic: true
+  }
+}
+
+const TRAIN: SimpleTrainFragment = {
+  cancelled: false,
+  departureDate: getCalendarDate(today.toISOString()),
+  trainNumber: 1,
+  trainType: { name: 'HL' },
+  version: '288282907688',
+  commuterLineid: 'R',
+  timeTableRows: [
+    ROW,
+    {
+      ...ROW,
+      station: { shortCode: 'JP', passengerTraffic: true },
+      scheduledTime: newDate(0, 2).toISOString()
+    }
+  ]
+}
+
+export default {
+  component: TrainPage,
+  decorators: [
+    Story => {
+      return (
+        <QueryClientProvider client={new QueryClient()}>
+          <Page layoutProps={{ ...constants, locale: 'en' }}>{Story()}</Page>
+        </QueryClientProvider>
+      )
+    }
+  ],
+  parameters: {
+    nextjs: {
+      router: {
+        locale: 'en',
+        asPath: `/train/${TRAIN.trainNumber}`,
+        query: {
+          trainNumber: String(TRAIN.trainNumber),
+          date: TRAIN.departureDate
+        }
+      }
+    }
+  }
+} satisfies Meta<typeof TrainPage>
