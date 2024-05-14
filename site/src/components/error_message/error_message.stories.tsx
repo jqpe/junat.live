96451b91b9aa73a419e81f6d473571c606c2e388
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-14 22:22:04 +0300

    refactor: remove obsolete status api code (#401)

--- site/src/components/error_message/error_message.stories.tsx
+++ site/src/components/error_message/error_message.stories.tsx
@@ -1,13 +1,11 @@
 import { DigitrafficError } from '@junat/digitraffic'
 import { Meta, StoryFn } from '@storybook/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { http, HttpResponse } from 'msw'
 import { ErrorMessage } from './'
 
-export const DigitrafficNetworkError: StoryFn<typeof ErrorMessage> = args => {
+export const DigitrafficNetworkError: StoryFn<typeof ErrorMessage> = () => {
   return (
     <ErrorMessage
-      locale={args.locale}
       error={new DigitrafficError({ path: '/', status: 0, type: 'error' })}
     />
   )
@@ -15,59 +13,35 @@ export const DigitrafficNetworkError: StoryFn<typeof ErrorMessage> = args => {
 
 export const DigitrafficTooManyRequestsError: StoryFn<
   typeof ErrorMessage
-> = args => {
+> = () => {
   return (
     <ErrorMessage
-      locale={args.locale}
       error={new DigitrafficError({ path: '/', status: 429, type: 'default' })}
     />
   )
 }
 
-export const UnknownError: StoryFn<typeof ErrorMessage> = args => {
-  return <ErrorMessage locale={args.locale} error={0} />
+export const UnknownError: StoryFn<typeof ErrorMessage> = () => {
+  return <ErrorMessage error={0} />
 }
 
-export const DigitrafficUnknownError: StoryFn<typeof ErrorMessage> = args => {
+export const DigitrafficUnknownError: StoryFn<typeof ErrorMessage> = () => {
   return (
     <ErrorMessage
-      locale={args.locale}
       error={new DigitrafficError({ path: '/', status: 404, type: 'default' })}
     />
   )
 }
 
-DigitrafficUnknownError.parameters = {
-  msw: {
-    handlers: [
-      http.get('https://status.digitraffic.fi/api/v2/summary.json', () => {
-        return HttpResponse.json({
-          components: [
-            {
-              id: 'nfys4zwym2wz',
-              name: 'Rail MQTT',
-              status: 'major_outage'
-            },
-            {
-              id: '9vty2wtf2tdz',
-              name: '/api/v1/metadata/stations',
-              status: 'under_maintenance'
-            },
-            {
-              id: '2m8xs6g8chhd',
-              name: 'Rail GraphQL',
-              status: 'operational'
-            }
-          ]
-        })
-      })
-    ]
-  }
-}
-
 export default {
   component: ErrorMessage,
-  args: { locale: 'en' },
+  parameters: {
+    nextjs: {
+      router: {
+        locale: 'en'
+      }
+    }
+  },
   argTypes: { error: { table: { disable: true } } },
   decorators: [
     Story => (
--- site/src/components/error_message/index.tsx
+++ site/src/components/error_message/index.tsx
@@ -4,96 +4,40 @@ import type { Locale } from '~/types/common'
 import { DigitrafficError } from '@junat/digitraffic'
 import Link from 'next/link'
 
-import { useDigitrafficApiStatus } from '~/lib/digitraffic'
-import interpolateString from '~/utils/interpolate_string'
 import translate from '~/utils/translate'
 import { PrimaryButton } from '~/components/primary_button'
+import { getLocale } from '~/utils/get_locale'
+import { useRouter } from 'next/router'
 
-type Status =
-  | 'operational'
-  | 'under_maintenance'
-  | 'partial_outage'
-  | 'major_outage'
+const Message = (props: { msg: ReactNode; showTrackStatusLink?: boolean }) => {
+  const router = useRouter()
+  const locale = getLocale(router.locale)
 
-const Message = ({
-  msg,
-  children,
-  locale
-}: {
-  msg: string | ReactNode
-  children?: ReactNode[]
-  locale?: Locale
-}) => {
   return (
-    <div className="bg-error-300 text-error-950 px-2 py-1 rounded-sm dark:bg-transparent dark:[border:1px_solid_theme(colors.error.700)] dark:text-error-50">
-      {msg}
-      {Number(children?.length) > 0 && (
-        <ol className="[counter-reset:digitraffic-status] pl-2 py-1 flex flex-col gap-2">
-          {children}
-        </ol>
-      )}
-      {Number(children?.length) > 0 && locale && (
+    <aside className="bg-error-300 text-error-950 px-2 py-1 rounded-sm dark:bg-transparent dark:[border:1px_solid_theme(colors.error.700)] dark:text-error-50">
+      {props.msg}
+      {props.showTrackStatusLink ? (
         <>
           {translate(locale)('trackStatus')}
           <Link href="https://status.digitraffic.fi" target="_blank">
             status.digitraffic.fi
-          </Link>
+          </Link>{' '}
         </>
-      )}
-    </div>
-  )
-}
-const Status = <
-  T extends {
-    status: Status
-    name: string
-  }
->(
-  component: T,
-  locale: Locale
-) => {
-  if (component.status === 'operational') {
-    return null
-  }
-  const t = translate(locale)
-  const i = interpolateString
-
-  const msg = {
-    major_outage: i(t('errors', 'digitraffic', '$componentMajor'), {
-      component: component.name
-    }),
-    partial_outage: i(t('errors', 'digitraffic', '$componentPartial'), {
-      component: component.name
-    }),
-    under_maintenance: i(t('errors', 'digitraffic', '$componentScheduled'), {
-      component: component.name
-    })
-  }[component.status]
-
-  return (
-    <li className="before:[content:counter(digitraffic-status)_'_'] before:text-error-500 before:font-bold before:text-xl [counter-increment:digitraffic-status]">
-      {msg}
-    </li>
+      ) : null}
+    </aside>
   )
 }
 
-type ErrorMessageProps = {
-  error: unknown
-  locale: Locale
-}
+export const ErrorMessage = ({ error }: { error: unknown }) => {
+  const router = useRouter()
+  const locale = getLocale(router.locale)
 
-export const ErrorMessage = ({ error, locale }: ErrorMessageProps) => {
   const digitraffic = error instanceof DigitrafficError
   const networkError = digitraffic && error.isNetworkError
   const tooManyRequests = digitraffic && error.status === 429
   const digitrafficHttpError = digitraffic && error.status >= 400
 
   const t = translate(locale)
-
-  const status = useDigitrafficApiStatus({
-    queryKey: error,
-    enabled: digitrafficHttpError
-  })
 
   if (tooManyRequests) {
     return <Message msg={t('errors', 'digitraffic', 'tooManyRequests')} />
@@ -104,16 +48,12 @@ export const ErrorMessage = ({ error, locale }: ErrorMessageProps) => {
   }
 
   if (digitrafficHttpError) {
-    const components =
-      status.data?.components && Object.values(status.data.components)
-
-    return Message({
-      msg: t('errors', 'digitraffic', 'unexpected'),
-      locale,
-      children: components
-        ? components.map(component => Status(component, locale))
-        : undefined
-    })
+    return (
+      <Message
+        showTrackStatusLink
+        msg={t('errors', 'digitraffic', 'unexpected')}
+      />
+    )
   }
 
   return Message({
@@ -138,7 +78,7 @@ export const ErrorMessageWithRetry = <
 }) => {
   return (
     <div className="flex flex-col gap-4 items-start">
-      <ErrorMessage error={props.error} locale={props.locale} />
+      <ErrorMessage error={props.error} />
       <PrimaryButton
         onClick={props.onRetryButtonClicked}
         style={{ position: 'relative' }}
