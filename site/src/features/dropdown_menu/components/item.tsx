2de43fb15596153a4af96a97a8d8157d9f0fc57e
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-05-14 11:26:04 +0300

    fix(a11y): indicate dropdown menu item focus

--- site/src/features/dropdown_menu/components/item.tsx
+++ site/src/features/dropdown_menu/components/item.tsx
@@ -10,12 +10,11 @@ export const Item = React.forwardRef<HTMLDivElement, Props>(function Item(
   props: Props,
   ref
 ) {
-  return (
-    <DropdownMenuItem
-      {...props}
-      ref={ref}
-      className="px-3 rounded-sm select-none transition-[background-color] duration-200 grid grid-cols-[1fr,24px]
-          items-center cursor-pointer min-h-[35px] text-[13px] font-ui"
-    />
-  )
+  return <DropdownMenuItem {...props} ref={ref} className={className} />
 })
+
+export const className = `
+    px-3 rounded-sm select-none duration-200 grid grid-cols-[1fr,24px]
+    items-center cursor-pointer min-h-[35px] text-[13px] font-ui
+    dark:focus:bg-gray-700 focus:bg-gray-100 transition-[background-color]
+`
