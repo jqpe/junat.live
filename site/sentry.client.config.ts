3fca3d2c085dca62acc610b131bdb1bb839772ec
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-22 19:55:39 +0300

    chore: use integration function (over class)
    class integrations are deprecated

--- site/sentry.client.config.ts
+++ site/sentry.client.config.ts
@@ -1,9 +1,9 @@
-import { CaptureConsole } from '@sentry/integrations'
+import { captureConsoleIntegration } from '@sentry/integrations'
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  integrations: [new CaptureConsole({ levels: ['error', 'warn'] })],
+  integrations: [captureConsoleIntegration({ levels: ['error', 'warn'] })],
   tracesSampleRate: 1,
   debug: false
 })
