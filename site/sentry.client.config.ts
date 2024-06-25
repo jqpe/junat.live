29fa4f9a6c4fd9ed8aee809b3a98aab1a434d419
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-25 14:01:03 +0300

    chore: update sentry packages

--- site/sentry.client.config.ts
+++ site/sentry.client.config.ts
@@ -1,9 +1,10 @@
-import { captureConsoleIntegration } from '@sentry/integrations'
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  integrations: [captureConsoleIntegration({ levels: ['error', 'warn'] })],
+  integrations: [
+    Sentry.captureConsoleIntegration({ levels: ['error', 'warn'] })
+  ],
   tracesSampleRate: 1,
   debug: false
 })
