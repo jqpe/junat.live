e33f57bc9a20101a59afee382089a64023ac9f29
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-26 13:23:14 +0300

    ci: update prettier

---
+++ tooling/prettier/index.js
@@ -0,0 +1,41 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@junat",
+    "^@junat/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  // Values and types are imported in the same line if version >= 4.5.0
+  // E.g. import { type Train, sortTrains } from "cool-pkg", use 4.4.0 to avoid this
+  importOrderTypeScriptVersion: "4.4.0",
+  overrides: [
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.js.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
+  ],
+};
+
+export default config;
