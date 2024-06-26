756224f9e746e1a1d8a70bff754461e934dec200
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-27 00:39:22 +0300

    revert: utils in locales package
    This requires some hacks to make it work in Next.js and would complicate the code base for little benefit. A better approach is to keep the actual loading of the localizations implementation specific (e.g. use next-intl)

--- packages/locales/src/index.ts
+++
@@ -1,6 +0,0 @@
-export type Locales = readonly ["fi", "en", "sv"];
-export type Locale = Locales[number];
-
-export const LOCALES = ["fi", "en", "sv"] as const satisfies Locales;
-
-export * from "./utils.js";
