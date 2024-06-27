aaae21c2d19095978aebbedfc39b7cf1fdf672b4
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-27 14:52:58 +0300

    refactor: move get destination timetable row to utils/train

--- site/src/components/timetable_row/index.tsx
+++ site/src/components/timetable_row/index.tsx
@@ -1,24 +1,27 @@
 import type { AnimationControls } from "framer-motion";
-import { motion, useAnimation } from "framer-motion";
 import type { LinkProps } from "next/link";
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import { motion, useAnimation } from "framer-motion";
 
 import type { Train } from "@junat/digitraffic/types";
-import { translate } from "~/utils/translate";
 
-import { useTimetableRow } from "~/hooks/use_timetable_row";
 import type { LocalizedStation } from "~/lib/digitraffic";
-import { getStationPath } from "~/lib/digitraffic";
 import type { Locale } from "~/types/common";
-import { getFormattedTime } from "~/utils/date";
-import { getDestinationTimetableRow } from "~/utils/get_destination_timetable_row";
 import type { Code } from "~/utils/train";
-import { getFutureTimetableRow, getTrainType } from "~/utils/train";
+import { useTimetableRow } from "~/hooks/use_timetable_row";
+import { getStationPath } from "~/lib/digitraffic";
+import { getFormattedTime } from "~/utils/date";
 import {
-    hasLiveEstimateTime as getHasLiveEstimateTime,
-    hasLongTrainType as getHasLongTrainType,
-    getTrainHref,
+  getDestinationTimetableRow,
+  getFutureTimetableRow,
+  getTrainType,
+} from "~/utils/train";
+import { translate } from "~/utils/translate";
+import {
+  hasLiveEstimateTime as getHasLiveEstimateTime,
+  hasLongTrainType as getHasLongTrainType,
+  getTrainHref,
 } from "./helpers";
 import { useRestoreScrollPosition } from "./hooks";
 
--- site/src/utils/train.test.ts
+++ site/src/utils/train.test.ts
@@ -1,9 +1,15 @@
 import { describe, expect, it } from "vitest";
 
+import {
+  getDestinationTimetableRow,
+  getFutureTimetableRow,
+  getTrainType,
+  sortTrains,
+} from "~/utils/train";
 import { translate } from "~/utils/translate";
-
-import { getFutureTimetableRow, getTrainType, sortTrains } from "~/utils/train";
 import { LOCALES } from "../../src/constants/locales";
+
+import "core-js/actual/array/at";
 
 describe("get train type", () => {
   it.each([
@@ -268,4 +274,57 @@ describe("sort trains", () => {
 
     expect(trains).toStrictEqual(trainsCopy);
   });
+});
+
+const train = {
+  timeTableRows: [
+    { stationShortCode: "HKI", type: "DEPARTURE" },
+    { stationShortCode: "LEN", type: "ARRIVAL" },
+    { stationShortCode: "LEN", type: "DEPARTURE" },
+    { stationShortCode: "AIN", type: "ARRIVAL" },
+  ],
+  commuterLineID: "P",
+} as const;
+
+const noCommuterLineId = { ...train, commuterLineID: undefined } as const;
+
+const [first, airport, last] = (() => {
+  return [0, 1, -1].map((i) => train.timeTableRows.at(i)?.stationShortCode);
+})();
+
+it("returns last timetable row if from is defined but commuter line id is undefined", () => {
+  const tr = getDestinationTimetableRow(noCommuterLineId, first);
+
+  expect(tr.stationShortCode).toBe(last);
+});
+
+it.each([train, { ...train, commuterLineID: "I" }])(
+  "always returns last timetable row if from is undefined or equal to LEN",
+  (trainMock) => {
+    const getTr = (from?: string) => {
+      return getDestinationTimetableRow(trainMock, from).stationShortCode;
+    };
+
+    expect(getTr()).toStrictEqual(last);
+    expect(getTr("LEN")).toStrictEqual(last);
+  },
+);
+
+it("returns airport if commuter line id is defined and from is not equal to LEN", () => {
+  const { stationShortCode } = getDestinationTimetableRow(train, first);
+
+  expect(stationShortCode).toStrictEqual(airport);
+});
+
+it("returns airport even if from doesn't exist in timetable rows", () => {
+  const noFrom = {
+    ...train,
+    timeTableRows: train.timeTableRows.filter(
+      (tr) => tr.stationShortCode !== first,
+    ),
+  } as const;
+
+  const { stationShortCode } = getDestinationTimetableRow(noFrom, first);
+
+  expect(stationShortCode).toStrictEqual(airport);
 });
