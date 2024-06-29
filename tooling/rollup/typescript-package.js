ea1061bbe7b5c6a2d21958663aee2fbe874680fd
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-29 19:46:18 +0300

    chore: production minifaction with terser

--- tooling/rollup/typescript-package.js
+++ tooling/rollup/typescript-package.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'rollup'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
+import { defineConfig } from 'rollup'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   input: 'src/index.ts',
   output: {
+    sourcemap: true,
     dir: 'dist',
     format: 'esm',
-    preserveModules: true
+    preserveModules: true,
   },
-  plugins: [typescript()]
+  plugins: [typescript(), isProd && terser({})],
 })
