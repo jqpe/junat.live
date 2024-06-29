0415764e30c8e7d62b4011a4e4a97bc4c727b2fe
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-06-29 15:52:36 +0300

    refactor: simplify function body

--- site/src/utils/date.ts
+++ site/src/utils/date.ts
@@ -1,15 +1,8 @@
-const prefix = (n: string) => (n.length === 1 ? `0${n}` : n)
-
 /**
  * Get calendar date in extended format as specified in [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601#Calendar_dates)
  */
 export const getCalendarDate = (date: string) => {
-  return new Intl.DateTimeFormat('fi')
-    .format(Date.parse(date))
-    .split(/\./g)
-    .reverse()
-    .map(part => prefix(part))
-    .join('-')
+  return new Date(date).toISOString().split('T')[0]
 }
 
 /**
