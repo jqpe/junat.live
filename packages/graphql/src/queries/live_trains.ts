36e29479165f00e81b83e0b1b6a53da890104615
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-04 23:41:44 +0300

    wip: remove interop from queries...
    todo: better typing and api

--- packages/graphql/src/queries/live_trains.ts
+++ packages/graphql/src/queries/live_trains.ts
@@ -1,5 +1,3 @@
-import type { LiveTrainFragment } from '#generated/graphql.js'
-
 import { graphql } from '#generated'
 
 export const trains = graphql(`
@@ -25,47 +23,3 @@ export const trains = graphql(`
     }
   }
 `)
-
-type Train = {
-  timeTableRows: {
-    stationShortCode: string
-    type: 'DEPARTURE' | 'ARRIVAL'
-    liveEstimateTime?: string
-    scheduledTime: string
-    commercialTrack?: string
-    cancelled: boolean
-  }[]
-  commuterLineID?: string
-  trainNumber: number
-  version: number
-  trainType: string
-  departureDate: string
-}
-
-export const normalizeTrains = (trains: LiveTrainFragment[]) => {
-  return trains.map(train => {
-    type NonNullRows = Array<
-      NonNullable<NonNullable<(typeof train)['timeTableRows']>[number]>
-    >
-
-    const timetableRows = <NonNullRows>(
-      train.timeTableRows?.filter(tr => tr !== null)
-    )
-
-    const normalizedTrain = <Train>{
-      ...train,
-      version: Number(train.version),
-      commuterLineID: train.commuterLineid,
-      trainType: train.trainType.name,
-      timeTableRows: timetableRows.map(tr => ({
-        ...tr,
-        stationShortCode: tr.station.shortCode,
-      })),
-    }
-
-    // @ts-expect-error typeof SimpleTrainFragment
-    delete normalizedTrain.commuterLineid
-
-    return normalizedTrain
-  })
-}
