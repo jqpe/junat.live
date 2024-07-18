962e01429ea55726db53f330d6ea42e657ee0ec0
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-18 17:33:53 +0300

    chore: prettier tailwind

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -1,12 +1,22 @@
 /** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
 /** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+import { fileURLToPath } from 'node:url'
 
 /** @type { PrettierConfig | SortImportsConfig } */
 const config = {
   semi: false,
   arrowParens: 'avoid',
   singleQuote: true,
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  tailwindConfig: fileURLToPath(
+    new URL('../../site/tailwind.config.ts', import.meta.url),
+  ),
+  tailwindFunctions: ['cx', 'cva'],
   importOrder: [
     '<TYPES>',
     '<TYPES>^@junat',
