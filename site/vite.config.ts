6f93c461ecefb70846c400e682404c4669e21ab3
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-31 18:50:33 +0300

    chore(deps): upgrade vitest, vite

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,16 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
-import path from 'node:path'
-
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
-
-// Vite does not support `publishConfig.directory` and will look for packages relative to `process.cwd()` which is `site`
-const resolveDistributedPackage = (name: string) => {
-  return path.resolve(process.cwd(), `../packages/${name}/dist`)
-}
 
 export default defineConfig({
   plugins: [
@@ -21,11 +14,6 @@ export default defineConfig({
     // @ts-ignore
     svgr({ exportAsDefault: true, include: '**/*.svg' }),
   ],
-  resolve: {
-    alias: {
-      '@junat/digitraffic': resolveDistributedPackage('digitraffic'),
-    },
-  },
   test: {
     environment: 'jsdom',
     setupFiles: ['tests/_setup.ts'],
@@ -43,7 +31,7 @@ export default defineConfig({
         '**/*.stories.tsx',
       ],
       provider: 'istanbul',
-      reporter: 'json',
+      reporter: ['json'],
     },
   },
 })
