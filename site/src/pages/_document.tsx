e312f6cc4cb370e3e916b8813b57520633d76556
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-08-01 12:45:32 +0300

    fix: msapplication tile color

--- site/src/pages/_document.tsx
+++ site/src/pages/_document.tsx
@@ -50,7 +50,7 @@ export default function Document() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="apple-mobile-web-app-title" content={SITE_NAME} />
         <meta name="application-name" content={SITE_NAME} />
-        <meta name="msapplication-TileColor" content="#fbfbfc" />
+        <meta name="msapplication-TileColor" content="#603cba" />
       </Head>
       <script
         dangerouslySetInnerHTML={{
