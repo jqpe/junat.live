eb5dbb41b888b570cbaa222b718d04805bc2d567
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-08-07 00:48:05 +0300

    fix: hoist prettier plugins (fixes prettier)

--- site/tailwind.config.ts
+++ site/tailwind.config.ts
@@ -1,5 +1,5 @@
 import config from '@junat/tailwind'
 
-config.content.push('../node_modules/@junat/ui/dist/**/*.js',)
+config.content.push('../node_modules/@junat/ui/dist/**/*.js')
 
 export default config
