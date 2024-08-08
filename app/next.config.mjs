3e7794f5937e1a1ccfa10307d92a89a6bdce997a
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-08-08 19:45:19 +0300

    chore: re-init app with next.js

---
+++ app/next.config.mjs
@@ -0,0 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? null : `http://${internalHost}:3000`,
+}
+
+export default nextConfig
