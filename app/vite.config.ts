ba69d4ebc2a8ba2839c16857cf6d5ee047cb284d
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-08-12 15:02:30 +0300

    begin migrating away from next.js
    While keeping Next.js would've been amazing on paper dynamic routes and search parameters just don't work on ios. This is issue is not present on tanstack router.

---
+++ app/vite.config.ts
@@ -0,0 +1,24 @@
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const host = process.env.TAURI_DEV_HOST
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite(), react()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: { ignored: ['**/src-tauri/**'] },
+  },
+})
