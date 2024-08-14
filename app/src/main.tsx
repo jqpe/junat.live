fc766600c92906dd463450a5094f0a04be613ec2
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-08-14 13:43:37 +0300

    refactor providers

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,17 +1,11 @@
 import { StrictMode } from 'react'
-import { Toast, ToastProvider } from '@radix-ui/react-toast'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import i18n from 'i18next'
 import ReactDOM from 'react-dom/client'
 import { initReactI18next } from 'react-i18next'
 
 import { DEFAULT_LOCALE } from '@junat/core'
-import { DialogProvider } from '@junat/ui/components/dialog'
 
-import { useWakeLock } from '~/hooks/use_wake_lock'
-import { queryClient } from '~/lib/react_query'
 import { routeTree } from './routeTree.gen'
 
 import '@junat/ui/bottom-sheet.css'
@@ -46,30 +40,12 @@ declare module '@tanstack/react-router' {
   }
 }
 
-function AppProvider() {
-  void useWakeLock()
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <DialogProvider>
-        <ToastProvider>
-          <RouterProvider router={router} />
-
-          <Toast />
-        </ToastProvider>
-      </DialogProvider>
-
-      <ReactQueryDevtools buttonPosition="bottom-left" />
-    </QueryClientProvider>
-  )
-}
-
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <AppProvider />
+      <RouterProvider router={router} />
     </StrictMode>,
   )
 }
--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -1,11 +1,28 @@
+import { Toast, ToastProvider } from '@radix-ui/react-toast'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+import { DialogProvider } from '@junat/ui/components/dialog'
+
+import { useWakeLock } from '~/hooks/use_wake_lock'
+import { queryClient } from '~/lib/react_query'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+  component: () => {
+    void useWakeLock()
+
+    return (
+      <>
+        <QueryClientProvider client={queryClient}>
+          <DialogProvider>
+            <ToastProvider>
+              <Outlet />
+
+              <Toast />
+            </ToastProvider>
+          </DialogProvider>
+        </QueryClientProvider>
+      </>
+    )
+  },
 })
