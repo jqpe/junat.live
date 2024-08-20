5ddf6aa00ae17d84b66148eb97ec9b123eb0a55e
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-08-20 20:01:41 +0300

    chore(deps): patches, tauri cli rc 5

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -8,11 +8,13 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_geolocation::init())
         .setup(|app| {
-            #[cfg(target_os = "ios")]
-            ios::enable_bf_navigation(app);
-
             #[cfg(not(target_os = "ios"))]
-            let _app = app;
+            let _ = app;
+
+            #[cfg(target_os = "ios")]
+            {
+                ios::enable_bf_navigation(app);
+            }
 
             Ok(())
         })
