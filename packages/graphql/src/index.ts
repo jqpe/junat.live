8fe777d88609edc3282ed4657a48e19087718052
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-12-10 11:12:44 +0200

    feat!: remove digitraffic passenger info
    The quality of the messages is suboptimal, will switch to digitransit as the provider instead.

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,13 +1,10 @@
 // fragments
 export * from '#fragments/live_train.js'
 export * from '#fragments/single_train.js'
-export * from '#fragments/passenger_info.js'
-
 
 // queries
 export * from '#queries/live_trains.js'
 export * from '#queries/single_train.js'
-export * from '#queries/passenger_info.js'
 
 // generated
 export * from '#generated/gql.js'
