4341e5c97e407e1f9f9c9946cbec5144b749e52b
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-12-11 12:38:44 +0200

    feat(a11y): add keyboard navigation

--- site/src/features/pages/station/components/alert.tsx
+++ site/src/features/pages/station/components/alert.tsx
@@ -47,6 +47,8 @@ export const Alert = ({ stationShortCode }: AlertProps) => {
 
   const iconFill = cx('fill-secondary-700 dark:fill-secondary-200')
   const [open, setOpen] = React.useState(false)
+  const [hasFocus, setHasFocus] = React.useState(false)
+
   const [hidden, setHidden] = React.useState<false | string>(false)
 
   const alertsQuery = useAlerts({
@@ -85,58 +87,67 @@ export const Alert = ({ stationShortCode }: AlertProps) => {
           animate={hidden ? { height: 0, padding: 0 } : {}}
           key={alert.alertHeaderText}
           className={cx(
-            'flex flex-col rounded-sm bg-secondaryA-300 p-1 text-secondary-700 shadow',
-            '-mt-3 mb-2 dark:border-secondaryA-600 dark:text-gray-500',
-            'overflow-hidden md:-mt-6 md:mb-4',
+            'flex rounded-md bg-secondaryA-300 p-1 text-secondary-700 shadow md:mb-4',
+            '-mt-3 mb-2 border border-transparent dark:text-gray-500 md:-mt-6',
+            hasFocus
+              ? 'border-secondary-500 dark:border-grayA-700'
+              : 'border-transparent',
           )}
         >
-          <div className="flex" onClick={() => setOpen(!open)}>
-            <div className="overflow-hidden text-sm leading-4">
-              <div className="flex select-none items-center gap-1">
-                <motion.div
-                  animate={{ rotate: open ? 0 : 180 }}
-                  className="flex"
-                >
-                  <Chevron className={cx('h-3 w-3', iconFill)} />
-                </motion.div>
+          <div
+            tabIndex={0}
+            onClick={() => setOpen(!open)}
+            onBlur={() => void setHasFocus(false)}
+            onFocusCapture={() => void setHasFocus(true)}
+            onKeyDown={event => {
+              if (event.key === ' ' || event.key === 'Enter') {
+                event.preventDefault()
+                setOpen(!open)
+              }
+            }}
+            className="overflow-hidden text-sm leading-4"
+          >
+            <div className="flex select-none items-center gap-1">
+              <motion.div animate={{ rotate: open ? 0 : 180 }} className="flex">
+                <Chevron className={cx('h-3 w-3', iconFill)} />
+              </motion.div>
 
-                <h5
-                  className={cx(
-                    'font-mono text-[.7rem] tracking-tight text-secondary-800',
-                    'dark:text-secondary-200',
-                  )}
-                >
-                  {alert.alertHeaderText}
-                </h5>
-              </div>
-
-              <AnimatePresence>
-                {open && (
-                  <motion.div
-                    className="mt-1"
-                    initial={{ height: '0', translateY: -10 }}
-                    animate={{ height: 'auto', translateY: 0 }}
-                    exit={{ height: '0', translateY: 10, opacity: 0 }}
-                  >
-                    {alert.alertDescriptionText}{' '}
-                    {hasAlertUrl(alert.alertUrl) && (
-                      <a target="_blank" href={alert.alertUrl}>
-                        {t('readMore')}
-                      </a>
-                    )}
-                  </motion.div>
+              <h5
+                className={cx(
+                  'font-mono text-[.7rem] tracking-tight text-secondary-800',
+                  'dark:text-secondary-200',
                 )}
-              </AnimatePresence>
+              >
+                {alert.alertHeaderText}
+              </h5>
             </div>
 
-            <button
-              className="ml-auto flex"
-              onClick={() => handleAlertHide(alert!)}
-              title={t('close')}
-            >
-              <Close className={iconFill} />
-            </button>
+            <AnimatePresence>
+              {open && (
+                <motion.div
+                  className="mt-1"
+                  initial={{ height: '0', translateY: -10 }}
+                  animate={{ height: 'auto', translateY: 0 }}
+                  exit={{ height: '0', translateY: 10, opacity: 0 }}
+                >
+                  {alert.alertDescriptionText}{' '}
+                  {hasAlertUrl(alert.alertUrl) && (
+                    <a target="_blank" href={alert.alertUrl}>
+                      {t('readMore')}
+                    </a>
+                  )}
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
+
+          <button
+            className="ml-auto flex h-min rounded-full"
+            onClick={() => handleAlertHide(alert!)}
+            title={t('close')}
+          >
+            <Close className={iconFill} />
+          </button>
         </motion.article>
       )
     })
