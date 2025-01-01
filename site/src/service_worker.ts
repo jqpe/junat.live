8ac90e2f3b268f647239cd52534eddc2d10273ce
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-01-01 23:08:03 +0200

    feat: cache metadata and analytics script (#600)

--- site/src/service_worker.ts
+++ site/src/service_worker.ts
@@ -1,7 +1,12 @@
 import type { PrecacheEntry, SerwistGlobalConfig } from 'serwist'
 
 import { defaultCache } from '@serwist/next/worker'
-import { Serwist } from 'serwist'
+import {
+  CacheFirst,
+  ExpirationPlugin,
+  Serwist,
+  StaleWhileRevalidate,
+} from 'serwist'
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -16,7 +21,35 @@ const serwist = new Serwist({
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: defaultCache,
+  runtimeCaching: [
+    {
+      matcher: ({ url }) =>
+        url.href === 'https://analytics.junat.live/script.js',
+      handler: new StaleWhileRevalidate({
+        cacheName: 'analytics',
+        plugins: [
+          new ExpirationPlugin({
+            maxAgeSeconds: 24 * 60 * 60, // day
+          }),
+        ],
+      }),
+    },
+    {
+      matcher: ({ url }) =>
+        url.host === 'rata.digitraffic.fi' && url.pathname.includes('metadata'),
+      handler: new CacheFirst({
+        cacheName: 'metadata',
+        plugins: [
+          new ExpirationPlugin({
+            maxAgeFrom: 'last-used',
+            purgeOnQuotaError: true,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // week
+          }),
+        ],
+      }),
+    },
+    ...defaultCache,
+  ],
 })
 
 serwist.addEventListeners()
