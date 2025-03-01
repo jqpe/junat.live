3ddca92b5430f5c5c998b0e2a015399b5ff33b99
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-03-01 15:18:20 +0200

    fix: do not trigger navigation for all keys

--- site/src/components/timetable_row/timetable_row.stories.tsx
+++ site/src/components/timetable_row/timetable_row.stories.tsx
@@ -3,7 +3,7 @@ import type { GetTranslatedValue } from '@junat/core/i18n'
 import type { TimetableRowProps } from '.'
 
 import { getRouter } from '@storybook/nextjs/router.mock'
-import { expect, userEvent, within } from '@storybook/test'
+import { expect, fireEvent, userEvent, within } from '@storybook/test'
 
 import { getTrainHref } from '@junat/core/utils/train'
 
@@ -58,6 +58,7 @@ export default {
     // and set the last station id
     {
       await userEvent.click(row)
+      await expect(getRouter().push).toHaveBeenCalledOnce()
 
       await expect(getRouter().push).toHaveBeenCalledWith(
         getTrainHref(
@@ -66,6 +67,26 @@ export default {
           TRAIN.trainNumber,
         ),
       )
+    }
+
+    // Enter key should push a new route
+    {
+      await fireEvent.keyDown(row, { key: 'Enter' })
+      await expect(getRouter().push).toHaveBeenCalledTimes(2)
+    }
+
+    // Space key should push a new route
+    {
+      await fireEvent.keyDown(row, { key: '\u0020' })
+      await expect(getRouter().push).toHaveBeenCalledTimes(3)
+    }
+
+    // Other keys should not push a new route
+    {
+      const keys = ['ArrowDown', 'ArrowUp', 'Escape']
+
+      await Promise.all(keys.map(key => fireEvent.keyDown(row, { key: key })))
+      await expect(getRouter().push).toHaveBeenCalledTimes(3)
     }
   },
 } satisfies Meta<TimetableRowProps>
