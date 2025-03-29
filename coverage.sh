3234906faf07184e872d03ea4b4b28560461948a
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-03-29 11:37:46 +0200

    fixes
    remove redundant merging (codecov should find automatically), remove unused deps

--- coverage.sh
+++ coverage.sh
@@ -9,13 +9,9 @@ run_coverage() {
     pnpm dlx serve $project/storybook-static -L -p 6006 &
     pnpm dlx wait-on tcp:6006 && pnpm -F $project "$filter"
 
-    pnpx nyc report --reporter=lcovonly -t $project/coverage --report-dir coverage/
-    pnpm shx mv coverage/lcov.info coverage/sb-$(basename $project).lcov.info
-
     kill $(lsof -t -i:6006) 2>/dev/null || true
 }
 
-pnpm shx rm -rf "coverage/**"
 pnpm build-storybook
 
 run_coverage "./site" "/coverage-storybook/"
@@ -23,9 +19,3 @@ run_coverage "./packages/ui" "/coverage-storybook/"
 
 # Unit tests
 pnpm coverage
-
-for file in $(find . -not -path "*/node_modules/*" -name "coverage-final.json"); do
-    project=$(dirname $(dirname $file))
-    pnpx nyc report --reporter=lcovonly -t $project/coverage --report-dir coverage/
-    pnpm shx mv coverage/lcov.info coverage/$(basename $project).lcov.info
-done
