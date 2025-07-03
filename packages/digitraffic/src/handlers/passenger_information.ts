20ce6b13b69a4f257a1e3ef5e9ece881183a9e9f
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-07-03 08:41:37 +0300

    feat: add rest of parameters

--- packages/digitraffic/src/handlers/passenger_information.ts
+++ packages/digitraffic/src/handlers/passenger_information.ts
@@ -5,16 +5,34 @@ import { createFetch } from '../base/create_fetch.js'
 
 interface GetPassengerInformationOptions extends HandlerOptions {
   stationShortCode?: string
+  /** Whether to return alerts that only affect stations, not trains. @default false*/
+  onlyGeneral?: true
+  /** Return alerts that affect the specified train */
+  trainNumber?: number
+  /** Return alerts that affect a specified train on the given date */
+  departureDate?: string
 }
 
 export const fetchPassengerInformationMessages = async ({
   stationShortCode: station,
+  onlyGeneral,
+  trainNumber,
+  departureDate,
   signal,
 }: GetPassengerInformationOptions) => {
   const params = new URLSearchParams()
 
   if (station) {
     params.append('station', station)
+  }
+  if (onlyGeneral === true) {
+    params.append('only_general', 'true')
+  }
+  if (trainNumber) {
+    params.append('train_number', trainNumber.toString())
+  }
+  if (departureDate) {
+    params.append('train_departure_date', departureDate)
   }
 
   const messages = await createFetch<PassengerInformationMessage[]>(
