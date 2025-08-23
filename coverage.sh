a3aa17410b9e56e5899e8ca8a96b1f826578eb8a
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-08-23 13:00:06 +0300

    Revert "maybe"
    This reverts commit 21177f34c65ca6ed3f769455e7cf6945148fd45b.

--- coverage.sh
+++ coverage.sh
@@ -8,17 +8,16 @@ run_coverage() {
     local project=$1
     local filter=$2
 
-    NEXT_PUBLIC_DIGITRANSIT_KEY="$NEXT_PUBLIC_DIGITRANSIT_KEY"\
-     pnpm dlx serve $project/storybook-static -L -p 6006 &
+    pnpm dlx serve $project/storybook-static -L -p 6006 &
     pnpm dlx wait-on tcp:6006 && pnpm -F $project "$filter"
 
     kill $(lsof -t -i:6006) 2>/dev/null || true
 }
 
-NEXT_PUBLIC_DIGITRANSIT_KEY="$NEXT_PUBLIC_DIGITRANSIT_KEY"\
- pnpm build-storybook
+pnpm build-storybook
 
 run_coverage "./site" "/coverage-storybook/"
 run_coverage "./packages/ui" "/coverage-storybook/"
 
+# Unit tests
 pnpm coverage
