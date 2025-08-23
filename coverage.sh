21177f34c65ca6ed3f769455e7cf6945148fd45b
Jasper Nykänen <jasperjnykanen@gmail.com>
2025-08-23 12:55:56 +0300

    maybe

--- coverage.sh
+++ coverage.sh
@@ -8,16 +8,17 @@ run_coverage() {
     local project=$1
     local filter=$2
 
-    pnpm dlx serve $project/storybook-static -L -p 6006 &
+    NEXT_PUBLIC_DIGITRANSIT_KEY="$NEXT_PUBLIC_DIGITRANSIT_KEY"\
+     pnpm dlx serve $project/storybook-static -L -p 6006 &
     pnpm dlx wait-on tcp:6006 && pnpm -F $project "$filter"
 
     kill $(lsof -t -i:6006) 2>/dev/null || true
 }
 
-pnpm build-storybook
+NEXT_PUBLIC_DIGITRANSIT_KEY="$NEXT_PUBLIC_DIGITRANSIT_KEY"\
+ pnpm build-storybook
 
 run_coverage "./site" "/coverage-storybook/"
 run_coverage "./packages/ui" "/coverage-storybook/"
 
-# Unit tests
 pnpm coverage
