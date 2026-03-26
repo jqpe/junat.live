ac0770b7c1c28954078de8b317299f23e6be41dd
Jasper Nykänen <jasperjnykanen@gmail.com>
2026-03-26 22:03:44 +0200

    chore: upgrade ui storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,21 +1,22 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
+import { mergeConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-themes',
-    '@storybook/addon-links',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
-      },
-    },
-    '@storybook/addon-interactions',
     '@storybook/addon-coverage',
+    '@storybook/addon-docs',
   ],
   core: {
     builder: '@storybook/builder-vite',
+  },
+  viteFinal: async config => {
+    return mergeConfig(config, {
+      plugins: [svgr()],
+    })
   },
   framework: '@storybook/react-vite',
   staticDirs: ['./static'],
